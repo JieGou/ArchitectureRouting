a2005bd98561e4fbd5089efbe85855d13903a955
Ken Toyoda <k.toyoda@arent3d.com>
2021-03-31 09:50:10 +0900

    Added FromToWindow

---
+++ Arent3d.Architecture.Routing.App/Forms/FromToWindow.xaml.cs
@@ -0,0 +1,93 @@
+﻿using System ;
+using System.Windows ;
+using System.Collections.ObjectModel ;
+using System.Collections.Generic ;
+using System.Linq ;
+using System.Windows.Controls ;
+using Arent3d.Architecture.Routing.RouteEnd ;
+using Autodesk.Revit.UI ;
+using Autodesk.Revit.DB ;
+
+namespace Arent3d.Architecture.Routing.App.Forms
+{
+  
+  public partial class FromToWindow : ModalWindowBase
+  {
+    /*public List<string>? Diameters { get ; set ; }
+    public int? DiameterIndex { get ; set ; }*/
+    public ObservableCollection<FromToItems> FromToItemsList { get ; set ; }
+
+    //public FromToWindow(Document doc, IList<double> diameters, int diameterIndex)
+    //public FromToWindow( Document doc, IEnumerable<Route> allRoute )
+    public FromToWindow( Document doc, ObservableCollection<FromToItems> fromToItemsList )
+    {
+      InitializeComponent() ;
+
+      FromToItemsList = fromToItemsList ;
+    }
+
+
+    public class FromToItems
+    {
+      public string? Id { get ; set ; }
+      
+      //From
+      public ConnectorIndicator? From { get ; set ; }
+      public string? FromType { get ; set ; }
+      public string? FromConnectorId { get ; set ; }
+      public string? FromElementId { get ; set ; }
+
+      
+      //public string? From { get ; set ; } ここから。ConnectorIndicatorの
+      //適切な値を、Textに表示できるように
+      public string? ToType { get ; set ; }
+      public string? ToConnectorId { get ; set ; }
+      public string? ToElementId { get ; set ; }
+
+      public string? Domain { get ; set ; }
+
+      //SystemType
+      public ObservableCollection<MEPSystemType>? SystemTypes { get ; set ; }
+      public MEPSystemType? SystemType { get ; set ; }
+      public int SystemTypeIndex { get ; set ; } 
+      
+      //CurveType
+      public ObservableCollection<MEPCurveType>? CurveTypes { get ; set ; }
+      public MEPCurveType? CurveType { get ; set ; }
+      public int CurveTypeIndex { get ; set ; }
+
+      public bool? Direct { get ; set ; }
+      //Diameters
+      public string? Diameters { get ; set ; }
+      
+      public string? PassPoints { get ; set ; }
+
+    }
+
+
+    private void Dilog2Buttons_OnImportOnClick( object sender, RoutedEventArgs e )
+    {
+      TaskDialog.Show( "test", "Import" ) ;
+    }
+
+    private void Dilog2Buttons_OnExportOnClick( object sender, RoutedEventArgs e )
+    {
+      TaskDialog.Show( "test", "Export" ) ;
+    }
+
+    private void Dialog3Buttons_OnOnOKClick( object sender, RoutedEventArgs e )
+    {
+      this.Close() ;
+    }
+
+    private void Dialog3Buttons_OnOnApplyClick( object sender, RoutedEventArgs e )
+    {
+      throw new NotImplementedException() ;
+    }
+
+    private void Dialog3Buttons_OnOnCancelClick( object sender, RoutedEventArgs e )
+    {
+      this.Close() ;
+    }
+  }
+}
---
+++ Arent3d.Architecture.Routing.App/ViewModel/FromToWindowViewModel.cs
@@ -0,0 +1,80 @@
+﻿using System ;
+using System.Collections.Generic ;
+using System.Collections.ObjectModel ;
+using System.Diagnostics ;
+using System.Linq ;
+using System.Runtime.CompilerServices ;
+using System.Windows ;
+using System.Windows.Interop ;
+using Arent3d.Architecture.Routing.App.Forms ;
+using Arent3d.Revit.UI ;
+using Autodesk.Revit.DB ;
+using Autodesk.Revit.UI ;
+
+namespace Arent3d.Architecture.Routing.App.ViewModel
+{
+  public class FromToWindowViewModel
+  {
+    private static UIDocument? UiDoc { get ; set ; }
+    //Dialog
+    private static FromToWindow? _openedDialog ;
+    
+    public static void ShowFromToWindow( UIDocument uiDocument, IEnumerable<Route> allRoutes)
+    {
+      UiDoc = uiDocument ;
+
+      if ( _openedDialog != null ) {
+        _openedDialog.Close() ;
+      }
+
+      ObservableCollection<FromToWindow.FromToItems> fromToItemsList = new ObservableCollection<FromToWindow.FromToItems>() ;
+
+      foreach ( var route in allRoutes ) {
+        RouteMEPSystem routeMepSystem = new RouteMEPSystem( UiDoc.Document, route ) ;
+        var systemTypeList = new ObservableCollection<MEPSystemType>( routeMepSystem.GetSystemTypes( uiDocument.Document, route.GetReferenceConnector() ).OrderBy( s => s.Name ).ToList() ) ; 
+        var systemType = routeMepSystem.MEPSystemType ;
+        int systemTypeIndex = systemTypeList.ToList().FindIndex( s => s.Id == systemType.Id  ) ;
+        var curveTypeList = new ObservableCollection<MEPCurveType>( routeMepSystem.GetCurveTypes( uiDocument.Document, routeMepSystem.CurveType.GetType() ).OrderBy( s => s.Name ).ToList() ) ;
+        var curveType = routeMepSystem.CurveType ;
+        int curveTypeIndex = curveTypeList.ToList().FindIndex( c => c.Id == curveType.Id ) ;
+        IEnumerable<string> subRouteDiameters = route.SubRoutes.Select(s => (int)Math.Round(UnitUtils.ConvertFromInternalUnits( s.GetDiameter(UiDoc.Document), UnitTypeId.Millimeters )) + " mm" );
+        
+        var test = route.FirstFromConnector() ;
+        fromToItemsList.Add(new FromToWindow.FromToItems()
+        {
+          Id = route.RouteName,
+          From = route.FirstFromConnector(),
+          FromType = route.FirstFromConnector()?.ToString().Split(':')[0],
+          FromConnectorId = route.FirstFromConnector()?.ConnectorId.ToString(),
+          FromElementId = route.FirstFromConnector()?.ElementId.ToString(),
+          ToType = route.FirstToConnector()?.ToString().Split(':')[0],
+          ToConnectorId = route.FirstToConnector()?.ConnectorId.ToString(),
+          ToElementId = route.FirstToConnector()?.ElementId.ToString(),
+          Domain = routeMepSystem.CurveType.GetType().Name.Split( 'T' )[ 0 ] + " Type",
+          SystemTypes = systemTypeList,
+          SystemType = systemType,
+          SystemTypeIndex = systemTypeIndex,
+          CurveTypes = curveTypeList,
+          CurveType = curveType,
+          CurveTypeIndex = curveTypeIndex,
+          Diameters = string.Join( ",", subRouteDiameters ),
+          //PassPoints = 
+          Direct = route.GetSubRoute(0)?.IsRoutingOnPipeSpace,
+        });
+        
+      }
+      
+      //var dialog = new FromToWindow( uiDocument.Document, allRoutes );
+      var dialog = new FromToWindow( uiDocument.Document, fromToItemsList );
+
+      System.Windows.Interop.WindowInteropHelper helper = new System.Windows.Interop.WindowInteropHelper(dialog);
+      HwndSource? hwndSource = HwndSource.FromHwnd(UiDoc.Application.MainWindowHandle);
+      Window? wnd = hwndSource.RootVisual as Window;
+      if(wnd != null) {
+        dialog.Owner = wnd ;
+        dialog.Show() ;
+        _openedDialog = dialog ;
+      }
+    }
+  }
+}
