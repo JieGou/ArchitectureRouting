980b6274131bed614cb22cf1678f104a123b9b2f
Ken Toyoda <k.toyoda@arent3d.com>
2021-04-07 08:29:45 +0900

    Added FindDoubleIndex method

---
+++ Arent3d.Architecture.Routing.App/EnumerableExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System ;
+using System.Collections.Generic ;
+using System.Linq ;
+using Autodesk.Revit.DB ;
+
+namespace Arent3d.Architecture.Routing.App
+{
+  public static class EnumerableExtensions
+  {
+    public static int FindDoubleIndex( this IEnumerable<double> source, double? value, Document doc )
+    {
+      if ( value != null ) {
+        var doubles = source.TakeWhile( d => Math.Abs( ( d - (double) value ) ) > doc.Application.VertexTolerance ) ;
+        var targetIndex = doubles.Count() ;
+
+        return targetIndex ;
+      }
+
+      return -1 ;
+    }
+  }
+}
