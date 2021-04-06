28d2bad20529470996e034145069052d7be84266
Ken Toyoda <k.toyoda@arent3d.com>
2021-04-06 10:34:55 +0900

    Added GetRibbonTabFromName method

--- Arent3d.Architecture.Routing.App/MonitorSelectionApplicationEvent.cs
+++ Arent3d.Architecture.Routing.App/MonitorSelectionApplicationEvent.cs
@@ -1,4 +1,9 @@
-﻿using System.Windows.Controls.Ribbon ;
+﻿using System.Collections.Generic ;
+using System.Diagnostics ;
+using System.Linq ;
+using System.Windows.Controls.Ribbon ;
+using Arent3d.Revit.I18n ;
+using Arent3d.Utility ;
 using Autodesk.Revit.DB.Events ;
 using Autodesk.Revit.DB ;
 using Autodesk.Windows ;
@@ -11,62 +16,52 @@ namespace Arent3d.Architecture.Routing.App
   {
     public static void MonitorSelectionApplicationInitialized( object sender, ApplicationInitializedEventArgs e )
     {
-      RibbonTab? SelectionTab = null ;
-      RibbonPanel? SelectionPanel = null ;
-      RibbonItem? SelectionButton = null ;
+      RibbonTab? selectionTab = null ;
+      RibbonPanel? selectionPanel = null ;
+      RibbonItem? selectionButton = null ;
+      string? targetTabName = "App.Routing.TabName".GetAppStringByKey() ;
 
-      foreach ( var tab in ComponentManager.Ribbon.Tabs ) {
-        if ( tab.Id == "自動ルーティング" ) {
-          SelectionTab = tab ;
 
-          //Messaging.DebugMessage($"Found Tab: {SelectionTab}");
-
-          foreach ( var panel in tab.Panels ) {
-            if ( panel.Source.Title == "From-To" ) {
-              SelectionPanel = panel ;
-
-              //Messaging.DebugMessage( $"Found Panel: {SelectionPanel}" ) ;
-
-              foreach ( var item in panel.Source.Items ) {
-                if ( item.Id == "CustomCtrl_%CustomCtrl_%自動ルーティング%arent3d.architecture.routing.routing%arent3d.architecture.routing.app.commands.monitor_selection_command" ) {
-                  SelectionButton = item ;
-
-                  //Messaging.DebugMessage( $"Found Button: {SelectionButton}" ) ;
-
-                  break ;
-                }
+      selectionTab = GetRibbonTabFromName( targetTabName ) ;
+      if ( selectionTab == null ) {
+        return ;
+      }
+      else {
+        foreach ( var panel in selectionTab.Panels ) {
+          if ( panel.Source.Title == "From-To" ) {
+            selectionPanel = panel ;
+            foreach ( var item in panel.Source.Items ) {
+              if ( item.Id == "CustomCtrl_%CustomCtrl_%" + targetTabName + "%arent3d.architecture.routing.routing%arent3d.architecture.routing.app.commands.monitor_selection_command" ) {
+                selectionButton = item ;
+                break ;
+              }
+              else {
+                continue ;
               }
             }
           }
-
-          break ;
+          else {
+            continue;
+          }
         }
       }
 
-      if ( SelectionPanel != null && SelectionButton != null ) {
+      if ( selectionPanel != null && selectionButton != null ) {
         var position = GetPositionBeforeButton( "ID_REVIT_FILE_PRINT" ) ;
 
-        PlaceButtonOnQuickAccess( position, SelectionButton ) ;
+        PlaceButtonOnQuickAccess( position, selectionButton ) ;
         
       }
     }
 
+    private static RibbonTab? GetRibbonTabFromName( string? targetTabName ) 
+      => ComponentManager.Ribbon.Tabs.ToList().Find( t => t.Id == targetTabName ) ;
+
     private static int GetPositionBeforeButton( string s )
     {
-      var position = 0 ;
+      var items = ComponentManager.QuickAccessToolBar.Items.TakeWhile( item => item.Id != s ) ;
 
-
-      foreach ( var item in ComponentManager.QuickAccessToolBar.Items ) {
-        if ( string.IsNullOrWhiteSpace( item.Id ) ) {
-          continue ;
-        }
-
-        position++ ;
-
-        if ( item.Id == s ) {
-          break ;
-        }
-      }
+      var position = items.Count() + 1 ;
 
       return position ;
     }
