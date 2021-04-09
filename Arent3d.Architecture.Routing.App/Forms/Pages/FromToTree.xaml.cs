3b2c3bb3c9192ec010c40af43b13bd16de99a634
Ken Toyoda <k.toyoda@arent3d.com>
2021-04-09 15:55:33 +0900

    Added AddConnectorItemToTreeViewItem, SetHeaderStackPanel method to Show Icon to Treeview

--- Arent3d.Architecture.Routing.App/Forms/Pages/FromToTree.xaml.cs
+++ Arent3d.Architecture.Routing.App/Forms/Pages/FromToTree.xaml.cs
@@ -2,12 +2,15 @@
 using System.Collections.Generic ;
 using System.Collections.ObjectModel ;
 using System.Diagnostics ;
+using System.IO.Packaging ;
 using System.Linq ;
 using System.Windows ;
 using System.Windows.Controls ;
 using System.Windows.Input ;
 using System.Windows.Media ;
+using System.Windows.Media.Imaging ;
 using Arent3d.Architecture.Routing.App.ViewModel ;
+using Arent3d.Utility ;
 using Autodesk.Revit.DB ;
 using Autodesk.Revit.UI ;
 
@@ -17,18 +20,15 @@ namespace Arent3d.Architecture.Routing.App.Forms
   {
     private Document? Doc { get ; set ; }
     private UIDocument? UiDoc { get ; set ; }
-
     private IReadOnlyCollection<Route>? AllRoutes { get ; set ; }
+
+    private SortedDictionary<string, TreeViewItem>? ItemDictionary { get ; set ; }
+
 
     public FromToTree()
     {
       InitializeComponent() ;
-    }
-
-
-    public class FromTo
-    {
-      public Route? Route { get ; set ; }
+      ItemDictionary = new SortedDictionary<string, TreeViewItem>() ;
     }
 
 
@@ -66,45 +66,144 @@ namespace Arent3d.Architecture.Routing.App.Forms
     /// </summary>
     private void DisplayTreeViewItem( IReadOnlyCollection<Route> allRoutes )
     {
-      //clear selection;
-      ClearSelectedItem() ;
-      // clear items first
-      FromToTreeView.Items.Clear() ;
+      if ( FromToTreeView != null ) {
+        //clear selection;
+        ClearSelectedItem() ;
+        // clear items first
+        FromToTreeView.Items.Clear() ;
 
-      // routename and childrenname dictionary
-      var routeDictionary = new SortedDictionary<string, TreeViewItem>() ;
+        if ( ItemDictionary != null ) {
+          ItemDictionary.Clear() ;
 
-      var childBranches = new List<Route>() ;
+          var childBranches = new List<Route>() ;
 
-      var parentFromTos = new List<Route>() ;
+          var parentFromTos = new List<Route>() ;
 
 
-      foreach ( var route in allRoutes ) {
-        //get ChildBranches
-        if ( route.HasParent() ) {
-          childBranches.Add( route ) ;
+          foreach ( var route in allRoutes ) {
+            // get ChildBranches
+            if ( route.HasParent() ) {
+              childBranches.Add( route ) ;
+            }
+            // get ChildBranches
+            else {
+              parentFromTos.Add( route ) ;
+            }
+          }
+
+          // create Route tree
+          foreach ( var route in parentFromTos.Distinct().OrderBy( r => r.RouteName ).ToList() ) {
+            // create route treeviewitem/
+            TreeViewItem routeItem = new TreeViewItem() { Uid = route.OwnerElement?.Id.ToString(), Tag = "Route" } ;
+            SetHeaderStackPanel( routeItem, route.RouteName ) ;
+            // store in dict
+            ItemDictionary[ route.RouteName ] = routeItem ;
+            // add to treeview
+            FromToTreeView.Items.Add( routeItem ) ;
+            // add connector to branch treeviewitem
+            AddConnectorItemToTreeViewItem( Doc, routeItem, route ) ;
+            /*foreach ( var subRoute in route.SubRoutes ) {
+              TreeViewItem subrouteItem = new TreeViewItem() { Uid = route.OwnerElement?.Id.ToString(), Tag = "Route" } ;
+            }*/
+          }
+
+          // create branches tree
+          foreach ( var c in childBranches ) {
+            TreeViewItem branchItem = new TreeViewItem() { Uid = c.OwnerElement?.Id.ToString(), Tag = "Route" } ;
+            SetHeaderStackPanel( branchItem, c.RouteName ) ;
+            var parentRouteName = c.GetParentBranches().ToList().Last().RouteName ;
+            // search own parent TreeViewItem
+            ItemDictionary[ parentRouteName ].Items.Add( branchItem ) ;
+            ItemDictionary[ c.RouteName ] = branchItem ;
+            // add connector to branch treeviewitem
+            AddConnectorItemToTreeViewItem( Doc, branchItem, c ) ;
+          }
         }
-        else {
-          parentFromTos.Add( route ) ;
+      }
+    }
+
+    /// <summary>
+    /// GetParentTree recursively
+    /// </summary>
+    /// <param name="targetItem"></param>
+    /// <param name="parentName"></param>
+    /// <returns></returns>
+    private TreeViewItem? GetParentTreeViewItem( TreeViewItem targetItem, string parentName )
+    {
+      foreach ( var item in targetItem.Items ) {
+        // sreach in current
+        if ( item is TreeViewItem treeViewItem ) {
+          if ( treeViewItem.Header.Equals( parentName ) ) {
+            return treeViewItem ;
+          }
+
+          // search in childs
+          TreeViewItem? childItem = this.GetParentTreeViewItem( treeViewItem, parentName ) ;
+          if ( childItem != null ) {
+            return childItem ;
+          }
         }
       }
 
-      // create Route tree
-      foreach ( var route in parentFromTos.Distinct().OrderBy( r => r.RouteName ).ToList() ) {
-        // create route treeviewitem
-        TreeViewItem routeItem = new TreeViewItem() { Header = route.RouteName } ;
-        // store in dict
-        routeDictionary[ route.RouteName ] = routeItem ;
-        // add to treeview
-        FromToTreeView.Items.Add( routeItem ) ;
+      return null ;
+    }
+
+    /// <summary>
+    /// Add Conncecotor to FromTo TreeVieItem
+    /// </summary>
+    /// <param name="doc"></param>
+    /// <param name="targetItem"></param>
+    /// <param name="targetRoute"></param>
+    private void AddConnectorItemToTreeViewItem( Document? doc, TreeViewItem targetItem, Route targetRoute )
+    {
+      if ( Doc != null ) {
+        var connectors = targetRoute.GetAllConnectors( Doc ) ;
+        foreach ( var connector in connectors ) {
+          if ( connector.Owner is FamilyInstance familyInstance ) {
+            TreeViewItem viewItem = new TreeViewItem() { Uid = connector.Owner.Id.ToString(), Tag = "Connector" } ;
+            SetHeaderStackPanel( viewItem, familyInstance.Symbol.Family.Name + ":" + connector.Owner.Name ) ;
+            targetItem.Items.Add( viewItem ) ;
+          }
+          else {
+            continue ;
+          }
+        }
+      }
+      else {
+        return ;
+      }
+    }
+
+    /// <summary>
+    /// set headerstackpanel to treeviewitem
+    /// </summary>
+    /// <param name="treeViewItem"></param>
+    /// <param name="routeName"></param>
+    private void SetHeaderStackPanel( TreeViewItem treeViewItem, string routeName )
+    {
+      BitmapImage bmi = new BitmapImage() ;
+
+      switch ( treeViewItem.Tag ) {
+        case "Route" :
+          bmi = new BitmapImage( new Uri( "../../resources/MEP.ico", UriKind.Relative ) ) ;
+          break ;
+        case "Connector" :
+          bmi = new BitmapImage( new Uri( "../../resources/InsertBranchPoint.png", UriKind.Relative ) ) ;
+          break ;
       }
 
-      // create branches tree
-      foreach ( var c in childBranches ) {
-        TreeViewItem viewItem = new TreeViewItem() { Header = c.RouteName } ;
-
-        routeDictionary[ c.GetParentBranches().ToList()[ 0 ].RouteName ].Items.Add( viewItem ) ;
-      }
+      // create stackpanel
+      StackPanel sp = new StackPanel() ;
+      sp.Orientation = Orientation.Horizontal ;
+      Image img = new Image() ;
+      img.Source = bmi ;
+      img.Height = 15 ;
+      img.Width = 15 ;
+      sp.Children.Add( img ) ;
+      TextBlock tb = new TextBlock() ;
+      tb.Text = routeName ;
+      sp.Children.Add( tb ) ;
+      treeViewItem.Header = sp ;
     }
 
     /// <summary>
@@ -143,14 +242,24 @@ namespace Arent3d.Architecture.Routing.App.Forms
         return ;
       }
 
-      var selectedRoute = AllRoutes?.ToList().Find( r => r.RouteName == selectedItem.Header.ToString() ) ;
+      List<ElementId>? targetElements = new List<ElementId>() ;
+      switch ( selectedItem.Tag ) {
+        case "Route" :
+          var selectedRoute = AllRoutes?.ToList().Find( r => r.OwnerElement?.Id == UIHelper.GetElementIdFromViewItem( selectedItem ) ) ;
 
-      if ( selectedRoute != null ) {
-        var targetElements = Doc?.GetAllElementsOfRouteName<Element>( selectedRoute.RouteName ).Select( elem => elem.Id ).ToList() ;
-        //Select targetElements
-        UiDoc?.Selection.SetElementIds( targetElements ) ;
+          if ( selectedRoute != null ) {
+            targetElements = Doc?.GetAllElementsOfRouteName<Element>( selectedRoute.RouteName ).Select( elem => elem.Id ).ToList() ;
+            //Select targetElements
+            UiDoc?.Selection.SetElementIds( targetElements ) ;
+            DisplaySelectedFromTo( selectedRoute ) ;
+          }
 
-        DisplaySelectedFromTo( selectedRoute ) ;
+          break ;
+        case "Connector" :
+          targetElements.Add( UIHelper.GetElementIdFromViewItem( selectedItem ) ) ;
+          UiDoc?.Selection.SetElementIds( targetElements ) ;
+          SelectedFromTo.ClearDialog();
+          break ;
       }
     }
 
@@ -165,12 +274,24 @@ namespace Arent3d.Architecture.Routing.App.Forms
         return ;
       }
 
-      var selectedRoute = AllRoutes?.ToList().Find( r => r.RouteName == selectedItem.Header.ToString() ) ;
+      selectedItem.IsExpanded = false ;
 
-      if ( selectedRoute != null ) {
-        var targetElements = Doc?.GetAllElementsOfRouteName<Element>( selectedRoute.RouteName ).Select( elem => elem.Id ).ToList() ;
-        //Show targetElements
-        UiDoc?.ShowElements( targetElements ) ;
+      List<ElementId>? targetElements = new List<ElementId>() ;
+      switch ( selectedItem.Tag ) {
+        case "Route" :
+          var selectedRoute = AllRoutes?.ToList().Find( r => r.OwnerElement?.Id == UIHelper.GetElementIdFromViewItem( selectedItem ) ) ;
+
+          if ( selectedRoute != null ) {
+            targetElements = Doc?.GetAllElementsOfRouteName<Element>( selectedRoute.RouteName ).Select( elem => elem.Id ).ToList() ;
+            //Select targetElements
+            UiDoc?.ShowElements( targetElements ) ;
+          }
+
+          break ;
+        case "Connector" :
+          targetElements.Add( UIHelper.GetElementIdFromViewItem( selectedItem ) ) ;
+          UiDoc?.ShowElements( targetElements ) ;
+          break ;
       }
     }
 
@@ -179,15 +300,18 @@ namespace Arent3d.Architecture.Routing.App.Forms
     /// SelecteViewItem from RouteName
     /// </summary>
     /// <param name="selectedRouteName"></param>
-    public void SelectTreeViewItem( string selectedRouteName )
+    public void SelectTreeViewItem( ElementId? elementId )
     {
-      var targetItem = GetTreeViewItemFromName( FromToTreeView.Items, selectedRouteName ) ;
+      var targetItem = GetTreeViewItemFromElementId( FromToTreeView.Items, elementId ) ;
 
       if ( targetItem != null ) {
         // Select in TreeView
         targetItem.IsSelected = true ;
         // Scorll to Item
         targetItem.BringIntoView() ;
+        if ( targetItem.Tag.ToString() == "Connector" ) {
+          SelectedFromTo.ClearDialog();
+        }
       }
     }
 
@@ -220,6 +344,33 @@ namespace Arent3d.Architecture.Routing.App.Forms
         if ( childItem != null ) {
           item.IsExpanded = true ;
           return childItem ;
+        }
+      }
+
+      return null ;
+    }
+
+    /// <summary>
+    /// Get TreeViewItemObject from elementId
+    /// </summary>
+    /// <param name="collection"></param>
+    /// <param name="targetName"></param>
+    /// <returns></returns>
+    private TreeViewItem? GetTreeViewItemFromElementId( ItemCollection collection, ElementId? elementId )
+    {
+      foreach ( var item in collection ) {
+        if ( item is TreeViewItem treeViewItem ) {
+          // Find in current
+          if ( treeViewItem.Uid.Equals( elementId?.ToString() ) ) {
+            return treeViewItem ;
+          }
+
+          // Find in Childs
+          TreeViewItem? childItem = this.GetTreeViewItemFromElementId( treeViewItem.Items, elementId ) ;
+          if ( childItem != null ) {
+            treeViewItem.IsExpanded = true ;
+            return childItem ;
+          }
         }
       }
 
