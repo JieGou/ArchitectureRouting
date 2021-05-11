4f6052df1d4773609f282d3cd150b4e1fdb35ba8
Ken Toyoda <k.toyoda@arent3d.com>
2021-05-11 10:27:02 +0900

    Added PickAndChangeFixedBopHeightCommand.cs

---
+++ Arent3d.Architecture.Routing.App/Commands/Routing/PickAndChangeFixedBopHeightCommand.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic ;
+using Arent3d.Revit.I18n ;
+using Arent3d.Revit.UI ;
+using Arent3d.Utility ;
+using Autodesk.Revit.Attributes ;
+using Autodesk.Revit.UI ;
+
+namespace Arent3d.Architecture.Routing.App.Commands.Routing
+{
+  [Transaction( TransactionMode.Manual )]
+  [DisplayNameKey( "App.Commands.Routing.PickAndChangeFixedBopHeightCommand", DefaultString = "Change\nFixedBopHeight" )]
+  [Image( "resources/MEP.ico" )]
+  public class PickAndChangeFixedBopHeightCommand : RoutingCommandBase
+  {
+    protected override string GetTransactionNameKey() => "TransactionName.Commands.Routing.PickAndChangeFixedBopHeight" ;
+    protected override IAsyncEnumerable<(string RouteName, RouteSegment Segment)> GetRouteSegmentsParallelToTransaction( UIDocument uiDocument )
+    {
+      var list = PointOnRoutePicker.PickedRoutesFromSelections( uiDocument ).EnumerateAll() ;
+
+      if ( 0 < list.Count ) {
+        return Route.CollectAllDescendantBranches( list ).ToSegmentsWithName().EnumerateAll().ToAsyncEnumerable() ;
+      }
+      else {
+        // newly select
+        var pickInfo = PointOnRoutePicker.PickRoute( uiDocument, false, "Dialog.Commands.Routing.PickAndReRoute.Pick".GetAppStringByKeyOrDefault( null ) ) ;
+
+        return pickInfo.Route.CollectAllDescendantBranches().ToSegmentsWithName().EnumerateAll().ToAsyncEnumerable() ;
+      }
+    }
+    
+  }
+}
--- Arent3d.Architecture.Routing.App/RoutingAppUI.Components.cs
+++ Arent3d.Architecture.Routing.App/RoutingAppUI.Components.cs
@@ -52,6 +52,8 @@ namespace Arent3d.Architecture.Routing.App
     private readonly RibbonButton _exportRacksCommandButton ;
     private readonly RibbonButton _eraseAllRacksCommandButton ;
 
+    private readonly RibbonButton _pickAndChangeFixedBopHeightCommandButton ;
+
     private readonly RibbonButton _monitorSelectionCommandButton ;
 
     private readonly RegisterFromToTreeCommand _registerFromToTreeCommand;
@@ -82,6 +84,7 @@ namespace Arent3d.Architecture.Routing.App
         _showFromToWindowCommandButton = routingPanel.AddButton<ShowFrom_ToWindowCommand>() ;
         _showFromToTreeCommandButton = routingPanel.AddButton<ShowFromToTreeCommand>() ;
 
+        _pickAndChangeFixedBopHeightCommandButton = routingPanel.AddButton<PickAndChangeFixedBopHeightCommand>() ;
         //_fileRoutingCommandButton = routingPanel.AddButton<FileRoutingCommand>() ;
         //_exportRoutingCommandButton = routingPanel.AddButton<ExportRoutingCommand>() ;
       }
@@ -129,6 +132,7 @@ namespace Arent3d.Architecture.Routing.App
       _replaceFromToCommandButton.Enabled = false ;
       _showFromToWindowCommandButton.Enabled = false ;
       _showFromToTreeCommandButton.Enabled = false ;
+      _pickAndChangeFixedBopHeightCommandButton.Enabled = false ;
 
       _importRacksCommandButton.Enabled = false ;
       _exportRacksCommandButton.Enabled = false ;
@@ -161,6 +165,8 @@ namespace Arent3d.Architecture.Routing.App
       _replaceFromToCommandButton.Enabled = setupIsDone ;
       _showFromToWindowCommandButton.Enabled = setupIsDone ;
       _showFromToTreeCommandButton.Enabled = setupIsDone ;
+      _pickAndChangeFixedBopHeightCommandButton.Enabled = setupIsDone ;
+      
       _importRacksCommandButton.Enabled = setupIsDone ;
       _exportRacksCommandButton.Enabled = setupIsDone ;
       _eraseAllRacksCommandButton.Enabled = setupIsDone ;
