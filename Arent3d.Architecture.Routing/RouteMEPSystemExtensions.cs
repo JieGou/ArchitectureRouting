6369d9553f118c41d0fd1fa70a10ac5e18fd8d56
Ken Toyoda <k.toyoda@arent3d.com>
2021-05-25 13:14:04 +0900

    Fixed GetRules method

--- Arent3d.Architecture.Routing/RouteMEPSystemExtensions.cs
+++ Arent3d.Architecture.Routing/RouteMEPSystemExtensions.cs
@@ -59,7 +59,7 @@ namespace Arent3d.Architecture.Routing
 
     public static IEnumerable<RoutingPreferenceRule> GetRules( this RoutingPreferenceManager rpm, RoutingPreferenceRuleGroupType groupType )
     {
-      var count = rpm.GetNumberOfRules( RoutingPreferenceRuleGroupType.MechanicalJoints ) ;
+      var count = rpm.GetNumberOfRules( RoutingPreferenceRuleGroupType.Segments ) ;
       for ( var i = 0 ; i < count ; ++i ) {
         yield return rpm.GetRule( groupType, i ) ;
       }
