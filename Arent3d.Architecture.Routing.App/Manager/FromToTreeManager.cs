e44de1ce1c10cce9d1f73ac29a6d10e32e3e0139
kenToyoda <k.toyoda@arent3d.com>
2021-06-01 10:55:30 +0900

    Added SetSelectionInViewToFromToTree method

--- Arent3d.Architecture.Routing.App/Manager/FromToTreeManager.cs
+++ Arent3d.Architecture.Routing.App/Manager/FromToTreeManager.cs
@@ -14,7 +14,7 @@ namespace Arent3d.Architecture.Routing.App.Manager
   public class FromToTreeManager
   {
     public static FromToTreeManager Instance { get ; } = new FromToTreeManager() ;
-    
+
     public FromToTree? FromToTreeView = null ;
     public UIApplication? UiApp = null ;
     public DockablePane? Dockable = null ;
@@ -22,57 +22,67 @@ namespace Arent3d.Architecture.Routing.App.Manager
     private FromToTreeManager()
     {
     }
-    
+
     // view activated event
-    public void Application_ViewActivated(ViewActivatedEventArgs e )
+    public void Application_ViewActivated( ViewActivatedEventArgs e )
     {
       // provide ExternalCommandData object to dockable page
-      if ( FromToTreeView != null && UiApp != null ) {
-        var doc = UiApp.ActiveUIDocument.Document ;
+      if ( FromToTreeView == null || UiApp == null ) return ;
+      var doc = UiApp.ActiveUIDocument.Document ;
 
-        //Initialize TreeView
-        FromToTreeView.CustomInitiator( UiApp ) ;
+      //Initialize TreeView
+      FromToTreeView.CustomInitiator( UiApp ) ;
 
-        // Get Selected Routes
-        var selectedRoutes = PointOnRoutePicker.PickedRoutesFromSelections( UiApp.ActiveUIDocument ).EnumerateAll() ;
-        var selectedConnectors = doc.CollectRoutes().SelectMany( r => r.GetAllConnectors() ).ToList() ;
+      SetSelectionInViewToFromToTree( doc ) ;
+    }
 
-        //Get ElementIds in activeview
-        ElementOwnerViewFilter elementOwnerViewFilter = new ElementOwnerViewFilter( doc.ActiveView.Id ) ;
-        FilteredElementCollector collector = new FilteredElementCollector( doc, doc.ActiveView.Id ) ;
-        var elementsInActiveView = collector.ToElementIds() ;
+    /// <summary>
+    /// Set Selection to FromToTree if any route or connector are selected in View
+    /// </summary>
+    /// <param name="doc"></param>
+    /// <param name="selectedRoutes"></param>
+    /// <param name="selectedConnectors"></param>
+    private void SetSelectionInViewToFromToTree( Document doc )
+    {
+      if ( UiApp == null ) return ;
+      //Get ElementIds in activeview
+      FilteredElementCollector collector = new FilteredElementCollector( doc, doc.ActiveView.Id ) ;
+      var elementsInActiveView = collector.ToElementIds() ;
+      
+      // Get Selected Routes
+      var selectedRoutes = PointOnRoutePicker.PickedRoutesFromSelections( UiApp.ActiveUIDocument ) ;
+      var connectorsInView = doc.CollectRoutes().SelectMany( r => r.GetAllConnectors() ).Where(c =>elementsInActiveView.Contains(c.Owner.Id) ) ;
+      
 
-        if ( selectedRoutes.FirstOrDefault() is { } selectedRoute ) {
-          var selectedRouteName = selectedRoute.RouteName ;
-          var targetElements = doc?.GetAllElementsOfRouteName<Element>( selectedRouteName ).Select( elem => elem.Id ).ToList() ;
+      if ( selectedRoutes.FirstOrDefault() is { } selectedRoute ) {
+        var selectedRouteName = selectedRoute.RouteName ;
+        var targetElements = doc?.GetAllElementsOfRouteName<Element>( selectedRouteName ).Select( elem => elem.Id ).ToList() ;
 
-          if ( targetElements != null ) {
-            if ( elementsInActiveView.Any( ids => targetElements.Contains( ids ) ) ) {
-              //Select TreeViewItem
-              FromToTreeViewModel.GetSelectedElementId( selectedRoute.OwnerElement?.Id ) ;
-            }
-          }
-        }
-        else if ( selectedConnectors.Any( c => UiApp.ActiveUIDocument.Selection.GetElementIds().Contains( c.Owner.Id ) ) ) {
-          var selectedElementId = UiApp.ActiveUIDocument.Selection.GetElementIds().FirstOrDefault() ;
-          FromToTreeViewModel.GetSelectedElementId( selectedElementId ) ;
-        }
-        else {
-          FromToTreeViewModel.ClearSelection() ;
+        if ( targetElements == null ) return ;
+        if ( elementsInActiveView.Any( ids => targetElements.Contains( ids ) ) ) {
+          //Select TreeViewItem
+          FromToTreeViewModel.GetSelectedElementId( selectedRoute.OwnerElement?.Id ) ;
         }
       }
+      else if ( connectorsInView.Any( c => UiApp.ActiveUIDocument.Selection.GetElementIds().Contains( c.Owner.Id ) ) ) {
+        var selectedElementId = UiApp.ActiveUIDocument.Selection.GetElementIds().FirstOrDefault() ;
+        FromToTreeViewModel.GetSelectedElementId( selectedElementId ) ;
+      }
+      else {
+        FromToTreeViewModel.ClearSelection() ;
+      }
     }
-    
+
     // document opened event
-    public void Application_DocumentOpened( )
+    public void Application_DocumentOpened()
     {
       // provide ExternalCommandData object to dockable page
       if ( FromToTreeView == null || UiApp == null || Dockable == null ) return ;
       FromToTreeView.CustomInitiator( UiApp ) ;
       FromToTreeViewModel.FromToTreePanel = FromToTreeView ;
-      Dockable.Show();
+      Dockable.Show() ;
     }
-    
+
     // document opened event
     public void Application_DocumentChanged( Autodesk.Revit.DB.Events.DocumentChangedEventArgs e )
     {
@@ -96,12 +106,11 @@ namespace Arent3d.Architecture.Routing.App.Manager
         "TransactionName.Commands.Routing.PickRouting".GetAppStringByKeyOrDefault( "Add From-To" ),
         "TransactionName.Commands.Routing.EraseSelectedRoutes".GetAppStringByKeyOrDefault( "Erase Selected" ),
         "TransactionName.Commands.Routing.EraseAllRoutes".GetAppStringByKeyOrDefault( "Erase All From-Tos" ),
-        "TransactionName.Commands.Routing.PickAndReRoute".GetAppStringByKeyOrDefault("Reroute All"),
-        "TransactionName.Commands.Routing.RerouteAll".GetAppStringByKeyOrDefault("Reroute Selected"),
+        "TransactionName.Commands.Routing.PickAndReRoute".GetAppStringByKeyOrDefault( "Reroute All" ),
+        "TransactionName.Commands.Routing.RerouteAll".GetAppStringByKeyOrDefault( "Reroute Selected" ),
       } ;
 
       return routingTransactions.Contains( t ) ;
     }
-
   }
 }
