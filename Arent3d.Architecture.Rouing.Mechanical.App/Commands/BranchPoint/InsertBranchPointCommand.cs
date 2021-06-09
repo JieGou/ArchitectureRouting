e02db530db92f69f64229aa39e1ec70ba3c2c626
kenToyoda <k.toyoda@arent3d.com>
2021-06-09 11:19:59 +0900

    Added Commands to Arent3d.Architecture.Rouing.Mechanical.App project

---
+++ Arent3d.Architecture.Rouing.Mechanical.App/Commands/BranchPoint/InsertBranchPointCommand.cs
@@ -0,0 +1,23 @@
+﻿using Arent3d.Architecture.Routing.AppBase.Forms ;
+using Arent3d.Revit.UI ;
+using Autodesk.Revit.Attributes ;
+using Autodesk.Revit.DB ;
+using Autodesk.Revit.UI ;
+using ImageType = Arent3d.Revit.UI.ImageType ;
+
+namespace Arent3d.Architecture.Routing.Mechanical.App.Commands.BranchPoint
+{
+  [Transaction( TransactionMode.Manual )]
+  [DisplayNameKey( "Mechanical.App.Commands.BranchPoint.InsertBranchPointCommand", DefaultString = "Insert\nBranch Point" )]
+  [Image( "resources/InsertBranchPoint.png", ImageType = ImageType.Large )]
+  public class InsertBranchPointCommand : IExternalCommand
+  {
+    public Result Execute( ExternalCommandData commandData, ref string message, ElementSet elements )
+    {
+      var dialog = new ShowDialog( this.ToString() ) ;
+      dialog.Show() ;
+
+      return Result.Succeeded ;
+    }
+  }
+}
---
+++ Arent3d.Architecture.Rouing.Mechanical.App/Commands/Routing/ShowFromToTreeCommand.cs
@@ -0,0 +1,38 @@
+﻿using System ;
+using Arent3d.Revit.UI ;
+using Autodesk.Revit.Attributes ;
+using Autodesk.Revit.DB ;
+using Autodesk.Revit.UI ;
+
+namespace Arent3d.Architecture.Routing.AppBase.Commands.Routing
+{
+  [Transaction( TransactionMode.Manual )]
+  [Regeneration( RegenerationOption.Manual )]
+  [DisplayNameKey( "App.Commands.Routing.ShowFromTreeCommand", DefaultString = "From-To\nTree" )]
+  [Image( "resources/MEP.ico" )]
+  public class ShowFromToTreeCommand : IExternalCommand
+  {
+    private UIDocument? _uiDocument = null ;
+
+    public Result Execute( ExternalCommandData commandData, ref string message, ElementSet elements )
+    {
+      _uiDocument = commandData.Application.ActiveUIDocument ;
+
+      try {
+        var dpid = new DockablePaneId( PaneIdentifiers.GetFromToTreePaneIdentifier() ) ;
+        var dp = _uiDocument.Application.GetDockablePane( dpid ) ;
+        if ( ! dp.IsShown() ) {
+          dp.Show() ;
+        }
+        else {
+          dp.Hide() ;
+        }
+      }
+      catch ( Exception e ) {
+        TaskDialog.Show( "ShowFromToTreeCommand", e.Message ) ;
+      }
+
+      return Result.Succeeded ;
+    }
+  }
+}
