f661f4da80a1cc7f4d4a0eb0bc244987c4a6b50d
kenToyoda <k.toyoda@arent3d.com>
2021-06-09 13:48:03 +0900

    Moved files from AppBase to Mechanical.App

---
+++ Arent3d.Architecture.Rouing.Mechanical.App/AppInfo.cs
@@ -0,0 +1,8 @@
+﻿namespace Arent3d.Architecture.Routing.Mechanical.App
+{
+  public class AppInfo
+  {
+    public const string ApplicationName = "Mechanical Routing Assist" ;
+    public const string ApplicationGuid = "{BC787B53-F35D-453C-9345-40F4A0EAC48A}" ;
+  }
+}
