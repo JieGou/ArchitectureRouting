9466537c90191a037cfa424ff89572d8bbab912d
kenToyoda <k.toyoda@arent3d.com>
2021-06-09 13:45:32 +0900

    Deleted Commands from AppBase

--- Arent3d.Architecture.Routing.AppBase/RevitAddinInfo.cs
+++
@@ -1,5 +0,0 @@
-﻿// Assembly info for `make_addin' command.
-
-using Arent3d.Architecture.Routing ;
-
-[assembly: Arent3d.Revit.RevitAddinVendor( AppInfo.VendorId )]
--- Arent3d.Architecture.Routing/AppInfo.cs
+++ Arent3d.Architecture.Routing/AppInfo.cs
@@ -3,7 +3,5 @@ namespace Arent3d.Architecture.Routing
   public static class AppInfo
   {
     public const string VendorId = "com.arent3d" ;
-    public const string ApplicationName = "Routing Assist" ;
-    public const string ApplicationGuid = "{BC787B53-F35D-453C-9345-40F4A0EAC48A}" ;
   }
 }
