6f35b5ec5cd75b1f7c89dddb9204b6d221bc64b1
kenToyoda <k.toyoda@arent3d.com>
2021-06-10 10:01:47 +0900

    Added PostCommandExecutor to Mechanical.App

---
+++ Arent3d.Architecture.Routing.Mechanical.App/PostCommandExecutor.cs
@@ -0,0 +1,23 @@
+﻿using System ;
+using System.Runtime.Remoting.Messaging ;
+using Arent3d.Architecture.Routing.AppBase ;
+using Arent3d.Revit.UI;
+using Autodesk.Revit.UI ;
+
+namespace Arent3d.Architecture.Routing.Mechanical.App
+{
+  public class PostCommandExecutor : IPostCommandBase
+  {
+    public void ApplyPostCommand( UIApplication app, PostCommandType postCommandType )
+    {
+      switch ( postCommandType ) {
+        case PostCommandType.ChangeRouteNameCommand :
+          app.PostCommand<Commands.PostCommands.ApplyChangeRouteNameCommand>() ;
+          break;
+        case PostCommandType.ApplySelectedFromtToChangesCommand :
+          app.PostCommand<Commands.PostCommands.ApplySelectedFromToChangesCommand>() ;
+          break;
+      }
+    }
+  }
+}
