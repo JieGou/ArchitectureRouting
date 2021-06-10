d422c4fd877bee487489cd63d7145844918f4174
kenToyoda <k.toyoda@arent3d.com>
2021-06-10 10:01:25 +0900

    Added IPostCommandBase to AppBase

---
+++ Arent3d.Architecture.Routing.AppBase/IPostCommandBase.cs
@@ -0,0 +1,15 @@
+﻿using Autodesk.Revit.UI ;
+
+namespace Arent3d.Architecture.Routing.AppBase
+{
+  public enum PostCommandType
+  {
+    ChangeRouteNameCommand,
+    ApplySelectedFromtToChangesCommand,
+  }
+  
+  public interface IPostCommandBase
+  {
+    void ApplyPostCommand(UIApplication app,PostCommandType postCommandType) ;
+  }
+}
