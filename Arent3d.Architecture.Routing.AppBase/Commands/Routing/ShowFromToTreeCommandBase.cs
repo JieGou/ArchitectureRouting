a79492d418d98c0eca82f79e904e3fc43520db13
kenToyoda <k.toyoda@arent3d.com>
2021-06-11 18:13:04 +0900

    Added ShowFromToTreeCommandBase

--- Arent3d.Architecture.Routing.AppBase/Commands/Routing/ShowFromToTreeCommandBase.cs
+++ Arent3d.Architecture.Routing.AppBase/Commands/Routing/ShowFromToTreeCommandBase.cs
@@ -14,27 +14,8 @@ namespace Arent3d.Architecture.Routing.AppBase.Commands.Routing
   [Image( "resources/MEP.ico" )]
   public abstract class ShowFromToTreeCommandBase : IExternalCommand
   {
-    private UIDocument? _uiDocument = null ;
+    protected UIDocument? _uiDocument = null ;
 
-    public Result Execute( ExternalCommandData commandData, ref string message, ElementSet elements )
-    {
-      _uiDocument = commandData.Application.ActiveUIDocument ;
-
-      try {
-        var dpid = FromToTreeManager.Instance.FromToTreeUiManager?.DpId ;
-        var dp = _uiDocument.Application.GetDockablePane( dpid ) ;
-        if ( ! dp.IsShown() ) {
-          dp.Show() ;
-        }
-        else {
-          dp.Hide() ;
-        }
-      }
-      catch ( Exception e ) {
-        TaskDialog.Show( "ShowFromToTreeCommand", e.Message ) ;
-      }
-
-      return Result.Succeeded ;
-    }
+    public abstract Result Execute( ExternalCommandData commandData, ref string message, ElementSet elements ) ;
   }
 }
--- Arent3d.Architecture.Routing.Mechanical.App/Commands/Routing/ShowFromToTreeCommand.cs
+++ Arent3d.Architecture.Routing.Mechanical.App/Commands/Routing/ShowFromToTreeCommand.cs
@@ -1,6 +1,9 @@
-﻿using Arent3d.Architecture.Routing.AppBase.Commands.Routing ;
+﻿using System ;
+using Arent3d.Architecture.Routing.AppBase.Commands.Routing ;
 using Arent3d.Revit.UI ;
 using Autodesk.Revit.Attributes ;
+using Autodesk.Revit.DB ;
+using Autodesk.Revit.UI ;
 
 namespace Arent3d.Architecture.Routing.Mechanical.App.Commands.Routing
 {
@@ -10,5 +13,25 @@ namespace Arent3d.Architecture.Routing.Mechanical.App.Commands.Routing
   [Image( "resources/MEP.ico" )]
   public class ShowFromToTreeCommand : ShowFromToTreeCommandBase
   {
+    public override Result Execute( ExternalCommandData commandData, ref string message, ElementSet elements )
+    {
+      _uiDocument = commandData.Application.ActiveUIDocument ;
+
+      try {
+        var dpid = RoutingApp.FromToTreeManager.FromToTreeUiManager?.DpId ;
+        var dp = _uiDocument.Application.GetDockablePane( dpid ) ;
+        if ( ! dp.IsShown() ) {
+          dp.Show() ;
+        }
+        else {
+          dp.Hide() ;
+        }
+      }
+      catch ( Exception e ) {
+        TaskDialog.Show( "ShowFromToTreeCommand", e.Message ) ;
+      }
+
+      return Result.Succeeded ;
+    }
   }
 }
