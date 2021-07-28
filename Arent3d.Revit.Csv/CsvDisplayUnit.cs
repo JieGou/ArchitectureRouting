364eb5b4b6f854a4779e59f6d7822baf9c30146a
Atsushi <maruyama@arent3d.com>
2021-07-28 15:44:52 +0900

    Moved some projects into routing-core & supported dll version info

--- Arent3d.Revit.Csv/CsvDisplayUnit.cs
+++
@@ -1,9 +0,0 @@
-using Autodesk.Revit.DB ;
-
-namespace Arent3d.Revit.Csv
-{
-  public static class CsvDisplayUnit
-  {
-    public static DisplayUnit DisplayUnit { get ; set ; } = DisplayUnit.METRIC ;
-  }
-}
