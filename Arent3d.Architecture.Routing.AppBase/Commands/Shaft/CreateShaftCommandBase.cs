6d57f07c899e077d3468877047c0a2f3e5cbb937
quoclotus <quocpc@lotus-ts.com>
2021-09-10 14:14:40 +0700

    add feature create wire between 2 connectors in create shaft command

--- Arent3d.Architecture.Routing.AppBase/Commands/Shaft/CreateShaftCommandBase.cs
+++ Arent3d.Architecture.Routing.AppBase/Commands/Shaft/CreateShaftCommandBase.cs
@@ -1,10 +1,12 @@
 ﻿using Arent3d.Revit;
-using Arent3d.Revit.I18n;
 using Arent3d.Revit.UI;
-using Arent3d.Utility;
 using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Electrical;
+using Autodesk.Revit.DB.Structure;
 using Autodesk.Revit.UI;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Arent3d.Architecture.Routing.AppBase.Commands.Shaft
 {
@@ -17,10 +19,23 @@ namespace Arent3d.Architecture.Routing.AppBase.Commands.Shaft
             try
             {
                 XYZ selectedPoint = uiDocument.Selection.PickPoint("Pick a point");
+                FilteredElementCollector collector = new FilteredElementCollector(document).OfClass(typeof(ConduitType));
+                ConduitType? type = collector.FirstElement() as ConduitType;
 
+                IList<Element> levels = new FilteredElementCollector(document).OfClass(typeof(Level)).ToElements();
+                Level? fl1 = levels.FirstOrDefault(x => x.Name.Equals("1FL")) as Level;
+                Level? fl2 = levels.FirstOrDefault(x => x.Name.Equals("2FL")) as Level;
                 var result = document.Transaction("Create Shaft", _ =>
                 {
+                    var familySymbol = document.GetFamilySymbol(RoutingFamilyType.ConnectorTwoSide)!;
+                    var fiConnector2SideFL1 = familySymbol.Instantiate(selectedPoint, fl1!, StructuralType.NonStructural);
+                    fiConnector2SideFL1.get_Parameter(BuiltInParameter.INSTANCE_ELEVATION_PARAM).Set(0);
+                    var fiConnector2SideFL2 = familySymbol.Instantiate(selectedPoint, fl2!, StructuralType.NonStructural);
+                    fiConnector2SideFL2.get_Parameter(BuiltInParameter.INSTANCE_ELEVATION_PARAM).Set(0);
 
+                    var firstPoint = new XYZ(selectedPoint.X, selectedPoint.Y, fl1!.Elevation);
+                    var secondPoint = new XYZ(selectedPoint.X, selectedPoint.Y, fl2!.Elevation);
+                    Conduit.Create(document, type!.Id, firstPoint, secondPoint, fl1.Id);
                     return Result.Succeeded;
                 });
 
