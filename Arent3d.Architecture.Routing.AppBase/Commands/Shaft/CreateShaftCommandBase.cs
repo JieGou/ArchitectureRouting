0a4bdcd6c2c957865c49214919d73159c7fed764
quoclotus <quocpc@lotus-ts.com>
2021-09-13 16:07:24 +0700

    Add check null exception for CreateShaftCommandBase

--- Arent3d.Architecture.Routing.AppBase/Commands/Shaft/CreateShaftCommandBase.cs
+++ Arent3d.Architecture.Routing.AppBase/Commands/Shaft/CreateShaftCommandBase.cs
@@ -23,19 +23,23 @@ namespace Arent3d.Architecture.Routing.AppBase.Commands.Shaft
                 ConduitType? type = collector.FirstElement() as ConduitType;
 
                 IList<Element> levels = new FilteredElementCollector(document).OfClass(typeof(Level)).ToElements();
-                Level? fl1 = levels.FirstOrDefault(x => x.Name.Equals("1FL")) as Level;
-                Level? fl2 = levels.FirstOrDefault(x => x.Name.Equals("2FL")) as Level;
+                Level? fl1 = levels.ElementAt(0) as Level;
+                Level? fl2 = levels.ElementAt(1) as Level;
                 var result = document.Transaction("Create Shaft", _ =>
                 {
-                    var familySymbol = document.GetFamilySymbol(RoutingFamilyType.ConnectorTwoSide)!;
-                    var fiConnector2SideFL1 = familySymbol.Instantiate(selectedPoint, fl1!, StructuralType.NonStructural);
-                    fiConnector2SideFL1.get_Parameter(BuiltInParameter.INSTANCE_ELEVATION_PARAM).Set(0);
-                    var fiConnector2SideFL2 = familySymbol.Instantiate(selectedPoint, fl2!, StructuralType.NonStructural);
-                    fiConnector2SideFL2.get_Parameter(BuiltInParameter.INSTANCE_ELEVATION_PARAM).Set(0);
+                    if (fl1 != null && fl2 != null)
+                    {
+                        double heightOfFloor = fl2.Elevation - fl1.Elevation;
+                        var familySymbol = document.GetFamilySymbol(RoutingFamilyType.ConnectorTwoSide)!;
+                        var bottomConnector = familySymbol.Instantiate(selectedPoint, fl1, StructuralType.NonStructural);
+                        bottomConnector.get_Parameter(BuiltInParameter.INSTANCE_ELEVATION_PARAM).Set(0);
+                        var topConnector = familySymbol.Instantiate(selectedPoint, fl1, StructuralType.NonStructural);
+                        topConnector.get_Parameter(BuiltInParameter.INSTANCE_ELEVATION_PARAM).Set(heightOfFloor);
 
-                    var firstPoint = new XYZ(selectedPoint.X, selectedPoint.Y, fl1!.Elevation);
-                    var secondPoint = new XYZ(selectedPoint.X, selectedPoint.Y, fl2!.Elevation);
-                    Conduit.Create(document, type!.Id, firstPoint, secondPoint, fl1.Id);
+                        var firstPoint = new XYZ(selectedPoint.X, selectedPoint.Y, fl1.Elevation);
+                        var secondPoint = new XYZ(selectedPoint.X, selectedPoint.Y, fl2.Elevation);
+                        Conduit.Create(document, type!.Id, firstPoint, secondPoint, fl1.Id);
+                    }
                     return Result.Succeeded;
                 });
 
