c682b410ca0ea3050dea85ce4620219ac55d0ddd
KhanhTQ <khanhtq@lotus-ts.com>
2021-09-24 11:09:10 +0700

    formnat file RoutingFamilyExtension

--- Arent3d.Architecture.Routing/RoutingFamilyExtensions.cs
+++ Arent3d.Architecture.Routing/RoutingFamilyExtensions.cs
@@ -76,14 +76,14 @@ namespace Arent3d.Architecture.Routing
     [FamilyCategory( BuiltInCategory.OST_GenericModel )]
     Envelope,
     
-    [NameOnRevit("Cable Tray")]
-    [FamilyCategory(BuiltInCategory.OST_CableTrayFitting)]
+    [NameOnRevit( "Cable Tray" )]
+    [FamilyCategory( BuiltInCategory.OST_CableTrayFitting )]
     CableTray,
     
-    [NameOnRevit("Cable Tray Fitting")]
-    [FamilyCategory(BuiltInCategory.OST_CableTrayFitting)]
+    [NameOnRevit( "Cable Tray Fitting" )]
+    [FamilyCategory( BuiltInCategory.OST_CableTrayFitting )]
     CableTrayFitting,
-    }
+  }
 
   public static class RoutingFamilyExtensions
   {
