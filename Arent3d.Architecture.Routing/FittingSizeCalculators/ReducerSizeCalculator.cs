1e51ba50254496ad2b833a782379ce63dba78a68
Atsushi <maruyama@arent3d.com>
2021-10-14 17:25:51 +0900

    Fixed bending radii calculation

--- Arent3d.Architecture.Routing/FittingSizeCalculators/ReducerSizeCalculator.cs
+++ Arent3d.Architecture.Routing/FittingSizeCalculators/ReducerSizeCalculator.cs
@@ -7,8 +7,15 @@ namespace Arent3d.Architecture.Routing.FittingSizeCalculators
 {
   internal class ReducerSizeCalculator : SizeCalculatorBase
   {
+    private readonly double _diameter1 ;
+    private readonly double _diameter2 ;
+
     public ReducerSizeCalculator( Document document, IMEPCurveGenerator mepCurveGenerator, double diameter1, double diameter2 ) : base( document, mepCurveGenerator, GetStraightLineLength( diameter1, diameter2 ) )
     {
+      _diameter1 = diameter1 ;
+      _diameter2 = diameter2 ;
+
+      CalculateConnectorPositions() ;
     }
 
     private static double GetStraightLineLength( double diameter1, double diameter2 ) => Math.Max( Math.Max( diameter1, diameter2 ) * 50, 1 ) ; // diameter * 50 or 1ft (greater)
@@ -18,6 +25,9 @@ namespace Arent3d.Architecture.Routing.FittingSizeCalculators
     protected override void GenerateFittingFromConnectors( IReadOnlyList<Connector> connectors )
     {
       if ( 2 != connectors.Count ) return ;
+
+      SetDiameter( connectors[ 0 ], _diameter1 ) ;
+      SetDiameter( connectors[ 1 ], _diameter2 ) ;
 
       Document.Create.NewTransitionFitting( connectors[ 0 ], connectors[ 1 ] ) ;
     }
