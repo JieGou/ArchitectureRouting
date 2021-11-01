217d559e31d2fc35ca1549abce6065e7078b552a
Mi Do Tra <midtt@rikkeisoft.com>
2021-11-01 17:06:20 +0700

    fix correct envelope code

--- Arent3d.Architecture.Routing/RouteGenerator.cs
+++ Arent3d.Architecture.Routing/RouteGenerator.cs
@@ -191,15 +191,12 @@ namespace Arent3d.Architecture.Routing
       if ( ! envelopes.Any() ) return ;
       var parentEnvelopes = envelopes.Where( f => string.IsNullOrEmpty( f.ParametersMap.get_Item( "Revit.Property.Builtin.ParentEnvelopeId".GetDocumentStringByKeyOrDefault( document, "Parent Envelope Id" ) ).AsString() ) ).ToList() ;
       if ( ! parentEnvelopes.Any() ) return ;
-      {
-        var childrenEnvelopes = envelopes.Where( f => ! string.IsNullOrEmpty( f.ParametersMap.get_Item( "Revit.Property.Builtin.ParentEnvelopeId".GetDocumentStringByKeyOrDefault( document, "Parent Envelope Id" ) ).AsString() ) ).ToList() ;
-        if ( ! childrenEnvelopes.Any() ) return ;
-        foreach ( var parentEnvelope in parentEnvelopes ) {
-          var parentLocation = parentEnvelope.Location as LocationPoint ;
-          foreach ( var childrenLocation in from childrenEnvelope in childrenEnvelopes let parentEnvelopeId = childrenEnvelope.ParametersMap.get_Item( "Revit.Property.Builtin.ParentEnvelopeId".GetDocumentStringByKeyOrDefault( document, "Parent Envelope Id" ) ).AsString() where parentEnvelopeId == parentEnvelope.Id.ToString() select childrenEnvelope.Location as LocationPoint ) {
-            childrenLocation!.Point = parentLocation!.Point ;
-          }
-        }
+      foreach ( var parentEnvelope in parentEnvelopes ) {
+        var parentLocation = parentEnvelope.Location as LocationPoint ;
+        var childrenEnvelope = envelopes.FirstOrDefault( f => f.ParametersMap.get_Item( "Revit.Property.Builtin.ParentEnvelopeId".GetDocumentStringByKeyOrDefault( document, "Parent Envelope Id" ) ).AsString() == parentEnvelope.Id.ToString() ) ;
+        if ( childrenEnvelope == null ) continue ;
+        var childrenLocation = childrenEnvelope.Location as LocationPoint ;
+        childrenLocation!.Point = parentLocation!.Point ;
       }
     }
   }
