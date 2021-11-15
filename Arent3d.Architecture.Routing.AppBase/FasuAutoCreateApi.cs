81959deea4a5bfe297c7410e08e6ff5b9f1ca5fc
tuank1991 <tua.nk@dtsvn.com>
2021-11-15 14:40:09 +0700

    VAVをFASUの隣に設置
    refactor code

--- Arent3d.Architecture.Routing.AppBase/FasuAutoCreateApi.cs
+++ Arent3d.Architecture.Routing.AppBase/FasuAutoCreateApi.cs
@@ -1,4 +1,5 @@
-﻿using Autodesk.Revit.DB;
+﻿using System;
+using Autodesk.Revit.DB;
 using System.Collections.Generic;
 
 namespace Arent3d.Architecture.Routing.AppBase
@@ -7,16 +8,16 @@ namespace Arent3d.Architecture.Routing.AppBase
     {
         public static IList<Element> GetAllSpaces(Document document)
         {
-            ElementCategoryFilter filter = new ElementCategoryFilter(BuiltInCategory.OST_MEPSpaces);
-            FilteredElementCollector collector = new FilteredElementCollector(document);
+            ElementCategoryFilter filter = new(BuiltInCategory.OST_MEPSpaces);
+            FilteredElementCollector collector = new(document);
             IList<Element> spaces = collector.WherePasses(filter).WhereElementIsNotElementType().ToElements();
             return spaces;
         }
 
         public static IList<Element> GetAllGroups(Document document)
         {
-            ElementCategoryFilter filter = new ElementCategoryFilter(BuiltInCategory.OST_IOSModelGroups);
-            FilteredElementCollector collector = new FilteredElementCollector(document);
+            ElementCategoryFilter filter = new(BuiltInCategory.OST_IOSModelGroups);
+            FilteredElementCollector collector = new(document);
             IList<Element> groups = collector.WherePasses(filter).WhereElementIsNotElementType().ToElements();
             return groups;
         }
@@ -24,18 +25,16 @@ namespace Arent3d.Architecture.Routing.AppBase
         public static bool GetHeightFasu(Document document, string nameFasu, ref double height)
         {
             bool brc = false;
-            ElementCategoryFilter filter = new ElementCategoryFilter(BuiltInCategory.OST_DuctAccessory);
-            FilteredElementCollector collector = new FilteredElementCollector(document);
+            ElementCategoryFilter filter = new(BuiltInCategory.OST_DuctAccessory);
+            FilteredElementCollector collector = new(document);
             IList<Element> ducts = collector.WherePasses(filter).WhereElementIsNotElementType().ToElements();
             foreach (var duct in ducts)
             {
-                if (duct.Name.IndexOf(nameFasu, 0) != -1)
-                {
-                    var locationPoint = (duct.Location as LocationPoint)!;
-                    height = locationPoint.Point.Z;
-                    brc = true;
-                    break;
-                }
+                if (duct.Name.IndexOf(nameFasu, 0, StringComparison.Ordinal) == -1) continue;
+                var locationPoint = (duct.Location as LocationPoint)!;
+                height = locationPoint.Point.Z;
+                brc = true;
+                break;
             }
 
             return brc;
