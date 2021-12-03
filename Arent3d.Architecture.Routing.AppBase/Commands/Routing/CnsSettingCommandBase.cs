0daf4db1ce5a0e3290fc5f4b16d140be3d88e91c
Nguyen Dinh Nhat <nhat.nguyen@rikai.technology>
2021-12-03 09:38:13 +0700

    nhatnguyen/show-message-box update messages, show message when select no conduit

--- Arent3d.Architecture.Routing.AppBase/Commands/Routing/CnsSettingCommandBase.cs
+++ Arent3d.Architecture.Routing.AppBase/Commands/Routing/CnsSettingCommandBase.cs
@@ -1,136 +1,128 @@
-﻿using System;
-using System.Linq;
-using System.Threading;
-using System.Windows.Forms;
-using Arent3d.Architecture.Routing.AppBase.Forms;
-using Arent3d.Architecture.Routing.AppBase.Selection;
-using Arent3d.Architecture.Routing.AppBase.ViewModel;
-using Arent3d.Architecture.Routing.Extensions;
-using Arent3d.Architecture.Routing.Storable;
-using Arent3d.Architecture.Routing.Storable.Model;
-using Arent3d.Revit;
-using Arent3d.Revit.I18n;
-using Arent3d.Revit.UI;
-using Autodesk.Revit.DB;
-using Autodesk.Revit.DB.Electrical;
-using Autodesk.Revit.UI;
-using ProgressBar = Arent3d.Revit.UI.Forms.ProgressBar;
+﻿using System.Linq ;
+using System.Threading ;
+using System.Windows.Forms ;
+using Arent3d.Architecture.Routing.AppBase.Forms ;
+using Arent3d.Architecture.Routing.AppBase.Selection ;
+using Arent3d.Architecture.Routing.AppBase.ViewModel ;
+using Arent3d.Architecture.Routing.Extensions ;
+using Arent3d.Architecture.Routing.Storable ;
+using Arent3d.Architecture.Routing.Storable.Model ;
+using Arent3d.Revit ;
+using Arent3d.Revit.I18n ;
+using Arent3d.Revit.UI ;
+using Autodesk.Revit.DB ;
+using Autodesk.Revit.DB.Electrical ;
+using Autodesk.Revit.UI ;
+using ProgressBar = Arent3d.Revit.UI.Forms.ProgressBar ;
 
 namespace Arent3d.Architecture.Routing.AppBase.Commands.Routing
 {
   public abstract class CnsSettingCommandBase : IExternalCommand
   {
-    protected UIDocument UiDocument { get; private set; } = null!;
+    protected UIDocument UiDocument { get ; private set ; } = null! ;
 
-    public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+    public Result Execute( ExternalCommandData commandData, ref string message, ElementSet elements )
     {
-      UiDocument = commandData.Application.ActiveUIDocument;
-      Document document = UiDocument.Document;
+      UiDocument = commandData.Application.ActiveUIDocument ;
+      Document document = UiDocument.Document ;
 
       // get data of Cns Category from snoop DB
-      CnsSettingStorable cnsStorables = document.GetCnsSettingStorable();
-      CnsSettingViewModel viewModel = new CnsSettingViewModel(cnsStorables);
-      var dialog = new CnsSettingDialog(viewModel);
+      CnsSettingStorable cnsStorables = document.GetCnsSettingStorable() ;
+      CnsSettingViewModel viewModel = new CnsSettingViewModel( cnsStorables ) ;
+      var dialog = new CnsSettingDialog( viewModel ) ;
 
-      dialog.ShowDialog();
-      if (dialog.DialogResult ?? false)
-      {
-        return document.Transaction("TransactionName.Commands.Routing.CnsSetting", _ =>
+      dialog.ShowDialog() ;
+      if ( dialog.DialogResult ?? false ) {
+        return document.Transaction( "TransactionName.Commands.Routing.CnsSetting", _ =>
         {
-          DataProcessBeforeSave(cnsStorables);
-          if (ShouldSaveCnsList(document, cnsStorables))
-          {
-            var tokenSource = new CancellationTokenSource();
-            using var progress = ProgressBar.ShowWithNewThread(tokenSource);
-            progress.Message = "Saving CNS Setting...";
-            using (progress?.Reserve(0.5))
-            {
-              SaveCnsList(document, cnsStorables);
+          DataProcessBeforeSave( cnsStorables ) ;
+          if ( ShouldSaveCnsList( document, cnsStorables ) ) {
+            var tokenSource = new CancellationTokenSource() ;
+            using var progress = ProgressBar.ShowWithNewThread( tokenSource ) ;
+            progress.Message = "Saving CNS Setting..." ;
+            using ( progress?.Reserve( 0.5 ) ) {
+              SaveCnsList( document, cnsStorables ) ;
             }
           }
 
-          if (cnsStorables.ConduitType == CnsSettingStorable.ConstructionItemType.Conduit)
-          {
-            try
-            {
-              MessageBox.Show("Dialog.Electrical.SelectConduit.Message".GetAppStringByKeyOrDefault("Select multiple conduits by dragging."),
-                "Dialog.Electrical.SelectConduit.Title".GetAppStringByKeyOrDefault("Message"),
-                MessageBoxButtons.OK);
+          if ( cnsStorables.ConduitType == CnsSettingStorable.ConstructionItemType.Conduit ) {
+            try {
               var selectedElements = UiDocument.Selection
-                .PickElementsByRectangle(ConduitSelectionFilter.Instance, "ドラックで複数コンジットを選択して下さい。")
-                .Where(p => p is FamilyInstance or Conduit);
+                .PickElementsByRectangle( ConduitSelectionFilter.Instance, "ドラックで複数コンジットを選択して下さい。" )
+                .Where( p => p is FamilyInstance or Conduit ) ;
 
-              var categoryName = cnsStorables.CnsSettingData[cnsStorables.SelectedIndex].CategoryName;
-              foreach (var element in selectedElements)
-              {
-                SetConstructionItemForConduit(element, categoryName);
+              var conduits = selectedElements.ToList() ;
+              if ( conduits.Any() ) {
+                var categoryName = cnsStorables.CnsSettingData[ cnsStorables.SelectedIndex ].CategoryName ;
+                foreach ( var conduit in conduits ) {
+                  SetConstructionItemForConduit( conduit, categoryName ) ;
+                }
+
+                MessageBox.Show( "Dialog.Electrical.SetConduitProperty.Success".GetAppStringByKeyOrDefault( "Success" ),
+                  "Dialog.Electrical.SetConduitProperty.Title".GetAppStringByKeyOrDefault(
+                    "Construction item addition result" ), MessageBoxButtons.OK ) ;
               }
-
-              MessageBox.Show("Dialog.Electrical.SetConduitProperty.Success".GetAppStringByKeyOrDefault("Success"),
-                "Dialog.Electrical.SetConduitProperty.Title".GetAppStringByKeyOrDefault("Application result"),
-                MessageBoxButtons.OK);
+              else {
+                MessageBox.Show(
+                  "Dialog.Electrical.SelectConduit.Message".GetAppStringByKeyOrDefault( "Please select a range." ),
+                  "Dialog.Electrical.SelectConduit.Title".GetAppStringByKeyOrDefault( "Message" ),
+                  MessageBoxButtons.OK ) ;
+              }
             }
-            catch
-            {
-              MessageBox.Show("Dialog.Electrical.SetConduitProperty.Failure".GetAppStringByKeyOrDefault("Failure"),
-                "Dialog.Electrical.SetConduitProperty.Title".GetAppStringByKeyOrDefault("Application result"),
-                MessageBoxButtons.OK);
-              return Result.Cancelled;
+            catch {
+              MessageBox.Show( "Dialog.Electrical.SetConduitProperty.Failure".GetAppStringByKeyOrDefault( "Failed" ),
+                "Dialog.Electrical.SetConduitProperty.Title".GetAppStringByKeyOrDefault(
+                  "Construction item addition result" ), MessageBoxButtons.OK ) ;
+              return Result.Cancelled ;
             }
           }
 
-          return Result.Succeeded;
-        });
+          return Result.Succeeded ;
+        } ) ;
       }
-      else
-      {
-        return Result.Cancelled;
+      else {
+        return Result.Cancelled ;
       }
     }
 
-    private static void SaveCnsList(Document document, CnsSettingStorable list)
+    private static void SaveCnsList( Document document, CnsSettingStorable list )
     {
-      list.Save();
+      list.Save() ;
     }
 
-    private static bool ShouldSaveCnsList(Document document, CnsSettingStorable newSettings)
+    private static bool ShouldSaveCnsList( Document document, CnsSettingStorable newSettings )
     {
       var old = document.GetAllStorables<CnsSettingStorable>()
-        .FirstOrDefault(); // generates new instance from document
-      return old == null || !newSettings.Equals(old);
+        .FirstOrDefault() ; // generates new instance from document
+      return old == null || ! newSettings.Equals( old ) ;
     }
 
-    private static void DataProcessBeforeSave(CnsSettingStorable cnsSettings)
+    private static void DataProcessBeforeSave( CnsSettingStorable cnsSettings )
     {
-      bool hadUpdating = false;
+      bool hadUpdating = false ;
       // Remove empty row
-      foreach (var item in cnsSettings.CnsSettingData.ToList())
-      {
-        if (string.IsNullOrWhiteSpace(item.CategoryName.Trim()))
-        {
-          cnsSettings.CnsSettingData.Remove(item);
-          hadUpdating = true;
+      foreach ( var item in cnsSettings.CnsSettingData.ToList() ) {
+        if ( string.IsNullOrWhiteSpace( item.CategoryName.Trim() ) ) {
+          cnsSettings.CnsSettingData.Remove( item ) ;
+          hadUpdating = true ;
         }
       }
 
-      if (cnsSettings.CnsSettingData.Count == 0)
-      {
+      if ( cnsSettings.CnsSettingData.Count == 0 ) {
         // Add default value if list empty
-        cnsSettings.CnsSettingData.Add(new CnsSettingModel(sequence: 1, categoryName: "未設定"));
+        cnsSettings.CnsSettingData.Add( new CnsSettingModel( sequence: 1, categoryName: "未設定" ) ) ;
       }
-      else if (hadUpdating)
-      {
+      else if ( hadUpdating ) {
         // Set sequence if list was changed
-        for (int i = 0; i < cnsSettings.CnsSettingData.Count; i++)
-        {
-          cnsSettings.CnsSettingData[i].Sequence = i + 1;
+        for ( int i = 0 ; i < cnsSettings.CnsSettingData.Count ; i++ ) {
+          cnsSettings.CnsSettingData[ i ].Sequence = i + 1 ;
         }
       }
     }
 
-    private static void SetConstructionItemForConduit(Element element, string categoryName)
+    private static void SetConstructionItemForConduit( Element element, string categoryName )
     {
-      element.SetProperty(RoutingFamilyLinkedParameter.ConstructionItem, categoryName);
+      element.SetProperty( RoutingFamilyLinkedParameter.ConstructionItem, categoryName ) ;
     }
   }
 }
