ef0685eb9a9d6e728fce81513c65a8068f97cf0a
thailtn <thailtn@rikkeisoft.com>
2021-12-13 21:50:19 +0700

    fix close form event

--- Arent3d.Architecture.Routing.AppBase/Forms/CnsSettingDialog.xaml.cs
+++ Arent3d.Architecture.Routing.AppBase/Forms/CnsSettingDialog.xaml.cs
@@ -3,9 +3,8 @@ using System.Linq ;
 using Arent3d.Architecture.Routing.AppBase.ViewModel;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Input ;
-using System.Windows.Threading ;
 using Arent3d.Architecture.Routing.Storable.Model ;
+using System.ComponentModel;
 
 namespace Arent3d.Architecture.Routing.AppBase.Forms
 {
@@ -19,7 +18,7 @@ namespace Arent3d.Architecture.Routing.AppBase.Forms
         
         private void Update_Click(object sender, RoutedEventArgs e)
         {
-            if( grdCategories.SelectedItem == null) return;
+            if( grdCategories.SelectedItem == null) return ;
             var selectedItem = ( (CnsSettingModel) grdCategories.SelectedItem ) ;
             if ( selectedItem.CategoryName == "未設定" ) return ;
             grdCategories.IsReadOnly = false ;
@@ -30,21 +29,28 @@ namespace Arent3d.Architecture.Routing.AppBase.Forms
         private void Close_Click(object sender, RoutedEventArgs e)
         {
             SetDuplicateName() ;
-            DialogResult = true;
-            Close();
+            DialogResult = true ;
+            Close() ;
+        }
+        
+        private void DataWindow_Closing(object sender, CancelEventArgs e)
+        {
+            SetDuplicateName() ;
+            DialogResult = true ;
         }
         
         private void GrdCategories_OnCellEditEnding( object sender, DataGridCellEditEndingEventArgs e )
         {
-           var isDuplicateName = grdCategories.ItemsSource.Cast<CnsSettingModel>().ToList()
-                .GroupBy(x => x.CategoryName).Any(g => g.Count() > 1);
-            if ( isDuplicateName ) {
-                MessageBox.Show( "工事項目名称がすでに存在しています。再度工事項目名称を入力してください。" ) ;
-                e.Cancel = true ;
-                return;
-            }
+           var isDuplicateName = grdCategories.ItemsSource.Cast<CnsSettingModel>()
+               .Where(x=> !string.IsNullOrEmpty(x.CategoryName)).GroupBy(x => x.CategoryName).Any(g => g.Count() > 1);
+            
+           if ( isDuplicateName ) {
+               MessageBox.Show( "工事項目名称がすでに存在しています。再度工事項目名称を入力してください。" ) ;
+               e.Cancel = true ;
+               return ;
+           }
 
-            grdCategories.IsReadOnly = true ;
+           grdCategories.IsReadOnly = true ;
         }
 
         private void ButtonAddNewRow_OnClick( object sender, RoutedEventArgs e )
@@ -54,16 +60,15 @@ namespace Arent3d.Architecture.Routing.AppBase.Forms
 
         private void SetDuplicateName()
         {
-            var duplicateCategoryName = grdCategories.ItemsSource.Cast<CnsSettingModel>().GroupBy( x => x.CategoryName )
+            var duplicateCategoryName = grdCategories.ItemsSource.Cast<CnsSettingModel>().Where(x=> !string.IsNullOrEmpty(x.CategoryName)).GroupBy( x => x.CategoryName )
                 .Where( g => g.Count() > 1 ).ToList() ;
            
-            if ( !duplicateCategoryName.Any()) return ;
+            if ( !duplicateCategoryName.Any() ) return ;
             var sequences = duplicateCategoryName.FirstOrDefault()!.Select(c=>c.Sequence).ToList() ;
             for ( var i = 0 ; i < sequences.Count() ; i++ ) {
-              if( i!=0 )
-                grdCategories.ItemsSource.Cast<CnsSettingModel>().ToList()[sequences[ i ] - 1].CategoryName = string.Empty;
+              if ( i != 0 )
+                grdCategories.ItemsSource.Cast<CnsSettingModel>().ToList()[sequences[ i ] - 1].CategoryName = string.Empty ;
             }
-           
         }
     }
 }
