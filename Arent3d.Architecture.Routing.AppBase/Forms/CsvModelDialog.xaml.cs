deea87a61dea8900fdf64493bef371ae726b91ad
Nguyen Dinh Nhat <nhat.nguyen@rikai.technology>
2021-12-14 16:53:04 +0700

    nhatnguyen/read-csvfiles-by-folder add read all csv files (include xlsx file) by select folder function

--- Arent3d.Architecture.Routing.AppBase/Forms/CsvModelDialog.xaml.cs
+++ Arent3d.Architecture.Routing.AppBase/Forms/CsvModelDialog.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System ;
 using System.Collections.Generic ;
+using System.Globalization ;
 using System.IO ;
 using System.Linq ;
 using System.Text ;
@@ -8,9 +9,11 @@ using System.Windows.Forms ;
 using Arent3d.Architecture.Routing.Extensions ;
 using Arent3d.Architecture.Routing.Storable ;
 using Arent3d.Architecture.Routing.Storable.Model ;
-using Arent3d.Revit ;
 using Autodesk.Revit.DB ;
+using NPOI.SS.UserModel ;
+using NPOI.XSSF.UserModel ;
 using MessageBox = System.Windows.MessageBox ;
+using CellType = NPOI.SS.UserModel.CellType ;
 
 namespace Arent3d.Architecture.Routing.AppBase.Forms
 {
@@ -24,6 +27,8 @@ namespace Arent3d.Architecture.Routing.AppBase.Forms
     private List<HiroiSetCdMasterModel> _allHiroiSetCdMasterNormalModels ;
     private List<HiroiSetCdMasterModel> _allHiroiSetCdMasterEcoModels ;
     private List<HiroiMasterModel> _allHiroiMasterModels ;
+    private List<CeedModel> _ceeDModelData ;
+    private bool _isLoadAll ;
 
     public CsvModelDialog( Document document )
     {
@@ -37,6 +42,7 @@ namespace Arent3d.Architecture.Routing.AppBase.Forms
       _allHiroiSetCdMasterNormalModels = new List<HiroiSetCdMasterModel>() ;
       _allHiroiSetCdMasterEcoModels = new List<HiroiSetCdMasterModel>() ;
       _allHiroiMasterModels = new List<HiroiMasterModel>() ;
+      _ceeDModelData = new List<CeedModel>() ;
     }
 
     private void Button_Save( object sender, RoutedEventArgs e )
@@ -67,6 +73,20 @@ namespace Arent3d.Architecture.Routing.AppBase.Forms
         catch ( Autodesk.Revit.Exceptions.OperationCanceledException ) {
           MessageBox.Show( "Save CSV Files Failed.", "Error Message" ) ;
           DialogResult = false ;
+        }
+      }
+      CeedStorable ceeDStorable = _document.GetCeeDStorable() ;
+      {
+        if ( _ceeDModelData.Any() ) {
+          ceeDStorable.CeedModelData = _ceeDModelData ;
+          try {
+            using Transaction t = new Transaction( _document, "Save CeeD data" ) ;
+            t.Start() ;
+            ceeDStorable.Save() ;
+            t.Commit() ;
+          }
+          catch ( Autodesk.Revit.Exceptions.OperationCanceledException ) {
+          }
         }
       }
 
@@ -232,7 +252,9 @@ namespace Arent3d.Architecture.Routing.AppBase.Forms
           MessageBox.Show( "Incorrect file format.", "Error Message" ) ;
         }
         else {
-          MessageBox.Show( "Load file successful.", "Result Message" ) ;
+          if ( ! _isLoadAll ) {
+            MessageBox.Show( "Load file successful.", "Result Message" ) ;
+          }
         }
       }
       catch ( Exception ) {
@@ -249,6 +271,163 @@ namespace Arent3d.Architecture.Routing.AppBase.Forms
       HiroiSetCdMasterNormal,
       HiroiSetCdMasterEco,
       HiroiMaster
+    }
+
+    private void BtnLoadAll_OnClick( object sender, RoutedEventArgs e )
+    {
+      var dialog = new FolderBrowserDialog() ;
+      dialog.ShowNewFolderButton = false ;
+      dialog.ShowDialog() ;
+      string[] fileNames = new[]
+      {
+        "hiroimaster.csv", "hiroisetcdmaster_normal.csv", "hiroisetcdmaster_eco.csv", "hiroisetmaster_eco.csv",
+        "hiroisetmaster_normal.csv", "電線管一覧.csv", "電線・ケーブル一覧.csv", "【CeeD】セットコード一覧表.xlsx"
+      } ;
+      _isLoadAll = true ;
+      foreach ( var fileName in fileNames ) {
+        var path = Path.Combine( dialog.SelectedPath, fileName ) ;
+        if ( File.Exists( path ) ) {
+          switch ( fileName ) {
+            case "hiroimaster.csv" :
+              _allHiroiMasterModels = new List<HiroiMasterModel>() ;
+              GetData( path, 0, ModelName.HiroiMaster ) ;
+              break ;
+            case "hiroisetcdmaster_normal.csv" :
+              _allHiroiSetCdMasterNormalModels = new List<HiroiSetCdMasterModel>() ;
+              GetData( path, 0, ModelName.HiroiSetCdMasterNormal ) ;
+              break ;
+            case "hiroisetcdmaster_eco.csv" :
+              _allHiroiSetCdMasterEcoModels = new List<HiroiSetCdMasterModel>() ;
+              GetData( path, 0, ModelName.HiroiSetCdMasterEco ) ;
+              break ;
+            case "hiroisetmaster_eco.csv" :
+              _allHiroiSetMasterEcoModels = new List<HiroiSetMasterModel>() ;
+              GetData( path, 0, ModelName.HiroiSetMasterEco ) ;
+              break ;
+            case "hiroisetmaster_normal.csv" :
+              _allHiroiSetMasterNormalModels = new List<HiroiSetMasterModel>() ;
+              GetData( path, 0, ModelName.HiroiSetMasterNormal ) ;
+              break ;
+            case "電線管一覧.csv" :
+              _allConduitModels = new List<ConduitsModel>() ;
+              GetData( path, 2, ModelName.Conduits ) ;
+              break ;
+            case "電線・ケーブル一覧.csv" :
+              _allWiresAndCablesModels = new List<WiresAndCablesModel>() ;
+              GetData( path, 2, ModelName.WiresAndCables ) ;
+              break ;
+            case "【CeeD】セットコード一覧表.xlsx" :
+              _ceeDModelData = GetAllCeeDModelNumber( path ) ;
+              break ;
+          }
+        }
+      }
+
+      MessageBox.Show( "Load files successful.", "Result Message" ) ;
+      _isLoadAll = false ;
+    }
+
+    private static List<CeedModel> GetAllCeeDModelNumber( string path )
+    {
+      List<CeedModel> ceedModelData = new List<CeedModel>() ;
+
+      try {
+        FileStream fs = new FileStream( path, FileMode.Open, FileAccess.Read ) ;
+        XSSFWorkbook wb = new XSSFWorkbook( fs ) ;
+        ISheet workSheet = wb.NumberOfSheets < 2 ? wb.GetSheetAt( wb.ActiveSheetIndex ) : wb.GetSheetAt( 1 ) ;
+        const int startRow = 7 ;
+        var endRow = workSheet.LastRowNum ;
+        for ( var i = startRow ; i <= endRow ; i++ ) {
+          List<string> ceeDModelNumbers = new List<string>() ;
+          List<string> ceeDSetCodes = new List<string>() ;
+          List<string> modelNumbers = new List<string>() ;
+          string generalDisplayDeviceSymbols = string.Empty ;
+          string floorPlanSymbol = string.Empty ;
+          string ceeDName = string.Empty ;
+
+          var record = workSheet.GetRow( i ).GetCell( 3 ) ;
+          if ( record == null || record.CellStyle.IsHidden ) continue ;
+          var name = GetCellValue( record ) ;
+          if ( string.IsNullOrEmpty( name ) ) continue ;
+          var firstIndexGroup = i ;
+          var nextName = GetCellValue( record ) ;
+          do {
+            i++ ;
+            if ( i > endRow ) break ;
+            name = nextName ;
+            record = workSheet.GetRow( i ).GetCell( 3 ) ;
+            if ( record == null ) break ;
+            nextName = GetCellValue( record ) ;
+          } while ( ! ( string.IsNullOrEmpty( name ) && ! string.IsNullOrEmpty( nextName ) ) ) ;
+
+          var lastIndexGroup = i ;
+          for ( var j = firstIndexGroup ; j < lastIndexGroup ; j++ ) {
+            var ceeDSetCodeCell = workSheet.GetRow( j ).GetCell( 0 ) ;
+            var ceeDSetCode = GetCellValue( ceeDSetCodeCell ) ;
+            if ( ! string.IsNullOrEmpty( ceeDSetCode ) ) ceeDSetCodes.Add( ceeDSetCode ) ;
+
+            var ceeDModelNumberCell = workSheet.GetRow( j ).GetCell( 1 ) ;
+            var ceeDModelNumber = GetCellValue( ceeDModelNumberCell ) ;
+            if ( ! string.IsNullOrEmpty( ceeDModelNumber ) ) ceeDModelNumbers.Add( ceeDModelNumber ) ;
+
+            var generalDisplayDeviceSymbolCell = workSheet.GetRow( j ).GetCell( 2 ) ;
+            var generalDisplayDeviceSymbol = GetCellValue( generalDisplayDeviceSymbolCell ) ;
+            if ( ! string.IsNullOrEmpty( generalDisplayDeviceSymbol ) && ! generalDisplayDeviceSymbol.Contains( "．" ) )
+              generalDisplayDeviceSymbols = generalDisplayDeviceSymbol ;
+
+            var ceeDNameCell = workSheet.GetRow( j ).GetCell( 3 ) ;
+            var modelName = GetCellValue( ceeDNameCell ) ;
+            if ( ! string.IsNullOrEmpty( modelName ) ) ceeDName = modelName ;
+
+            var modelNumberCell = workSheet.GetRow( j ).GetCell( 4 ) ;
+            var modelNumber = GetCellValue( modelNumberCell ) ;
+            if ( ! string.IsNullOrEmpty( modelNumber ) ) modelNumbers.Add( modelNumber ) ;
+
+            var symbolCell = workSheet.GetRow( j ).GetCell( 5 ) ;
+            var symbol = GetCellValue( symbolCell ) ;
+            if ( ! string.IsNullOrEmpty( symbol ) && ! symbol.Contains( "又は" ) ) floorPlanSymbol = symbol ;
+          }
+
+          var strModelNumbers = modelNumbers.Any() ? string.Join( "\n", modelNumbers ) : string.Empty ;
+          if ( ! ceeDModelNumbers.Any() ) {
+            CeedModel ceeDModel = new CeedModel( string.Empty, string.Empty, generalDisplayDeviceSymbols,
+              strModelNumbers, floorPlanSymbol, ceeDName ) ;
+            ceedModelData.Add( ceeDModel ) ;
+          }
+          else {
+            for ( var k = 0 ; k < ceeDModelNumbers.Count ; k++ ) {
+              var ceeDSetCode = ceeDSetCodes.Any() ? ceeDSetCodes[ k ] : string.Empty ;
+              CeedModel ceeDModel = new CeedModel( ceeDModelNumbers[ k ], ceeDSetCode, generalDisplayDeviceSymbols,
+                strModelNumbers, floorPlanSymbol, ceeDName ) ;
+              ceedModelData.Add( ceeDModel ) ;
+            }
+          }
+
+          i-- ;
+        }
+      }
+      catch ( Exception ) {
+        return new List<CeedModel>() ;
+      }
+
+      return ceedModelData ;
+    }
+
+    private static string GetCellValue( ICell? cell )
+    {
+      string cellValue = string.Empty ;
+      if ( cell == null ) return cellValue ;
+      cellValue = cell.CellType switch
+      {
+        CellType.Blank => string.Empty,
+        CellType.Numeric => DateUtil.IsCellDateFormatted( cell )
+          ? cell.DateCellValue.ToString( CultureInfo.InvariantCulture )
+          : cell.NumericCellValue.ToString( CultureInfo.InvariantCulture ),
+        CellType.String => cell.StringCellValue,
+        _ => cellValue
+      } ;
+
+      return cellValue ;
     }
   }
 }
