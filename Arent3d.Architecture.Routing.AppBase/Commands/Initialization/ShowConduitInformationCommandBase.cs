c1f0477672fff78461dce2ae16f3f0c07a54496e
Nguyen Dinh Nhat <nhat.nguyen@rikai.technology>
2021-12-20 17:35:04 +0700

    nhatnguyen/conduit-information update mapping data base 10-119

--- Arent3d.Architecture.Routing.AppBase/Commands/Initialization/ShowConduitInformationCommandBase.cs
+++ Arent3d.Architecture.Routing.AppBase/Commands/Initialization/ShowConduitInformationCommandBase.cs
@@ -22,7 +22,6 @@ namespace Arent3d.Architecture.Routing.AppBase.Commands.Initialization
     {
       var doc = commandData.Application.ActiveUIDocument.Document ;
       var uiDoc = commandData.Application.ActiveUIDocument ;
-      var wiresAndCablesModelData = doc.GetCsvStorable().WiresAndCablesModelData ;
       var conduitsModelData = doc.GetCsvStorable().ConduitsModelData ;
       var hiroiSetMasterNormalModelData = doc.GetCsvStorable().HiroiSetMasterNormalModelData ;
       var hiroiMasterModelData = doc.GetCsvStorable().HiroiMasterModelData ;
@@ -30,84 +29,85 @@ namespace Arent3d.Architecture.Routing.AppBase.Commands.Initialization
       var ceedStorable = doc.GetAllStorables<CeedStorable>().FirstOrDefault() ;
       ObservableCollection<ConduitInformationModel> conduitInformationModels =
         new ObservableCollection<ConduitInformationModel>() ;
-      var detailSymbolStorable = doc.GetAllStorables<DetailSymbolStorable>().FirstOrDefault() ?? doc.GetDetailSymbolStorable() ;    
+      var detailSymbolStorable =
+        doc.GetAllStorables<DetailSymbolStorable>().FirstOrDefault() ?? doc.GetDetailSymbolStorable() ;
       try {
         var pickedObjects = uiDoc.Selection
           .PickElementsByRectangle( ConduitSelectionFilter.Instance, "ドラックで複数コンジットを選択して下さい。" )
           .Where( p => p is FamilyInstance or Conduit ) ;
         foreach ( var element in pickedObjects ) {
           string floor = doc.GetElementById<Level>( element.GetLevelId() )?.Name ?? string.Empty ;
-          string costructionItem = element.LookupParameter( "Construction Item" ).AsValueString() ;
+          string constructionItem = element.LookupParameter( "Construction Item" ).AsValueString() ;
           var existSymbolDetail =
             detailSymbolStorable.DetailSymbolModelData.FirstOrDefault( x => element.Id.ToString() == x.ConduitId ) ;
-          if(existSymbolDetail!=null) {
-            if ( ceedStorable != null ) {
-              var ceedModel = ceedStorable.CeedModelData.FirstOrDefault( x => x.CeeDSetCode == existSymbolDetail.Code ) ;
-              if ( ceedModel != null ) {
-                var hiroiCdModel =
-                  hiroiSetCdMasterNormalModelData.FirstOrDefault( x => x.SetCode == ceedModel.CeeDSetCode ) ;
-                var hiroiSetModels =
-                  hiroiSetMasterNormalModelData.Where( x => x.ParentPartName == ceedModel.CeeDSetCode ) ;
-                foreach ( var item in hiroiSetModels ) {
-                  List<string> listMaterialCode = new List<string>() ;
-                  if ( ! string.IsNullOrWhiteSpace( item.MaterialCode1 ) ) {
-                    listMaterialCode.Add( item.MaterialCode1 ) ;
-                  }
+          if ( existSymbolDetail != null && ceedStorable != null ) {
+            var ceedModel = ceedStorable.CeedModelData.FirstOrDefault( x => x.CeeDSetCode == existSymbolDetail.Code ) ;
+            if ( ceedModel != null ) {
+              var hiroiCdModel =
+                hiroiSetCdMasterNormalModelData.FirstOrDefault( x => x.SetCode == ceedModel.CeeDSetCode ) ;
+              var hiroiSetModels =
+                hiroiSetMasterNormalModelData.Where(
+                  x => x.ParentPartModelNumber.Contains( ceedModel.CeeDModelNumber ) ) ;
+              foreach ( var item in hiroiSetModels ) {
+                List<string> listMaterialCode = new List<string>() ;
+                if ( ! string.IsNullOrWhiteSpace( item.MaterialCode1 ) ) {
+                  listMaterialCode.Add( item.MaterialCode1 ) ;
+                }
 
-                  if ( ! string.IsNullOrWhiteSpace( item.MaterialCode2 ) ) {
-                    listMaterialCode.Add( item.MaterialCode2 ) ;
-                  }
+                if ( ! string.IsNullOrWhiteSpace( item.MaterialCode2 ) ) {
+                  listMaterialCode.Add( item.MaterialCode2 ) ;
+                }
 
-                  if ( ! string.IsNullOrWhiteSpace( item.MaterialCode3 ) ) {
-                    listMaterialCode.Add( item.MaterialCode3 ) ;
-                  }
+                if ( ! string.IsNullOrWhiteSpace( item.MaterialCode3 ) ) {
+                  listMaterialCode.Add( item.MaterialCode3 ) ;
+                }
 
-                  if ( ! string.IsNullOrWhiteSpace( item.MaterialCode4 ) ) {
-                    listMaterialCode.Add( item.MaterialCode4 ) ;
-                  }
+                if ( ! string.IsNullOrWhiteSpace( item.MaterialCode4 ) ) {
+                  listMaterialCode.Add( item.MaterialCode4 ) ;
+                }
 
-                  if ( ! string.IsNullOrWhiteSpace( item.MaterialCode5 ) ) {
-                    listMaterialCode.Add( item.MaterialCode5 ) ;
-                  }
+                if ( ! string.IsNullOrWhiteSpace( item.MaterialCode5 ) ) {
+                  listMaterialCode.Add( item.MaterialCode5 ) ;
+                }
 
-                  if ( ! string.IsNullOrWhiteSpace( item.MaterialCode6 ) ) {
-                    listMaterialCode.Add( item.MaterialCode6 ) ;
-                  }
+                if ( ! string.IsNullOrWhiteSpace( item.MaterialCode6 ) ) {
+                  listMaterialCode.Add( item.MaterialCode6 ) ;
+                }
 
-                  if ( ! string.IsNullOrWhiteSpace( item.MaterialCode7 ) ) {
-                    listMaterialCode.Add( item.MaterialCode7 ) ;
-                  }
+                if ( ! string.IsNullOrWhiteSpace( item.MaterialCode7 ) ) {
+                  listMaterialCode.Add( item.MaterialCode7 ) ;
+                }
 
-                  if ( ! string.IsNullOrWhiteSpace( item.MaterialCode8 ) ) {
-                    listMaterialCode.Add( item.MaterialCode8 ) ;
-                  }
+                if ( ! string.IsNullOrWhiteSpace( item.MaterialCode8 ) ) {
+                  listMaterialCode.Add( item.MaterialCode8 ) ;
+                }
 
-                  if ( listMaterialCode.Any() ) {
-                    var masterModels = hiroiMasterModelData.Where( x => listMaterialCode.Contains( x.Buzaicd ) ) ;
-                    foreach ( var master in masterModels ) {
-                      var conduitModels =
-                        conduitsModelData.Where( x => x.PipingType == master.Type && x.Size == master.Size1 ) ;
-                      conduitInformationModels.Add( new ConduitInformationModel( false, floor,
-                        existSymbolDetail.DetailSymbol, master.Type, master.Hinmei, master.Size1, master.Size2,
-                        string.Empty, string.Empty, string.Empty, master.Type, master.Size1,
-                        conduitModels.Count().ToString(), hiroiCdModel?.ConstructionClassification,
-                        conduitModels.FirstOrDefault().Classification, costructionItem, costructionItem, "" ) ) ;
-                    }
+                if ( listMaterialCode.Any() ) {
+                  var masterModels = hiroiMasterModelData.Where( x => listMaterialCode.Contains( x.Buzaicd ) ) ;
+                  foreach ( var master in masterModels ) {
+                    var conduitModels = conduitsModelData
+                      .Where( x => x.PipingType == master.Type && x.Size == master.Size1 ).ToList() ;
+                    conduitInformationModels.Add( new ConduitInformationModel( false, floor,
+                      existSymbolDetail.DetailSymbol, master.Type, master.Hinmei, master.Size1, master.Size2,
+                      string.Empty, string.Empty, string.Empty, master.Type, master.Size1,
+                      conduitModels.Count().ToString(), hiroiCdModel?.ConstructionClassification,
+                      conduitModels.FirstOrDefault()?.Classification ?? "", constructionItem, constructionItem, "" ) ) ;
                   }
                 }
               }
             }
-            }
+          }
         }
-        conduitInformationModels =new ObservableCollection<ConduitInformationModel>( conduitInformationModels.GroupBy( x => x.WireType )
-          .Select( g =>
+
+        conduitInformationModels = new ObservableCollection<ConduitInformationModel>( conduitInformationModels
+          .GroupBy( x => new { x?.DetailSymbol, x?.WireType } ).Select( g =>
           {
             g.First().Quantity = g.Count() ;
             g.First().Remark = $"x{g.Count()}" ;
             return g.First() ;
-          } ) );
+          } ).OrderBy( y => y.DetailSymbol ) ) ;
       }
-      catch{
+      catch {
         return Result.Cancelled ;
       }
 
@@ -130,7 +130,8 @@ namespace Arent3d.Architecture.Routing.AppBase.Commands.Initialization
               var noteWidth = 0.4 ;
               TextNoteOptions opts = new(defaultTextTypeId) ;
               var txtPosition = new XYZ( originX, originY, heightOfConnector ) ;
-              TextNote.Create( doc, doc.ActiveView.Id, txtPosition, noteWidth, GenerateTextTable( viewModel, level.Name ), opts ) ;
+              TextNote.Create( doc, doc.ActiveView.Id, txtPosition, noteWidth,
+                GenerateTextTable( viewModel, level.Name ), opts ) ;
               viewModel.IsCreateSchedule = false ;
             }
 
@@ -153,7 +154,10 @@ namespace Arent3d.Architecture.Routing.AppBase.Commands.Initialization
       result += $"{line}\r{level}階平面图" ;
       foreach ( var group in conduitInformationDictionary ) {
         result += $"\r{line}\r{group.Key}" ;
-        result = @group.Value.Aggregate( result, ( current, item ) => current + $"\r{line}\r{item.WireType + item.WireSize}\t-{item.WireStrip}\tX{item.Quantity}\t({item.PipingType + item.PipingSize})\t{item.Remark}" ) ;
+        result = @group.Value.Aggregate( result,
+          ( current, item ) =>
+            current +
+            $"\r{line}\r{item.WireType + item.WireSize}\t-{item.WireStrip}\tX{item.Quantity}\t({item.PipingType + item.PipingSize})\t{item.Remark}" ) ;
       }
 
       result += $"\r{line}" ;
