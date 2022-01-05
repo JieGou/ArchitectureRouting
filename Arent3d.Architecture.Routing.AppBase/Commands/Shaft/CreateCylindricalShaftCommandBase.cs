0b2c9591cb9744468b770e74ebaf9765dbb1ecad
MathVip <quangnhu95@gmail.com>
2022-01-05 22:02:41 +0700

    Format code wrap and changed field name

--- Arent3d.Architecture.Routing.AppBase/Commands/Shaft/CreateCylindricalShaftCommandBase.cs
+++ Arent3d.Architecture.Routing.AppBase/Commands/Shaft/CreateCylindricalShaftCommandBase.cs
@@ -1,129 +1,126 @@
 ﻿using Autodesk.Revit.DB ;
-using Arent3d.Architecture.Routing.AppBase.UI.ExternalGraphics;
-using Arent3d.Architecture.Routing.Extensions;
-using Arent3d.Architecture.Routing.Storable;
-using Autodesk.Revit.ApplicationServices;
-using Autodesk.Revit.UI;
-using Autodesk.Revit.UI.Selection;
-using System;
-using System.Linq;
+using Arent3d.Architecture.Routing.AppBase.UI.ExternalGraphics ;
+using Arent3d.Architecture.Routing.Extensions ;
+using Arent3d.Architecture.Routing.Storable ;
+using Autodesk.Revit.ApplicationServices ;
+using Autodesk.Revit.UI ;
+using Autodesk.Revit.UI.Selection ;
+using System ;
+using System.Linq ;
 using Arent3d.Revit ;
 
 namespace Arent3d.Architecture.Routing.AppBase.Commands.Shaft
 {
-  public class CreateCylindricalShaftCommandBase : IExternalCommand
-  {
-    private const double rotateAngle = Math.PI / 3 ;
-    private double lengthEndOne = 6000 ;
-    private double lengthEndTwo = 6000 ;
-    public Result Execute( ExternalCommandData commandData, ref string message, ElementSet elements )
-    {
-      UIApplication uiApp = commandData.Application ;
-      UIDocument uiDocument = uiApp.ActiveUIDocument ;
-      Document document = uiDocument.Document ;
-      Application app = uiApp.Application ;
-      var selection = uiDocument.Selection ;
-      bool checkEx = false ;
-      try {
-        // Pick first point 
-        XYZ firstPoint = selection.PickPoint( "Pick first point" ) ;
-        XYZ? secondPoint = null ;
-        // This is the object to render the guide line
-        CircleExternal circleExternal = new CircleExternal( uiApp ) ;
-        try {
-          // Add first point to list picked points
-          circleExternal.PickedPoints.Add( firstPoint ) ;
-          // Assign first point
-          circleExternal.DrawingServer.BasePoint = firstPoint ;
-          // Render the guide line
-          circleExternal.DrawExternal() ;
+	public class CreateCylindricalShaftCommandBase : IExternalCommand
+	{
+		private const double RotateAngle = Math.PI / 3 ;
+		private const double LengthEndOne = 6000 ;
+		private const double LengthEndTwo = 6000 ;
 
-          // Pick next point 
-          secondPoint = selection.PickPoint( "Pick next point" ) ;
-        }
-        catch ( Exception ) {
-          checkEx = true ;
-        }
-        finally {
-          // End to render guide line
-          circleExternal.Dispose() ;
-        }
+		public Result Execute( ExternalCommandData commandData, ref string message, ElementSet elements )
+		{
+			UIApplication uiApp = commandData.Application ;
+			UIDocument uiDocument = uiApp.ActiveUIDocument ;
+			Document document = uiDocument.Document ;
+			Application app = uiApp.Application ;
+			var selection = uiDocument.Selection ;
+			bool checkEx = false ;
+			try {
+				// Pick first point 
+				XYZ firstPoint = selection.PickPoint( "Pick first point" ) ;
+				XYZ? secondPoint = null ;
+				// This is the object to render the guide line
+				CircleExternal circleExternal = new CircleExternal( uiApp ) ;
+				try {
+					// Add first point to list picked points
+					circleExternal.PickedPoints.Add( firstPoint ) ;
+					// Assign first point
+					circleExternal.DrawingServer.BasePoint = firstPoint ;
+					// Render the guide line
+					circleExternal.DrawExternal() ;
 
-        // If second point is null. Return failed to end command
-        if ( secondPoint == null || checkEx ) return Result.Failed ;
-        
-        // Get height setting
-        HeightSettingStorable heightSetting = document.GetHeightSettingStorable() ;
-        var levels = heightSetting.Levels.OrderBy( x => x.Elevation ).ToList() ;
-        // Get lowest and highest level
-        Level? lowestLevel = levels.FirstOrDefault() ;
-        Level? highestLevel = levels.LastOrDefault() ;
-        if ( lowestLevel == null && highestLevel == null ) return Result.Failed ;
+					// Pick next point 
+					secondPoint = selection.PickPoint( "Pick next point" ) ;
+				}
+				catch ( Exception ) {
+					checkEx = true ;
+				}
+				finally {
+					// End to render guide line
+					circleExternal.Dispose() ;
+				}
 
-        using ( Transaction trans = new Transaction( document, "Create Arent Shaft" ) ) {
-          trans.Start() ;
+				// If second point is null. Return failed to end command
+				if ( secondPoint == null || checkEx ) return Result.Failed ;
 
-          // Create CurveArray for NewOpening method from list selected points
-          CurveArray shaftProfile = app.Create.NewCurveArray() ;
-          double radius = firstPoint.DistanceTo( secondPoint ) ;
-          double startAngle = 0;  
-          double endAngle = Math.PI * 2;
-          XYZ xAxis = new XYZ(1, 0, 0);  
-          XYZ yAxis = new XYZ(0, 1, 0);
-          if ( radius > 0.001 ) {
-            Curve cylinderCurve = Arc.Create( firstPoint, radius, startAngle, endAngle, xAxis, yAxis ) ;
-            shaftProfile.Append( cylinderCurve ) ;
-          }
+				// Get height setting
+				HeightSettingStorable heightSetting = document.GetHeightSettingStorable() ;
+				var levels = heightSetting.Levels.OrderBy( x => x.Elevation ).ToList() ;
+				// Get lowest and highest level
+				Level? lowestLevel = levels.FirstOrDefault() ;
+				Level? highestLevel = levels.LastOrDefault() ;
+				if ( lowestLevel == null && highestLevel == null ) return Result.Failed ;
 
-          // Create Shaft opening
-          Opening shaftOpening = document.Create.NewOpening( lowestLevel, highestLevel, shaftProfile ) ;
-          // Set offset from top
-          shaftOpening.get_Parameter( BuiltInParameter.WALL_TOP_OFFSET ).Set( 0 ) ;
-          // Set offset from base
-          shaftOpening.get_Parameter( BuiltInParameter.WALL_BASE_OFFSET ).Set( 0 ) ;
-          // Set base level is lowest level
-          shaftOpening.get_Parameter( BuiltInParameter.WALL_BASE_CONSTRAINT ).Set( lowestLevel!.Id ) ;
-          // Set top level is highest level
-          shaftOpening.get_Parameter( BuiltInParameter.WALL_HEIGHT_TYPE ).Set( highestLevel!.Id ) ;
+				using ( Transaction trans = new Transaction( document, "Create Arent Shaft" ) ) {
+					trans.Start() ;
 
-          if ( Math.Abs( lengthEndOne.MillimetersToRevitUnits() ) + Math.Abs( lengthEndTwo.MillimetersToRevitUnits() ) <=
-               document.Application.ShortCurveTolerance ) {
-            message =
-              $"Direction symbol length must be greater than {document.Application.ShortCurveTolerance.RevitUnitsToMillimeters()}mm!" ;
-            return Result.Cancelled ;
-          }
+					// Create CurveArray for NewOpening method from list selected points
+					CurveArray shaftProfile = app.Create.NewCurveArray() ;
+					double radius = firstPoint.DistanceTo( secondPoint ) ;
+					double startAngle = 0 ;
+					const double endAngle = Math.PI * 2 ;
+					XYZ xAxis = new XYZ( 1, 0, 0 ) ;
+					XYZ yAxis = new XYZ( 0, 1, 0 ) ;
+					if ( radius > 0.001 ) {
+						Curve cylinderCurve = Arc.Create( firstPoint, radius, startAngle, endAngle, xAxis, yAxis ) ;
+						shaftProfile.Append( cylinderCurve ) ;
+					}
 
-          var symbol = document.GetFamilySymbols( RoutingFamilyType.DirectionCylindricalShaft ).FirstOrDefault() ?? throw new InvalidOperationException();
-          if ( ! symbol.IsActive ) symbol.Activate() ;
+					// Create Shaft opening
+					Opening shaftOpening = document.Create.NewOpening( lowestLevel, highestLevel, shaftProfile ) ;
+					// Set offset from top
+					shaftOpening.get_Parameter( BuiltInParameter.WALL_TOP_OFFSET ).Set( 0 ) ;
+					// Set offset from base
+					shaftOpening.get_Parameter( BuiltInParameter.WALL_BASE_OFFSET ).Set( 0 ) ;
+					// Set base level is lowest level
+					shaftOpening.get_Parameter( BuiltInParameter.WALL_BASE_CONSTRAINT ).Set( lowestLevel!.Id ) ;
+					// Set top level is highest level
+					shaftOpening.get_Parameter( BuiltInParameter.WALL_HEIGHT_TYPE ).Set( highestLevel!.Id ) ;
 
-          if ( document.ActiveView.ViewType != ViewType.FloorPlan ) {
-            message = "Only created in floor plan view!" ;
-            return Result.Cancelled ;
-          }
+					if ( Math.Abs( LengthEndOne.MillimetersToRevitUnits() ) + Math.Abs( LengthEndTwo.MillimetersToRevitUnits() ) <= document.Application.ShortCurveTolerance ) {
+						message = $"Direction symbol length must be greater than {document.Application.ShortCurveTolerance.RevitUnitsToMillimeters()}mm!" ;
+						return Result.Cancelled ;
+					}
 
-          //Place symbol family
-          var instance =
-            document.Create.NewFamilyInstance( firstPoint, symbol, document.ActiveView ) ;
-          var axis = Line.CreateBound( firstPoint, Transform.CreateTranslation( XYZ.BasisZ ).OfPoint( firstPoint ) ) ;
-          ElementTransformUtils.RotateElement( document, instance.Id, axis, rotateAngle ) ;
+					var symbol = document.GetFamilySymbols( RoutingFamilyType.DirectionCylindricalShaft ).FirstOrDefault() ?? throw new InvalidOperationException() ;
+					if ( ! symbol.IsActive ) symbol.Activate() ;
 
-          //Set parameters
-          instance.LookupParameter( "Length End One" ).Set( lengthEndOne.MillimetersToRevitUnits() ) ;
-          instance.LookupParameter( "Length End Two" ).Set( lengthEndTwo.MillimetersToRevitUnits() ) ;
+					if ( document.ActiveView.ViewType != ViewType.FloorPlan ) {
+						message = "Only created in floor plan view!" ;
+						return Result.Cancelled ;
+					}
 
-          trans.Commit() ;
-          
-        }
+					//Place symbol family
+					var instance = document.Create.NewFamilyInstance( firstPoint, symbol, document.ActiveView ) ;
+					var axis = Line.CreateBound( firstPoint, Transform.CreateTranslation( XYZ.BasisZ ).OfPoint( firstPoint ) ) ;
+					ElementTransformUtils.RotateElement( document, instance.Id, axis, RotateAngle ) ;
 
-        return Result.Succeeded ;
-      }
-      catch ( Autodesk.Revit.Exceptions.OperationCanceledException ) {
-        return Result.Cancelled ;
-      }
-      catch ( Exception e ) {
-        message = e.Message ;
-        return Result.Failed ;
-      }
-    }
-  }
+					//Set parameters
+					instance.LookupParameter( "Length End One" ).Set( LengthEndOne.MillimetersToRevitUnits() ) ;
+					instance.LookupParameter( "Length End Two" ).Set( LengthEndTwo.MillimetersToRevitUnits() ) ;
+
+					trans.Commit() ;
+				}
+
+				return Result.Succeeded ;
+			}
+			catch ( Autodesk.Revit.Exceptions.OperationCanceledException ) {
+				return Result.Cancelled ;
+			}
+			catch ( Exception e ) {
+				message = e.Message ;
+				return Result.Failed ;
+			}
+		}
+	}
 }
