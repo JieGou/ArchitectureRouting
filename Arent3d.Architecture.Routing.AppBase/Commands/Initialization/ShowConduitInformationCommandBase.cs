aa3bfc4e47d6fa22ffa28ea07b4a3f2e8bea1255
Mi Do Tra <midtt@rikkeisoft.com>
2022-01-06 17:20:51 +0700

    fix numberOfPipes parameter when create detail table

--- Arent3d.Architecture.Routing.AppBase/Commands/Initialization/ShowConduitInformationCommandBase.cs
+++ Arent3d.Architecture.Routing.AppBase/Commands/Initialization/ShowConduitInformationCommandBase.cs
@@ -40,64 +40,65 @@ namespace Arent3d.Architecture.Routing.AppBase.Commands.Initialization
           string floor = doc.GetElementById<Level>( element.GetLevelId() )?.Name ?? string.Empty ;
           string constructionItem = element.LookupParameter( "Construction Item" ).AsValueString() ;
           string pipingType = element.Name ;
-          var existSymbolDetail =
-            detailSymbolStorable.DetailSymbolModelData.FirstOrDefault( x => element.Id.ToString() == x.ConduitId ) ;
-          string pipingNumber = "1" ;
-          var routeName = element.GetRouteName() ;
-          if ( ! string.IsNullOrEmpty( routeName ) ) {
-            var route = uiDoc.Document.CollectRoutes( AddInType.Electrical )
-              .FirstOrDefault( x => x.RouteName == routeName ) ;
-            if ( route != null ) {
-              var parentRouteName = route.GetParentBranches().ToList().LastOrDefault()?.RouteName ;
-              var childBranches = route.GetChildBranches().ToList() ;
-              if ( ! string.IsNullOrEmpty( parentRouteName ) ) {
-                pipingNumber = "↑" ;
+          var existSymbolDetails = detailSymbolStorable.DetailSymbolModelData.Where( x => element.Id.ToString() == x.ConduitId ).ToList() ;
+          foreach ( var existSymbolDetail in existSymbolDetails ) {
+            string pipingNumber = "1" ;
+            var routeName = element.GetRouteName() ;
+            if ( ! string.IsNullOrEmpty( routeName ) ) {
+              var route = uiDoc.Document.CollectRoutes( AddInType.Electrical )
+                .FirstOrDefault( x => x.RouteName == routeName ) ;
+              if ( route != null && existSymbolDetail.CountCableSamePosition != 1 ) {
+                var parentRouteName = route.GetParentBranches().ToList().LastOrDefault()?.RouteName ;
+                var childBranches = route.GetChildBranches().ToList() ;
+                if ( ! string.IsNullOrEmpty( parentRouteName ) ) {
+                  pipingNumber = "↑" ;
+                }
               }
             }
-          }
 
-          if ( existSymbolDetail != null ) {
-            var existSymbolRoute = element.GetRouteName() ?? string.Empty ;
-            if ( ! string.IsNullOrEmpty( existSymbolRoute ) && ceedStorable != null ) {
-              if ( ! processedDetailSymbol.Contains( existSymbolRoute ) ) {
-                var conduitInformationModel = new ConduitInformationModel( false, floor, string.Empty,
-                  existSymbolDetail.DetailSymbol, string.Empty, string.Empty, string.Empty, "1", string.Empty,
-                  string.Empty, string.Empty, pipingType, string.Empty, pipingNumber, string.Empty, string.Empty,
-                  constructionItem, constructionItem, "" ) ;
-                processedDetailSymbol.Add( existSymbolRoute ) ;
-                var ceedModel =
-                  ceedStorable.CeedModelData.FirstOrDefault( x => x.CeeDSetCode == existSymbolDetail.Code ) ;
-                if ( ceedModel != null ) {
-                  conduitInformationModel.CeeDCode = ceedModel.CeeDSetCode ;
+            if ( existSymbolDetail != null ) {
+              var existSymbolRoute = element.GetRouteName() ?? string.Empty ;
+              if ( ! string.IsNullOrEmpty( existSymbolRoute ) && ceedStorable != null ) {
+                if ( ! processedDetailSymbol.Contains( existSymbolRoute + "-" + existSymbolDetail.CountCableSamePosition ) ) {
+                  var conduitInformationModel = new ConduitInformationModel( false, floor, string.Empty,
+                    existSymbolDetail.DetailSymbol, string.Empty, string.Empty, string.Empty, "1", string.Empty,
+                    string.Empty, string.Empty, pipingType, string.Empty, pipingNumber, string.Empty, string.Empty,
+                    constructionItem, constructionItem, "" ) ;
+                  processedDetailSymbol.Add( existSymbolRoute + "-" + existSymbolDetail.CountCableSamePosition ) ;
+                  var ceedModel =
+                    ceedStorable.CeedModelData.FirstOrDefault( x => x.CeeDSetCode == existSymbolDetail.Code ) ;
+                  if ( ceedModel != null ) {
+                    conduitInformationModel.CeeDCode = ceedModel.CeeDSetCode ;
 
-                  var hiroiCdModel =
-                    hiroiSetCdMasterNormalModelData.FirstOrDefault( x => x.SetCode == ceedModel.CeeDSetCode ) ;
-                  var hiroiSetModels = hiroiSetMasterNormalModelData
-                    .Where( x => x.ParentPartModelNumber.Contains( ceedModel.CeeDModelNumber ) ).Skip( 1 ) ;
-                  conduitInformationModel.ConstructionClassification = hiroiCdModel?.ConstructionClassification ;
-                  foreach ( var item in hiroiSetModels ) {
-                    List<string> listMaterialCode = new List<string>() ;
-                    if ( ! string.IsNullOrWhiteSpace( item.MaterialCode1 ) ) {
-                      listMaterialCode.Add( item.MaterialCode1 ) ;
-                    }
+                    var hiroiCdModel =
+                      hiroiSetCdMasterNormalModelData.FirstOrDefault( x => x.SetCode == ceedModel.CeeDSetCode ) ;
+                    var hiroiSetModels = hiroiSetMasterNormalModelData
+                      .Where( x => x.ParentPartModelNumber.Contains( ceedModel.CeeDModelNumber ) ).Skip( 1 ) ;
+                    conduitInformationModel.ConstructionClassification = hiroiCdModel?.ConstructionClassification ;
+                    foreach ( var item in hiroiSetModels ) {
+                      List<string> listMaterialCode = new List<string>() ;
+                      if ( ! string.IsNullOrWhiteSpace( item.MaterialCode1 ) ) {
+                        listMaterialCode.Add( item.MaterialCode1 ) ;
+                      }
 
-                    if ( listMaterialCode.Any() ) {
-                      var masterModels = hiroiMasterModelData.Where( x => listMaterialCode.Contains( x.Buzaicd ) ) ;
-                      foreach ( var master in masterModels ) {
-                        var conduitModels = conduitsModelData
-                          .Where( x => x.PipingType == master.Type && x.Size == master.Size1 ).ToList() ;
-                        conduitInformationModel.Classification = conduitModels.FirstOrDefault()?.Classification ?? "" ;
-                        if ( existSymbolDetail != null ) {
-                          conduitInformationModel.WireType = master.Type ;
-                          conduitInformationModel.WireSize = master.Size1 ;
-                          conduitInformationModel.WireStrip = master.Size2 ;
+                      if ( listMaterialCode.Any() ) {
+                        var masterModels = hiroiMasterModelData.Where( x => listMaterialCode.Contains( x.Buzaicd ) ) ;
+                        foreach ( var master in masterModels ) {
+                          var conduitModels = conduitsModelData
+                            .Where( x => x.PipingType == master.Type && x.Size == master.Size1 ).ToList() ;
+                          conduitInformationModel.Classification = conduitModels.FirstOrDefault()?.Classification ?? "" ;
+                          if ( existSymbolDetail != null ) {
+                            conduitInformationModel.WireType = master.Type ;
+                            conduitInformationModel.WireSize = master.Size1 ;
+                            conduitInformationModel.WireStrip = master.Size2 ;
+                          }
                         }
                       }
                     }
                   }
-                }
 
-                conduitInformationModels.Add( conduitInformationModel ) ;
+                  conduitInformationModels.Add( conduitInformationModel ) ;
+                }
               }
             }
           }
