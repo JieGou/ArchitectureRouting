2529b0ee7e20c7bd62eda6c7e726af841a46a344
Mi Do Tra <midtt@rikkeisoft.com>
2022-01-12 09:38:21 +0700

    edit "Room Wiring" button icon

--- Arent3d.Architecture.Routing.Electrical.App/Commands/Routing/RoomPickRoutingCommand.cs
+++ Arent3d.Architecture.Routing.Electrical.App/Commands/Routing/RoomPickRoutingCommand.cs
@@ -11,8 +11,7 @@ namespace Arent3d.Architecture.Routing.Electrical.App.Commands.Routing
 {
   [Transaction( TransactionMode.Manual )]
   [DisplayNameKey( "Electrical.App.Commands.Routing.RoomPickRoutingCommand", DefaultString = "Room Wiring" )]
-  [Image( "resources/Initialize-16.bmp", ImageType = ImageType.Normal )]
-  [Image( "resources/Initialize-32.bmp", ImageType = ImageType.Large )]
+  [Image( "resources/PickFrom-To.png" )]
   public class RoomPickRoutingCommand : RoomPickRoutingCommandBase
   {
     protected override string GetTransactionNameKey() => "TransactionName.Commands.Routing.PickRouting" ;
