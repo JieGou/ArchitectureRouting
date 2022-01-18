de6ded645c44e3ec08c86638de788aa4df2caef7
Pham Duy <duy.pham@arent3d.com>
2022-01-18 18:29:01 +0900

    fix language

--- Arent3d.Architecture.Routing.Electrical.App/Commands/Initialization/ChangeToNormalCommand.cs
+++ Arent3d.Architecture.Routing.Electrical.App/Commands/Initialization/ChangeToNormalCommand.cs
@@ -5,7 +5,7 @@ using Autodesk.Revit.Attributes ;
 namespace Arent3d.Architecture.Routing.Electrical.App.Commands.Initialization
 {
   [Transaction( TransactionMode.Manual )]
-  [DisplayNameKey( "Electrical.App.Commands.Initialization.ChangeToEcoCommand", DefaultString = "Change to Normal" )]
+  [DisplayNameKey( "Electrical.App.Commands.Initialization.ChangeToNormalCommand", DefaultString = "Change to Normal" )]
   [Image( "resources/Initialize-32.bmp", ImageType = ImageType.Large )]
   public class ChangeToNormalCommand :  ChangeConduitModeCommandBase
   {
