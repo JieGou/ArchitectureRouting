f7542bef34f8b70d9e27ff4fae5ef6354172d6e8
Nhu.Truong <quangnhu95@gmail.com>
2022-03-04 09:47:32 +0700

    Initial function

---
+++ Arent3d.Architecture.Routing.Mechanical.haseko.App/Commands/Routing/ReplaceFlexDuctCommand.cs
@@ -0,0 +1,16 @@
+﻿using Autodesk.Revit.DB ;
+using Autodesk.Revit.UI ;
+
+namespace Arent3d.Architecture.Routing.Mechanical.haseko.App.Commands.Routing
+{
+  public class ReplaceFlexDuctCommand : IExternalCommand
+  {
+    public Result Execute( ExternalCommandData commandData, ref string message, ElementSet elements )
+    {
+      var document = commandData.Application.ActiveUIDocument.Document ;
+      var selection = commandData.Application.ActiveUIDocument.Selection ;
+
+      return Result.Succeeded ;
+    }
+  }
+}
