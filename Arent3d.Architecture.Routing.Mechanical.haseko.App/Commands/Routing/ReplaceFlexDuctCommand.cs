5bcd40067f8cc01966ba8a6acc3c022256978de0
Nhu.Truong <quangnhu95@gmail.com>
2022-03-08 19:14:12 +0700

    Change flext duct

--- Arent3d.Architecture.Routing.Mechanical.haseko.App/Commands/Routing/ReplaceFlexDuctCommand.cs
+++ Arent3d.Architecture.Routing.Mechanical.haseko.App/Commands/Routing/ReplaceFlexDuctCommand.cs
@@ -1,10 +1,7 @@
 ﻿using System ;
-using System.Collections ;
 using System.Collections.Generic ;
 using System.Linq ;
-using Arent3d.Architecture.Routing.AppBase.Commands ;
 using Arent3d.Architecture.Routing.AppBase.Selection ;
-using Arent3d.Architecture.Routing.Mechanical.haseko.App.Extensions ;
 using Arent3d.Architecture.Routing.Mechanical.haseko.App.Forms ;
 using Arent3d.Architecture.Routing.Mechanical.haseko.App.ViewModel ;
 using Autodesk.Revit.Attributes ;
@@ -12,6 +9,7 @@ using Autodesk.Revit.DB ;
 using Autodesk.Revit.DB.Mechanical ;
 using Autodesk.Revit.UI ;
 using Autodesk.Revit.UI.Selection ;
+using MoreLinq.Extensions ;
 
 namespace Arent3d.Architecture.Routing.Mechanical.haseko.App.Commands.Routing
 {
@@ -19,6 +17,7 @@ namespace Arent3d.Architecture.Routing.Mechanical.haseko.App.Commands.Routing
   public class ReplaceFlexDuctCommand : IExternalCommand
   {
     private const string Title = "Arent" ;
+    private const string GeneralNotify = "The elements are not continuously connected!" ;
 
     public Result Execute( ExternalCommandData commandData, ref string message, ElementSet elementSet )
     {
@@ -28,82 +27,152 @@ namespace Arent3d.Architecture.Routing.Mechanical.haseko.App.Commands.Routing
       try {
         Func<Element, bool> filter = f =>
           f.Category.Id.IntegerValue == (int) BuiltInCategory.OST_DuctCurves || f.Category.Id.IntegerValue == (int) BuiltInCategory.OST_DuctFitting ;
-        
-        var elements = selection.PickObjects( ObjectType.Element, SelectionFilter.GetElementFilter( filter ) )
-          .Select( r => document.GetElement( r ) ) ;
-        
-        var connectorManagers = GetConnectorManagers( elements ) ;
-        if ( connectorManagers.Count == 0 ) {
+
+        var elements = selection.PickObjects( ObjectType.Element, SelectionFilter.GetElementFilter( filter ) ).Select( r => document.GetElement( r ) )
+          .ToList() ;
+
+        if ( elements.Count() == 0 ) {
           TaskDialog.Show( Title, "Please select again!" ) ;
-          return Result.Failed ;
+          return Result.Cancelled ;
+        }
+        
+        var (isContinuousConnected, mesage, connectorRefs, points) = IsContinuousConnected( elements ) ;
+        if ( ! isContinuousConnected ) {
+          TaskDialog.Show( Title, mesage, TaskDialogCommonButtons.Ok ) ;
+          return Result.Cancelled ;
         }
 
-        
-        
-        // var (isValid, mesage) = IsContinuousConnected( connectorManagers, out (Connector Start, Connector End)? connectors ) ;
-        // if ( ! isValid ) {
-        //   TaskDialog.Show( Title, mesage, TaskDialogCommonButtons.Ok ) ;
-        //   return Result.Cancelled ;
-        // }
-        
-        var count = GetConnectors( connectorManagers[ 0 ] ).Count() ;
-        TaskDialog.Show( Title, $"{count}" ) ;
-
-        // var replaceFlexDuctView = new ReplaceFlexDuctView() { DataContext = new ReplaceFlexDuctViewModel( document ) } ;
-        // replaceFlexDuctView.ShowDialog() ;
-
+        var viewModel = new ReplaceFlexDuctViewModel( document, ( connectorRefs, points, elements ) ) ;
+        var replaceFlexDuctView = new ReplaceFlexDuctView() { DataContext = viewModel } ;
+        replaceFlexDuctView.ShowDialog() ;
 
         return Result.Succeeded ;
       }
+      catch ( Autodesk.Revit.Exceptions.OperationCanceledException ) {
+        return Result.Cancelled ;
+      }
       catch ( Exception exception ) {
-        CommandUtils.DebugAlertException( exception ) ;
+        message = exception.Message ;
         return Result.Failed ;
       }
     }
 
-    // private (bool IsValid, string Message, (Connector Start, Connector End)? connectors) IsContinuousConnected(
-    //   List<ConnectorManager> connectorManagers )
-    // {
-    //   if ( ! connectorManagers.Any() )
-    //     return ( false, "Not found the connectors!", null ) ;
-    //
-    //   if ( connectorManagers.Count == 1 ) {
-    //     if ( connectorManagers[ 0 ].Connectors.Size != 2 )
-    //       return ( false, "The number of the connectors is not satisfied!", null ) ;
-    //     else
-    //       return ( true, string.Empty, ( connectorManagers[ 0 ].Lookup( 0 ), connectorManagers[ 0 ].Lookup( 1 ) ) ) ;
-    //   }
-    //
-    //   var connectorManager = connectorManagers[ 0 ] ;
-    //   var otherConnectorManager = connectorManagers.GetRange( 1, connectorManagers.Count - 1 ) ;
-    // }
-    
-    
-    
-    private static List<Connector> GetConnectors( ConnectorManager connectorManager )
+    private (bool IsValid, string Message, List<Connector> ConnectorRefs, List<(XYZ Origin, XYZ Direction)> Points) IsContinuousConnected( IList<Element> elements )
     {
-      var connectors = new List<Connector>() ;
-      
-      var connector = connectorManager.Connectors.GetEnumerator() ;
-      while ( connector.MoveNext() ) {
-        connectors.Add((Connector)connector.Current!);
+      var connectorRefs = new List<Connector>() ;
+      var points = new List<(XYZ, XYZ)>() ;
+
+      var connectorManagers = GetConnectorManagers( elements ) ;
+      if ( connectorManagers.Count == 0 )
+        return ( false, "Not found the connectors!", connectorRefs, points ) ;
+
+      if ( connectorManagers.Count == 1 )
+        if ( connectorManagers[ 0 ].Connectors.Size != 2 )
+          return ( false, "The number of the connectors is not satisfied!", connectorRefs, points ) ;
+        else
+          return ( true, string.Empty, connectorRefs,
+            new List<(XYZ, XYZ)>()
+            {
+              (connectorManagers[ 0 ].Lookup( 0 ).Origin, connectorManagers[ 0 ].Lookup( 0 ).CoordinateSystem.BasisZ), 
+              (connectorManagers[ 0 ].Lookup( 1 ).Origin, connectorManagers[ 0 ].Lookup( 1 ).CoordinateSystem.BasisZ)
+            } ) ;
+
+      var endConnectors = new List<(List<Connector> Connecteds, List<Connector> UnConnects)>() ;
+      foreach ( var connectorManager in connectorManagers ) {
+        var connectors = GetConnectors( connectorManager.Connectors ) ;
+
+        if ( connectors.Connecteds.Count == 0 )
+          return ( false, GeneralNotify, connectorRefs, points ) ;
+        else {
+          var insideConnecteds = GetInsideConnecteds(connectors.Connecteds, elements) ;
+          
+          if ( insideConnecteds.Count == 0 )
+            return ( false, GeneralNotify, connectorRefs, points ) ;
+          else if ( insideConnecteds.Count == 1 ) {
+            var outsideConnecteds = connectors.Connecteds.Where( x => x.Id != insideConnecteds[ 0 ].Id ).ToList() ;
+            endConnectors.Add( ( outsideConnecteds, connectors.Unconnects ) ) ;
+          }
+        }
       }
 
-      return connectors ;
+      if ( endConnectors.Count != 2 )
+        return ( false, GeneralNotify, connectorRefs, points ) ;
+      else {
+        foreach ( var endConnector in endConnectors )
+          ResolveConnector( endConnector.Connecteds, endConnector.UnConnects, ref connectorRefs, ref points ) ;
+
+        if ( ( connectorRefs.Count == 2 && points.Count == 0 ) || ( connectorRefs.Count == 1 && points.Count == 1 ) ||
+             ( connectorRefs.Count == 0 && points.Count == 2 ) )
+          return ( true, string.Empty, connectorRefs, points ) ;
+
+        return ( false, "The flex duct cannot create!", connectorRefs, points ) ;
+      }
     }
 
-    private List<ConnectorManager> GetConnectorManagers( IEnumerable<Element> elements )
+    private List<Connector> GetInsideConnecteds(IList<Connector> connecteds, IList<Element> selectedElements)
+    {
+      var insideConnecteds = new List<Connector>() ;
+      
+      foreach ( var connected in connecteds ) {
+        var conRefs = GetConnectorRefs( connected ) ;
+        var otherElements = selectedElements.Where( x => x.Id != connected.Owner.Id ) ;
+        foreach ( var element in otherElements ) {
+          if(conRefs.Any(x => x.Owner.Id == element.Id))
+            insideConnecteds.Add(connected);
+        }
+      }
+
+      return insideConnecteds ;
+    }
+    
+    private void ResolveConnector( List<Connector> connecteds, List<Connector> unConnects, 
+      ref List<Connector> connectors, ref List<(XYZ, XYZ)> points )
+    {
+      if ( connecteds.Count > 0 ) {
+        var connectedRefs = GetConnectors( connecteds[ 0 ].AllRefs ).Connecteds ;
+        if ( connectedRefs.Count > 0 )
+          connectors.Add( connectedRefs[ 0 ] ) ;
+      }
+      else if ( unConnects.Count > 0 ) {
+        points.Add( (unConnects[ 0 ].Origin, unConnects[ 0 ].CoordinateSystem.BasisZ) ) ;
+      }
+    }
+
+    private List<Connector> GetConnectorRefs( Connector connector )
+    {
+      var connectorRefs = connector.AllRefs.OfType<Connector>() ;
+      return connectorRefs.Where( x => x.Owner.Id != connector.Owner.Id ).ToList() ;
+    }
+
+    private (List<Connector> Connecteds, List<Connector> Unconnects) GetConnectors( ConnectorSet connectorSet )
+    {
+      var connecteds = new List<Connector>() ;
+      var unconnects = new List<Connector>() ;
+
+      connectorSet.OfType<Connector>().ForEach( x =>
+      {
+        if ( x.IsConnected )
+          connecteds.Add( x ) ;
+        else
+          unconnects.Add( x ) ;
+      } ) ;
+
+      return ( connecteds, unconnects ) ;
+    }
+
+    private List<ConnectorManager> GetConnectorManagers( IList<Element> elements )
     {
       var connectorManagers = new List<ConnectorManager>() ;
       foreach ( var element in elements ) {
         if ( element is FamilyInstance familyInstance ) {
-          connectorManagers.Add(familyInstance.MEPModel.ConnectorManager);
+          connectorManagers.Add( familyInstance.MEPModel.ConnectorManager ) ;
         }
         else if ( element is Duct duct ) {
-          connectorManagers.Add(duct.ConnectorManager);
+          connectorManagers.Add( duct.ConnectorManager ) ;
         }
       }
-      return connectorManagers ;
+
+      return connectorManagers.Where( x => x.Connectors.Size > 0 ).ToList() ;
     }
   }
 }
--- Arent3d.Architecture.Routing.Mechanical.haseko.App/ViewModel/ReplaceFlexDuctViewModel.cs
+++ Arent3d.Architecture.Routing.Mechanical.haseko.App/ViewModel/ReplaceFlexDuctViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using System ;
+using System.Collections.Generic ;
 using System.Collections.ObjectModel ;
 using Arent3d.Architecture.Routing.AppBase.ViewModel ;
 using Arent3d.Revit ;
@@ -9,6 +10,7 @@ using System.Windows ;
 using System.Windows.Input ;
 using Arent3d.Architecture.Routing.AppBase.Commands.Routing ;
 using Autodesk.Revit.UI ;
+using MoreLinq ;
 
 namespace Arent3d.Architecture.Routing.Mechanical.haseko.App.ViewModel
 {
@@ -17,9 +19,12 @@ namespace Arent3d.Architecture.Routing.Mechanical.haseko.App.ViewModel
     #region Members
 
     private Document _document ;
-    private const string Title = "Arent" ; 
+    private ( List<Connector> ConnectorRefs, List<(XYZ Origin, XYZ Direction)> Points, List<Element> DeletedElements) _data ;
+    private const string Title = "Arent" ;
+    private DisplayUnit DisplayUnit => _document.DisplayUnitSystem ;
 
     private ObservableCollection<FlexDuctType>? _flexDuctTypes ;
+
     public ObservableCollection<FlexDuctType> FlexDuctTypes
     {
       get
@@ -38,6 +43,7 @@ namespace Arent3d.Architecture.Routing.Mechanical.haseko.App.ViewModel
     }
 
     private FlexDuctType? _flexDuctType ;
+
     public FlexDuctType? FlexDuctType
     {
       get { return _flexDuctType ??= FlexDuctTypes.FirstOrDefault() ; }
@@ -49,20 +55,14 @@ namespace Arent3d.Architecture.Routing.Mechanical.haseko.App.ViewModel
     }
 
     private ObservableCollection<string>? _diameters ;
+
     public ObservableCollection<string> Diameters
     {
       get
       {
         if ( null == _diameters ) {
           var ductSize = DuctSizeSettings.GetDuctSizeSettings( _document )[ DuctShape.Round ].Select( x => x.NominalDiameter ).OrderBy( x => x ) ;
-          Func<double, string> displayLength = d =>
-          {
-            if ( DisplayUnit == DisplayUnit.METRIC )
-              return $"{Math.Round( d.RevitUnitsToMillimeters() )} mm" ;
-            else
-              return $"{Math.Round( UnitUtils.ConvertFromInternalUnits( d, DisplayUnitTypes.Inches ) )}" ;
-          } ;
-          _diameters = new ObservableCollection<string>( ductSize.Select( displayLength ) ) ;
+          _diameters = new ObservableCollection<string>( ductSize.Select( DisplayDiameter ) ) ;
         }
 
         return _diameters ;
@@ -70,21 +70,31 @@ namespace Arent3d.Architecture.Routing.Mechanical.haseko.App.ViewModel
     }
 
     private string? _diameter ;
+
     public string? Diameter
     {
-      get { return _diameter ??= Diameters.FirstOrDefault() ; }
+      get
+      {
+        if ( _data.ConnectorRefs.Count > 0 ) {
+          var value = SuggestionDiameter( _data.ConnectorRefs ) ;
+          _diameter = DisplayDiameter( value ) ;
+        }
+        return _diameter ??= Diameters.FirstOrDefault() ;
+      }
       set
       {
         _diameter = value ;
         OnPropertyChanged() ;
       }
     }
-    protected DisplayUnit DisplayUnit => _document.DisplayUnitSystem ;
+
     #endregion
 
-    public ReplaceFlexDuctViewModel( Document document )
+    public ReplaceFlexDuctViewModel( Document document,
+      ( List<Connector> ConnectorRefs, List<(XYZ, XYZ)> Points, List<Element> DeletedElements) data)
     {
       _document = document ;
+      _data = data ;
     }
 
     #region Commands
@@ -96,16 +106,86 @@ namespace Arent3d.Architecture.Routing.Mechanical.haseko.App.ViewModel
 
     public ICommand OkCommand
     {
-      get 
-      { 
+      get
+      {
         return new RelayCommand<Window>( ( wd ) => { return null != wd ; }, ( wd ) =>
         {
-          wd.Close();
-          TaskDialog.Show( Title, Diameter ) ;
-        } ) ; 
+          try {
+            if ( null == FlexDuctType )
+              TaskDialog.Show( Title, "Not found the flex duct type!" ) ;
+            else {
+              var (canParse, diameter) = TryParseDiamater( Diameter ) ;
+              if(!canParse)
+                TaskDialog.Show( Title, "The diameter is invalid!" ) ;
+              else {
+                
+                using Transaction transaction = new Transaction( _document ) ;
+                transaction.Start( "Change Flex Duct" ) ;
+                  
+                FlexDuct flexDuct ;
+                if ( _data.ConnectorRefs.Count == 2 ) 
+                  flexDuct = _document.Create.NewFlexDuct( _data.ConnectorRefs[ 0 ], _data.ConnectorRefs[ 1 ], FlexDuctType ) ;
+                else if ( _data.ConnectorRefs.Count == 1 ) {
+                  flexDuct = _document.Create.NewFlexDuct( _data.ConnectorRefs[ 0 ], _data.Points.Select(x => x.Origin).ToList(), FlexDuctType ) ;
+                  flexDuct.EndTangent = _data.Points[ 0 ].Direction ;
+                }
+                else {
+                  flexDuct = _document.Create.NewFlexDuct( _data.Points.Select(x => x.Origin).ToList(), FlexDuctType ) ;
+                  flexDuct.StartTangent = _data.Points[ 0 ].Direction.Negate() ;
+                  flexDuct.EndTangent = _data.Points[ 1 ].Direction ;
+                }
+                  
+                flexDuct.get_Parameter( BuiltInParameter.RBS_CURVE_DIAMETER_PARAM ).Set( diameter ) ;
+
+                _document.Delete( _data.DeletedElements.Select( x => x.Id ).ToList() ) ;
+                  
+                transaction.Commit() ;
+                  
+                wd.Close();
+              }
+            }
+          }
+          catch ( Exception exception ) {
+            TaskDialog.Show( Title, exception.Message ) ;
+          }
+        } ) ;
       }
     }
+
     #endregion
-    
+
+    #region Methods
+
+    private (bool CanParse, double Diameter) TryParseDiamater( string? diameter )
+    {
+      if ( DisplayUnit == DisplayUnit.METRIC )
+        return ( double.TryParse( diameter?.Replace( "mm", "" ).Replace( "MM", "" ).Trim(), out double value ), value.MillimetersToRevitUnits() ) ;
+      else
+        return ( double.TryParse( diameter?.Trim(), out double value ), UnitUtils.ConvertFromInternalUnits( value, DisplayUnitTypes.Inches ) ) ;
+    }
+
+    private double SuggestionDiameter(IList<Connector> connectors )
+    {
+      var values = new List<double>() ;
+
+      foreach ( var connector in connectors ) {
+        if(connector.Shape == ConnectorProfileType.Round)
+          values.Add(2 * connector.Radius);
+        else if (connector.Shape == ConnectorProfileType.Rectangular 
+                 || connector.Shape == ConnectorProfileType.Oval)
+          values.Add(Math.Min(connector.Width, connector.Height));
+      }
+      
+      return values.Min() ;
+    }
+
+    private string DisplayDiameter( double diameter )
+    {
+      if ( DisplayUnit == DisplayUnit.METRIC )
+        return $"{Math.Round( diameter.RevitUnitsToMillimeters() )} mm" ;
+      else
+        return $"{Math.Round( UnitUtils.ConvertFromInternalUnits( diameter, DisplayUnitTypes.Inches ) )}" ;
+    }
+    #endregion
   }
 }
