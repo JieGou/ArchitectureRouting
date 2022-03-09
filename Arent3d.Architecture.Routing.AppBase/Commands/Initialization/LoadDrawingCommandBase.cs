3c00dc5dcf2953c90e11bda8e758f151c70f51c5
tung_le_arent <tung.le@arent3d.com>
2022-03-09 20:18:05 +0700

    + Set floor height instead distance between floors   + Not create new file when import dwg   + View import file on 3D

--- Arent3d.Architecture.Routing.AppBase/Commands/Initialization/LoadDrawingCommandBase.cs
+++ Arent3d.Architecture.Routing.AppBase/Commands/Initialization/LoadDrawingCommandBase.cs
@@ -6,6 +6,7 @@ using System.Windows.Forms ;
 using Arent3d.Architecture.Routing.AppBase.Forms ;
 using Arent3d.Architecture.Routing.AppBase.Model ;
 using Arent3d.Architecture.Routing.AppBase.ViewModel ;
+using Arent3d.Revit ;
 using Autodesk.Revit.DB ;
 using Autodesk.Revit.UI ;
 
@@ -19,9 +20,9 @@ namespace Arent3d.Architecture.Routing.AppBase.Commands.Initialization
       if ( openFileDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK ) {
         var importDwgMappingModels = new List<ImportDwgMappingModel>()
         {
-          new ImportDwgMappingModel( string.Empty, "B1FL", 3000 ),
-          new ImportDwgMappingModel( string.Empty, "PH1F", 3000  ),
-          new ImportDwgMappingModel( string.Empty, "PH1RFL", 3000  )
+          new ImportDwgMappingModel( string.Empty, "B1FL", 0 ),
+          new ImportDwgMappingModel( string.Empty, "PH1F", 51200  ),
+          new ImportDwgMappingModel( string.Empty, "PH1RFL", 54600  )
         } ;
         var fileItems = new List<FileComboboxItemType>() ;
         for ( int i = 1 ; i <= openFileDialog.FileNames.Length ; i++ ) {
@@ -40,54 +41,55 @@ namespace Arent3d.Architecture.Routing.AppBase.Commands.Initialization
             var fileItem = fileItems.FirstOrDefault( x => x.FileName.Equals( importDwgMappingModel.FileName ) ) ;
             importDwgMappingModel.FullFilePath = fileItem != null ? fileItem.FullFilePath : "" ;
           }
-          SaveFileDialog saveFileDialog = new SaveFileDialog() { Filter = "Revit files (*.rvt )|*.rvt", Title = "Save an Revit File" };
-          if ( saveFileDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK ) {
-            var doc = commandData.Application.Application.NewProjectDocument( UnitSystem.Metric ) ;
-            File.Delete( saveFileDialog.FileName ) ;
-            doc.SaveAs( saveFileDialog.FileName ) ;
-            commandData.Application.OpenAndActivateDocument( doc.PathName ) ;
-            
-            var actView = doc.ActiveView;
-            var dwgImportOptions = new DWGImportOptions
-            {
-              ColorMode = ImportColorMode.Preserved,
-              CustomScale = 0.0,
-              Unit = ImportUnit.Default,
-              OrientToView = true,
-              Placement = ImportPlacement.Origin,
-              ThisViewOnly = false,
-              VisibleLayersOnly = false
-            } ;
-            var firstMappingModel = completeImportDwgMappingModels.First() ;
-            var processFirstFloorTrans = new Transaction( doc ) ;
-            processFirstFloorTrans.SetName("Import");    
-            processFirstFloorTrans.Start();
-            Level firstFloorLevel = actView.GenLevel;
-            firstFloorLevel.Elevation = firstMappingModel.FloorHeight ;
-            firstFloorLevel.Name = "Level " + firstMappingModel.FloorName ;
-            actView.Name = firstMappingModel.FloorName ;
-            if(!string.IsNullOrEmpty(firstMappingModel.FullFilePath)) doc.Import( firstMappingModel.FullFilePath, dwgImportOptions, actView, out ElementId firstElementId ) ;
-            processFirstFloorTrans.Commit();
-            
-            double levelElevation = actView.GenLevel.Elevation ;
-            var importTrans = new Transaction( doc ) ;
-            importTrans.SetName("Import");
-            importTrans.Start();
-            for ( int i = 1 ; i < completeImportDwgMappingModels.Count() ; i++ ) {
-              var importDwgMappingModel = completeImportDwgMappingModels[ i ] ;
-              if ( ! string.IsNullOrEmpty( importDwgMappingModel.FullFilePath ) ) {
-                var viewFamily = new FilteredElementCollector(doc).OfClass(typeof(ViewFamilyType)).Cast<ViewFamilyType>().First(x => x.ViewFamily == ViewFamily.FloorPlan);
-                levelElevation += importDwgMappingModel.FloorHeight ;
-                var importDwgLevel = Level.Create(doc, levelElevation) ;
-                importDwgLevel.Name = "Level " + importDwgMappingModel.FloorName ;
-                var viewPlan = ViewPlan.Create(doc, viewFamily.Id , importDwgLevel.Id);
-                doc.Import( importDwgMappingModel.FullFilePath, dwgImportOptions, viewPlan, out ElementId importElementId ) ;
+          Document doc = commandData.Application.ActiveUIDocument.Document ;
+          var actView = doc.ActiveView;
+          var dwgImportOptions = new DWGImportOptions
+          {
+            ColorMode = ImportColorMode.Preserved,
+            CustomScale = 0.0,
+            Unit = ImportUnit.Default,
+            OrientToView = true,
+            Placement = ImportPlacement.Origin,
+            ThisViewOnly = false,
+            VisibleLayersOnly = false
+          } ;
+          /*var firstMappingModel = completeImportDwgMappingModels.First() ;
+          var processFirstFloorTrans = new Transaction( doc ) ;
+          processFirstFloorTrans.SetName("Import");    
+          processFirstFloorTrans.Start();
+          Level firstFloorLevel = actView.GenLevel;
+          firstFloorLevel.Elevation = firstMappingModel.FloorHeight ;
+          firstFloorLevel.Name = "Level " + firstMappingModel.FloorName ;
+          actView.Name = firstMappingModel.FloorName ;
+          if(!string.IsNullOrEmpty(firstMappingModel.FullFilePath)) doc.Import( firstMappingModel.FullFilePath, dwgImportOptions, actView, out ElementId firstElementId ) ;
+          processFirstFloorTrans.Commit();*/
+
+          var viewFamily = new FilteredElementCollector(doc).OfClass(typeof(ViewFamilyType)).Cast<ViewFamilyType>().First(x => x.ViewFamily == ViewFamily.FloorPlan);
+          var allCurrentLevels = new FilteredElementCollector( doc ).OfClass( typeof( Level ) ).ToList() ;
+          var allCurrentViewPlans = new FilteredElementCollector( doc ).OfClass( typeof( ViewPlan ) ).ToList() ;
+          var importTrans = new Transaction( doc ) ;
+          importTrans.SetName("Import");
+          importTrans.Start();
+          for ( int i = 0 ; i < completeImportDwgMappingModels.Count() ; i++ ) {
+            var importDwgMappingModel = completeImportDwgMappingModels[ i ] ;
+            if ( ! string.IsNullOrEmpty( importDwgMappingModel.FullFilePath ) ) {
+              var levelName = "Level " + importDwgMappingModel.FloorName ;
+              var importDwgLevel = allCurrentLevels.FirstOrDefault( x => x.Name.Equals( levelName ) ) ;
+              if ( importDwgLevel == null ) {
+                importDwgLevel = Level.Create( doc, importDwgMappingModel.FloorHeight ) ;
+                importDwgLevel.Name =  levelName;
+              }
+              var viewPlan = allCurrentViewPlans.FirstOrDefault( x => x.Name.Equals( importDwgMappingModel.FloorName ) ) as ViewPlan;
+              if ( viewPlan == null ) {
+                viewPlan = ViewPlan.Create(doc, viewFamily.Id , importDwgLevel.Id);
                 viewPlan.Name = importDwgMappingModel.FloorName;
               }
+              /*var columnSymbolicOffset = viewPlan.get_Parameter( BuiltInParameter.VIEW_SLANTED_COLUMN_SYMBOL_OFFSET ).AsValueString();
+              importDwgLevel.SetProperty( BuiltInParameter.LEVEL_ELEV, importDwgMappingModel.FloorHeight / Double.Parse(columnSymbolicOffset) );*/
+              doc.Import( importDwgMappingModel.FullFilePath, dwgImportOptions, viewPlan, out ElementId importElementId ) ;
             }
-            importTrans.Commit();
-            doc.Save() ;
           }
+          importTrans.Commit();
         }
       }
       
