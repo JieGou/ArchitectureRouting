63aa07081729c4dd912a10d68d0aebbd1823b781
nhutruong-arent <quangnhu95@gmail.com>
2022-03-28 18:35:32 +0700

    Implement UI

--- Arent3d.Architecture.Routing.Electrical.App/Commands/Initialization/RegisterSymbolCommand.cs
+++ Arent3d.Architecture.Routing.Electrical.App/Commands/Initialization/RegisterSymbolCommand.cs
@@ -1,4 +1,6 @@
-﻿using Autodesk.Revit.Attributes ;
+﻿using Arent3d.Architecture.Routing.Electrical.App.Forms ;
+using Arent3d.Architecture.Routing.Electrical.App.ViewModels ;
+using Autodesk.Revit.Attributes ;
 using Autodesk.Revit.DB ;
 using Autodesk.Revit.UI ;
 
@@ -9,6 +11,11 @@ namespace Arent3d.Architecture.Routing.Electrical.App.Commands.Initialization
   {
     public Result Execute( ExternalCommandData commandData, ref string message, ElementSet elementSet )
     {
+      var registerSymbolView = new RegisterSymbolView
+      {
+        DataContext = new RegisterSymbolViewModel( commandData.Application.ActiveUIDocument )
+      } ;
+      registerSymbolView.ShowDialog() ;
       
       return Result.Succeeded ; 
     }
---
+++ Arent3d.Architecture.Routing.Electrical.App/ViewModels/RegisterSymbolViewModel.cs
@@ -0,0 +1,80 @@
+﻿using System ;
+using System.Collections.Generic ;
+using System.Collections.ObjectModel ;
+using System.IO ;
+using System.Windows ;
+using System.Windows.Forms ;
+using System.Windows.Input ;
+using Arent3d.Architecture.Routing.AppBase.Commands.Routing ;
+using Arent3d.Architecture.Routing.AppBase.ViewModel ;
+using Arent3d.Architecture.Routing.Electrical.App.ViewModels.Models ;
+using Autodesk.Revit.UI ;
+
+namespace Arent3d.Architecture.Routing.Electrical.App.ViewModels
+{
+  public class RegisterSymbolViewModel : NotifyPropertyChanged
+  {
+    private readonly UIDocument _uiDocument ;
+    private string? _browseFolderPath ;
+    private const string SearchPattern = "*.dwg|*.png|*.jpg" ;
+
+    private ObservableCollection<FolderItemModel>? _folderItems ;
+
+    public ObservableCollection<FolderItemModel> FolderItems
+    {
+      get { return _folderItems ??= new ObservableCollection<FolderItemModel>() ; }
+      set
+      {
+        _folderItems = value ;
+        OnPropertyChanged();
+      }
+    }
+
+    public RegisterSymbolViewModel( UIDocument uiDocument )
+    {
+      _uiDocument = uiDocument ;
+    }
+
+    #region Commands
+
+    public ICommand BrowseCommand
+    {
+      get
+      {
+        return new RelayCommand<Window>( wd => null != wd, wd =>
+        {
+          wd?.Hide() ;
+
+          using var folderBrowserDialog = new FolderBrowserDialog { ShowNewFolderButton = true } ;
+          folderBrowserDialog.Reset() ;
+          folderBrowserDialog.RootFolder = Environment.SpecialFolder.MyComputer ;
+          folderBrowserDialog.Description = "Select folder contains .dwg and image file." ;
+          if ( folderBrowserDialog.ShowDialog() == DialogResult.OK && ! string.IsNullOrWhiteSpace( folderBrowserDialog.SelectedPath ) )
+            _browseFolderPath = folderBrowserDialog.SelectedPath ;
+
+          wd?.Show() ;
+        } ) ;
+      }
+    }
+
+    #endregion
+
+    #region Methods
+
+    private void GetFolders()
+    {
+      var folderItems = new List<FolderItemModel>() ;
+      if ( null != _browseFolderPath && Directory.Exists( _browseFolderPath ) ) {
+        foreach ( var directory in Directory.GetDirectories(_browseFolderPath, SearchPattern, SearchOption.TopDirectoryOnly) ) {
+          
+        }
+      }
+      else {
+        System.Windows.MessageBox.Show( "The folder path does not exist.", "Arent Notification" ) ;
+      }
+    }
+
+    #endregion
+    
+  }
+}
