10f0f9ccb974e5227eac74dfdad376801e617b0a
nhutruong-arent <quangnhu95@gmail.com>
2022-03-28 13:24:50 +0700

    Initial

---
+++ Arent3d.Architecture.Routing.Electrical.App/Commands/Initialization/RegisterSymbolCommand.cs
@@ -0,0 +1,16 @@
+﻿using Autodesk.Revit.Attributes ;
+using Autodesk.Revit.DB ;
+using Autodesk.Revit.UI ;
+
+namespace Arent3d.Architecture.Routing.Electrical.App.Commands.Initialization
+{
+  [Transaction( TransactionMode.Manual )]
+  public class RegisterSymbolCommand : IExternalCommand
+  {
+    public Result Execute( ExternalCommandData commandData, ref string message, ElementSet elementSet )
+    {
+      
+      return Result.Succeeded ; 
+    }
+  }
+}
