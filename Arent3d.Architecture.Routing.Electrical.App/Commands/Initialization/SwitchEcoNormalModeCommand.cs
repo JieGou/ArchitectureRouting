51f7676671d9fe807af33dbe157989dd40cd918e
vutranarent <vu.tran@arent3d.com>
2022-03-30 13:26:25 +0700

    add SwitchEcoNormalModeDialog

--- Arent3d.Architecture.Routing.Electrical.App/Commands/Initialization/SwitchEcoNormalModeCommand.cs
+++ Arent3d.Architecture.Routing.Electrical.App/Commands/Initialization/SwitchEcoNormalModeCommand.cs
@@ -1,6 +1,8 @@
 ﻿using System.Linq ;
+using System.Windows ;
 using Arent3d.Architecture.Routing.AppBase.Commands ;
 using Arent3d.Architecture.Routing.AppBase.Commands.Initialization ;
+using Arent3d.Architecture.Routing.Electrical.App.Forms ;
 using Arent3d.Revit ;
 using Arent3d.Revit.UI ;
 using Autodesk.Revit.Attributes ;
@@ -15,28 +17,35 @@ namespace Arent3d.Architecture.Routing.Electrical.App.Commands.Initialization
   [Image( "resources/Initialize-32.bmp", ImageType = Revit.UI.ImageType.Large )]
   public class SwitchEcoNormalModeCommand: ChangeConduitModeCommandBase
   {
-    private static bool? IsEco = null;
+    private static bool? IsProjectInEcoMode = null;
     public override Result Execute( ExternalCommandData commandData, ref string message, ElementSet elements )
     {
       UiDocument = commandData.Application.ActiveUIDocument ;
       Document document = UiDocument.Document ;
-      FilteredElementCollector collector = new FilteredElementCollector(document);
-      collector = collector.OfClass(typeof(FamilyInstance)) ;
-      var conduitList = collector.ToElements().ToList();
-      collector = new FilteredElementCollector(document);
-      conduitList.AddRange( collector.OfClass(typeof( Conduit )).ToElements()) ;
-      conduitList = conduitList.Where( elem => BuiltInCategorySets.ConnectorsAndConduits.Contains( elem.GetBuiltInCategory() )).ToList();
-      collector = new FilteredElementCollector(document);
-      collector = collector.OfClass(typeof(FamilyInstance)) ;
-      var connectorList = collector.ToElements().ToList();
-      collector = new FilteredElementCollector(document);
-      connectorList.AddRange( collector.OfClass(typeof( TextNote )).ToElements()) ;
-      connectorList = connectorList.Where( elem => ( elem.GetBuiltInCategory() == BuiltInCategory.OST_ElectricalFixtures || elem.GetBuiltInCategory() == BuiltInCategory.OST_ElectricalEquipment )).ToList() ;
-      IsEco = false ;
-      var listApplyConduit = ConduitUtil.GetConduitRelated(document, conduitList) ;
-      SetModeForConduit( listApplyConduit, (bool) IsEco, document ) ;
-      SetModeForConnector( connectorList, (bool) IsEco, document ) ;
-
+      var dialog = new SwitchEcoNormalModeDialog( commandData.Application, IsProjectInEcoMode ) ;
+      dialog.ShowDialog() ;
+      if ( dialog.DialogResult == false ) return Result.Cancelled ;
+      if ( dialog.ApplyForProject == true ) {
+        FilteredElementCollector collector = new FilteredElementCollector(document);
+        collector = collector.OfClass(typeof(FamilyInstance)) ;
+        var conduitList = collector.ToElements().ToList();
+        collector = new FilteredElementCollector(document);
+        conduitList.AddRange( collector.OfClass(typeof( Conduit )).ToElements()) ;
+        conduitList = conduitList.Where( elem => BuiltInCategorySets.ConnectorsAndConduits.Contains( elem.GetBuiltInCategory() )).ToList();
+        collector = new FilteredElementCollector(document);
+        collector = collector.OfClass(typeof(FamilyInstance)) ;
+        var connectorList = collector.ToElements().ToList();
+        collector = new FilteredElementCollector(document);
+        connectorList.AddRange( collector.OfClass(typeof( TextNote )).ToElements()) ;
+        connectorList = connectorList.Where( elem => ( elem.GetBuiltInCategory() == BuiltInCategory.OST_ElectricalFixtures || elem.GetBuiltInCategory() == BuiltInCategory.OST_ElectricalEquipment )).ToList() ;
+        IsProjectInEcoMode = false ;
+        var listApplyConduit = ConduitUtil.GetConduitRelated(document, conduitList) ;
+        SetModeForConduit( listApplyConduit, (bool) IsProjectInEcoMode, document ) ;
+        SetModeForConnector( connectorList, (bool) IsProjectInEcoMode, document ) ;
+      }
+      else {
+        MessageBox.Show( "Select a range đi ông ei" ) ;
+      }
       return Result.Succeeded ;
     }
   }
---
+++ Arent3d.Architecture.Routing.Electrical.App/Forms/SwitchEcoNormalModeDialog.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System ;
+using System.Collections.Generic ;
+using System.Linq ;
+using System.Windows ;
+using System.Windows.Controls ;
+using Autodesk.Revit.UI ;
+
+namespace Arent3d.Architecture.Routing.Electrical.App.Forms
+{
+  public enum EcoNormalMode
+  {
+    EcoMode,
+    NormalMode
+  }
+  public partial class SwitchEcoNormalModeDialog
+  {
+    public SwitchEcoNormalModeDialog( UIApplication uiApplication, bool? isEcoMode ) : base( uiApplication )
+    {
+      InitializeComponent() ;
+      EcoNormalModeComboBox.SelectedItem = isEcoMode is null or true ? EcoNormalMode.EcoMode : EcoNormalMode.NormalMode ;
+    }
+    public IReadOnlyDictionary<EcoNormalMode, string> EcoNormalModes { get ; } = new Dictionary<EcoNormalMode, string>
+    {
+      [ EcoNormalMode.EcoMode ] = "エコモード",
+      [ EcoNormalMode.NormalMode] = "ノーマル",
+    } ;
+
+    public bool? ApplyForProject = null ;
+    private void Button_BtnApplyForProject_Click( object sender, RoutedEventArgs e )
+    {
+      ApplyForProject = true ;
+      DialogResult = true ;
+    }
+
+    private void Button_BtnApplyForARange_Click( object sender, RoutedEventArgs e )
+    {
+      ApplyForProject = false ;
+      DialogResult = true ;
+    }
+
+    private void Button_Cancel_Click( object sender, RoutedEventArgs e )
+    {
+      DialogResult = false ;
+    }
+
+    private EcoNormalMode EcoNormalMode = EcoNormalMode.NormalMode;
+    private void EcoNormalModeComboBox_SelectionChanged( object sender, SelectionChangedEventArgs e )
+    {
+      OnValueChanged( EventArgs.Empty ) ;
+      if ( e.RemovedItems.OfType<KeyValuePair<EcoNormalMode, string>?>().FirstOrDefault() is not { } oldValue || e.AddedItems.OfType<KeyValuePair<EcoNormalMode, string>?>().FirstOrDefault() is not { } newValue ) return ;
+      if ( oldValue.Key == newValue.Key ) return ;
+      EcoNormalMode = newValue.Key ;
+    }
+    private void OnValueChanged( EventArgs e )
+    {
+      ValueChanged?.Invoke( this, e ) ;
+    }
+    public event EventHandler? ValueChanged ;
+  }
+}
