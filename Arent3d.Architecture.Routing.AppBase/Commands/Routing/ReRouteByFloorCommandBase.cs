639f12449ad97da62f3840cd16e5414e89a13069
triarent3d <102777408+triarent3d@users.noreply.github.com>
2022-04-01 16:45:48 +0700

    Update ReRouteByFloorCommandBase.cs
    reformat code

--- Arent3d.Architecture.Routing.AppBase/Commands/Routing/ReRouteByFloorCommandBase.cs
+++ Arent3d.Architecture.Routing.AppBase/Commands/Routing/ReRouteByFloorCommandBase.cs
@@ -1,9 +1,6 @@
-using System ;
 using System.Collections.Generic ;
 using System.Linq ;
 using Arent3d.Architecture.Routing.AppBase.Forms ;
-using Arent3d.Architecture.Routing.AppBase.Manager ;
-using Arent3d.Revit.I18n ;
 using Arent3d.Revit.UI ;
 using Arent3d.Utility ;
 using Autodesk.Revit.DB ;
@@ -16,32 +13,29 @@ namespace Arent3d.Architecture.Routing.AppBase.Commands.Routing
   {
     protected abstract AddInType GetAddInType() ;
 
-    protected override OperationResult<IReadOnlyCollection<ElementId>> OperateUI( ExternalCommandData commandData, ElementSet elements )
+    protected override OperationResult<IReadOnlyCollection<ElementId>> OperateUI( ExternalCommandData commandData,
+      ElementSet elements )
     {
       var document = commandData.Application.ActiveUIDocument.Document ;
       var dialog = new GetLevel( document ) ;
-      return false == dialog.ShowDialog() 
-        ? OperationResult<IReadOnlyCollection<ElementId>>.Cancelled 
-        : new OperationResult<IReadOnlyCollection<ElementId>>( dialog.GetSelectedLevels().Select( item => item.Id ).ToList() ) ;
+      return false == dialog.ShowDialog()
+        ? OperationResult<IReadOnlyCollection<ElementId>>.Cancelled
+        : new OperationResult<IReadOnlyCollection<ElementId>>( dialog.GetSelectedLevels().Select( item => item.Id )
+          .ToList() ) ;
     }
-    
-    protected override IReadOnlyCollection<(string RouteName, RouteSegment Segment)> GetRouteSegments( Document document, IReadOnlyCollection<ElementId> levelIds)
+
+    protected override IReadOnlyCollection<(string RouteName, RouteSegment Segment)> GetRouteSegments(
+      Document document, IReadOnlyCollection<ElementId> levelIds )
     {
       RouteGenerator.CorrectEnvelopes( document ) ;
       var allConduits = new FilteredElementCollector( document ).OfClass( typeof( Conduit ) )
-        .OfCategory( BuiltInCategory.OST_Conduit )
-        .AsEnumerable()
-        .OfType<Conduit>();
+        .OfCategory( BuiltInCategory.OST_Conduit ).AsEnumerable().OfType<Conduit>() ;
       // get route names belong to selected level
       var routeNames = allConduits.Where( conduit => levelIds.Contains( conduit.ReferenceLevel.Id ) )
-                                .GroupBy( conduit => conduit.GetRouteName() )
-                                .Select( conduit => conduit.Key )
-                                .ToList() ;
-      var routes = document.CollectRoutes( GetAddInType() )
-                                                            .ToSegmentsWithName()
-                                                            .Where( segment => routeNames.Contains( segment.RouteName ) )
-                                                            .EnumerateAll() ;
-      return routes;
+        .GroupBy( conduit => conduit.GetRouteName() ).Select( conduit => conduit.Key ).ToList() ;
+      var routes = document.CollectRoutes( GetAddInType() ).ToSegmentsWithName()
+        .Where( segment => routeNames.Contains( segment.RouteName ) ).EnumerateAll() ;
+      return routes ;
     }
   }
 }
