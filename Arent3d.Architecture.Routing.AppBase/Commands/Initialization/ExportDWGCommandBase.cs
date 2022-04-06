ed90b30e45ead52364902d661ed83ed823431cdf
Mi Do Tra <midtt@rikkeisoft.com>
2022-04-06 17:47:33 +0700

    create "Export DWG" command

---
+++ Arent3d.Architecture.Routing.AppBase/Commands/Initialization/ExportDWGCommandBase.cs
@@ -0,0 +1,50 @@
+﻿using System ;
+using System.Collections.Generic ;
+using System.IO ;
+using System.Linq ;
+using System.Reflection ;
+using System.Windows.Forms ;
+using Arent3d.Architecture.Routing.AppBase.Forms ;
+using Arent3d.Revit.I18n ;
+using Arent3d.Revit.UI ;
+using Autodesk.Revit.DB ;
+using Autodesk.Revit.UI ;
+
+namespace Arent3d.Architecture.Routing.AppBase.Commands.Initialization
+{
+  public class ExportDWGCommandBase: IExternalCommand
+  {
+    public Result Execute( ExternalCommandData commandData, ref string message, ElementSet elements )
+    {
+      var document = commandData.Application.ActiveUIDocument.Document ;
+      
+      try {
+        return document.Transaction( "Electrical.App.Commands.Initialization.ExportDWGCommandBase".GetAppStringByKeyOrDefault( "Export DWG" ), _ =>
+        {
+          ExportDWG( document ) ;
+          return Result.Succeeded ;
+        } ) ;
+      }
+      catch ( Exception e ) {
+        CommandUtils.DebugAlertException( e ) ;
+        return Result.Failed ;
+      }
+    }
+
+    private void ExportDWG( Document document )
+    {
+      var activeView = document.ActiveView ;
+      OpenFileDialog openFileDialog = new() { Filter = "Setting file (*.txt)|*.txt", Multiselect = false } ;
+      string settingFilePath = string.Empty ;
+      if ( openFileDialog.ShowDialog() == DialogResult.OK ) {
+        settingFilePath = openFileDialog.FileName ;
+      }
+      
+      DWGExportOptions options = new() { LayerMapping = settingFilePath } ;
+      var filePath = Environment.GetFolderPath( Environment.SpecialFolder.MyDocuments ) ;
+      var fileName = activeView.Name + "-layer.dwg" ;
+      List<ElementId> viewIds = new() { activeView.Id } ;
+      document.Export(filePath, fileName, viewIds, options) ;
+    }
+  }
+}
--- Arent3d.Architecture.Routing.Electrical.App/RoutingAppUI.Components.cs
+++ Arent3d.Architecture.Routing.Electrical.App/RoutingAppUI.Components.cs
@@ -29,6 +29,9 @@ namespace Arent3d.Architecture.Routing.Electrical.App
 
           [Button( typeof( Show3DViewsCommand ), OnlyInitialized = true )]
           private static class Show3DViewsCommandButton { }
+          
+          [Button( typeof( ExportDWGCommand ), OnlyInitialized = true )]
+          private static class ExportDWGCommandButton { }
 
           [Button(typeof(CnsSettingCommand), OnlyInitialized = true)]
           private static class CnsSettingCommandButton { }
