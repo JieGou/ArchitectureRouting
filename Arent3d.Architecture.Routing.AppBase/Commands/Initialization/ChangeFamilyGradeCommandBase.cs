f028c545143f90553ada08f69bb596aaeb61c405
triarent3d <102777408+triarent3d@users.noreply.github.com>
2022-04-13 08:36:14 +0700

    update families of CeedStorable -> all families

--- Arent3d.Architecture.Routing.AppBase/Commands/Initialization/ChangeFamilyGradeCommandBase.cs
+++ Arent3d.Architecture.Routing.AppBase/Commands/Initialization/ChangeFamilyGradeCommandBase.cs
@@ -18,48 +18,15 @@ namespace Arent3d.Architecture.Routing.AppBase.Commands.Initialization
     {
       try {
         var document = commandData.Application.ActiveUIDocument.Document ;
-        var ceedStorable = document.GetAllStorables<CeedStorable>().FirstOrDefault() ;
-        if ( ceedStorable == null ) return Result.Failed ;
-
-        var floorPlans = ceedStorable.CeedModelData.Select( item => item.FloorPlanType ).GroupBy( item => item )
-          .Select( item => item.Key ).Where( item => ! string.IsNullOrEmpty( item ) ).ToList() ;
-
-        var connectorOneSideFamilyTypeNames =
-          ( (ConnectorOneSideFamilyType[]) Enum.GetValues( typeof( ConnectorOneSideFamilyType ) ) )
-          .Select( f => f.GetFieldName() ).ToHashSet() ;
-
-        var instances = new List<FamilyInstance>() ;
-
-        foreach ( var item in floorPlans ) {
-          // get symbols
-          var symbol = new List<FamilySymbol>() ;
-          if ( connectorOneSideFamilyTypeNames.Contains( item ) ) {
-            var connectorOneSideFamilyType = GetConnectorFamilyType( item ) ;
-            symbol.Add( document.GetFamilySymbols( connectorOneSideFamilyType ).FirstOrDefault() ??
-                        ( document.GetFamilySymbols( ElectricalRoutingFamilyType ).FirstOrDefault() ??
-                          throw new InvalidOperationException() ) ) ;
-          }
-          else {
-            if ( new FilteredElementCollector( document ).OfClass( typeof( Family ) )
-                  .FirstOrDefault( f => f.Name == item ) is Family family ) {
-              symbol.AddRange( from familySymbolId in family.GetFamilySymbolIds()
-                select document.GetElementById<FamilySymbol>( familySymbolId ) ??
-                       throw new InvalidOperationException() ) ;
-            }
-          }
-
-          // find instances
-          instances.AddRange( document.GetAllFamilyInstances( symbol )
-            .Where( instance => instance.HasParameter( "グレード3" ) ).ToList() ) ;
-        }
-
+        var instances = new FilteredElementCollector( document ).OfClass( typeof( FamilyInstance ) )
+          .Cast<FamilyInstance>().Where( a => a.HasParameter( "グレード3" ) ).ToList() ;
         // select mode
         var isInGrade3Mode = instances.Any( item => item.GetPropertyBool( "グレード3" ) ) ;
         var dialog = new ChangeFamilyGradeDialog( commandData.Application, isInGrade3Mode ) ;
         dialog.ShowDialog() ;
         if ( dialog.DialogResult == false ) return Result.Cancelled ;
         isInGrade3Mode = dialog.SelectedMode == GradeMode.Grade3 ;
-        
+
         // update property グレード3 of instances
         using Transaction t = new(document, "Update grade") ;
         t.Start() ;
