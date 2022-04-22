57eba17e607ca2bb368ebb51f92ae8af94977f3a
anhtv <anhtv@rikkeisoft.com>
2022-04-22 16:44:02 +0700

    break new line for name display

--- Arent3d.Architecture.Routing.Electrical.App/Commands/Initialization/ShowRegistrationOfBoardDataCommand.cs
+++ Arent3d.Architecture.Routing.Electrical.App/Commands/Initialization/ShowRegistrationOfBoardDataCommand.cs
@@ -5,7 +5,7 @@ using Autodesk.Revit.Attributes ;
 namespace Arent3d.Architecture.Routing.Electrical.App.Commands.Initialization
 {
   [Transaction( TransactionMode.Manual )]
-  [DisplayNameKey( "Electrical.App.Commands.Initialization.RegistrationOfBoardDataCommand", DefaultString = "Registration of\nboard data" )]
+  [DisplayNameKey( "Electrical.App.Commands.Initialization.RegistrationOfBoardDataCommand", DefaultString = "Registration Of\nBoard Data" )]
   [Image( "resources/Initialize-32.bmp", ImageType = ImageType.Large )]
   public class ShowRegistrationOfBoardDataCommand : ShowRegistrationOfBoardDataCommandBase
   {
