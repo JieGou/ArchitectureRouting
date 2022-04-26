661b2fd97b073ea793e62b8abc0656f1f5d81262
anhnv11 <anhnv@hachinet.com>
2022-04-26 16:13:46 +0700

    - Restructure View, ViewModel - Change: Do not apply mode for the project when change default mode - Use enums to catch user choices (apply default mode, apply for project, apply for a range)

---
+++ Arent3d.Architecture.Routing.AppBase/Forms/SwitchEcoNormalModeDialog.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System ;
+using System.Collections.Generic ;
+using System.Linq ;
+using System.Windows ;
+using System.Windows.Controls ;
+using Arent3d.Architecture.Routing.AppBase.ViewModel ;
+using Arent3d.Utility ;
+
+namespace Arent3d.Architecture.Routing.AppBase.Forms
+{ 
+  public partial class SwitchEcoNormalModeDialog
+  { 
+    public SwitchEcoNormalModeViewModel ViewModel => (SwitchEcoNormalModeViewModel)DataContext ;
+    public SwitchEcoNormalModeDialog( SwitchEcoNormalModeViewModel switchEcoNormalModeViewModel ) : base()
+    {
+      InitializeComponent() ;
+      DataContext = switchEcoNormalModeViewModel ;
+    }
+ 
+    private void Button_Cancel_Click( object sender, RoutedEventArgs e )
+    {
+      DialogResult = false ;
+    }
+    
+    private void Button_OK_Click( object sender, RoutedEventArgs e )
+    {
+      DialogResult = true ;
+    }
+    
+  }
+  
+  public class DesignSwitchEcoNormalModeViewModel : SwitchEcoNormalModeViewModel
+  { 
+  }
+}
---
+++ Arent3d.Architecture.Routing.AppBase/ViewModel/SwitchEcoNormalModeViewModel.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic ;
+using System.Windows.Input ;
+using Arent3d.Architecture.Routing.AppBase.Commands.Routing ;
+using Arent3d.Revit.I18n ;
+
+namespace Arent3d.Architecture.Routing.AppBase.ViewModel
+{
+  public class SwitchEcoNormalModeViewModel: NotifyPropertyChanged
+  {  
+    private const string EcoModeKey = "Dialog.Electrical.SwitchEcoNormalModeDialog.EcoNormalMode.EcoMode" ;
+    private const string EcoModeDefaultString = "Eco Mode" ;
+    private const string NormalModeKey = "Dialog.Electrical.SwitchEcoNormalModeDialog.EcoNormalMode.NormalMode" ;
+    private const string NormalModeDefaultString = "Normal Mode" ;
+    
+    public SwitchEcoDialogEnumResult SwitchEcoDialogResult ;
+    public int SelectedEcoNormalModeIndex { get ; set ; } = 0 ;
+    public EcoNormalMode SelectedEcoNormalModeItem  => 0 == SelectedEcoNormalModeIndex ? EcoNormalMode.NormalMode: EcoNormalMode.EcoMode ;
+    public enum  SwitchEcoDialogEnumResult
+    {
+      SetDefault,
+      ApplyAllProject,
+      ApplyRange
+    }
+    
+    public enum EcoNormalMode
+    {
+      EcoMode,
+      NormalMode
+    }
+     
+    public ICommand SetDefaultCommand => new RelayCommand( SetDefault ) ;
+    public ICommand ApplyAllProjectCommand => new RelayCommand( ApplyAllProject ) ;
+    public ICommand ApplyRangeCommand => new RelayCommand( ApplyRange ) ;
+
+    public IReadOnlyDictionary<EcoNormalMode, string> EcoNormalModes { get ; } = new Dictionary<EcoNormalMode, string>
+    {
+      [ EcoNormalMode.NormalMode ] = NormalModeKey.GetAppStringByKeyOrDefault(NormalModeDefaultString),
+      [ EcoNormalMode.EcoMode ] = EcoModeKey.GetAppStringByKeyOrDefault(EcoModeDefaultString)
+    } ;
+     
+    private void SetDefault( )
+    {
+      SwitchEcoDialogResult = SwitchEcoDialogEnumResult.SetDefault ;
+    }
+    
+    private void ApplyAllProject( )
+    {
+      SwitchEcoDialogResult = SwitchEcoDialogEnumResult.ApplyAllProject ;
+    }
+    
+    private void ApplyRange( )
+    {
+      SwitchEcoDialogResult = SwitchEcoDialogEnumResult.ApplyRange ;
+    }
+  }
+}
