4d1bf6df688d11486601c97fcfd40bd9579ab283
anhtv <anhtv@rikkeisoft.com>
2022-05-19 17:30:52 +0700

    Load data from file and save data when edit on UI

--- Arent3d.Architecture.Routing.AppBase/Commands/Initialization/ExportDWGCommandBase.cs
+++ Arent3d.Architecture.Routing.AppBase/Commands/Initialization/ExportDWGCommandBase.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic ;
 using System.IO ;
 using System.Linq ;
 using System.Reflection ;
+using System.Text ;
 using System.Windows.Forms ;
 using Arent3d.Architecture.Routing.AppBase.Forms ;
+using Arent3d.Architecture.Routing.AppBase.ViewModel ;
 using Arent3d.Revit.I18n ;
 using Arent3d.Revit.UI ;
 using Autodesk.Revit.DB ;
@@ -42,9 +44,73 @@ namespace Arent3d.Architecture.Routing.AppBase.Commands.Initialization
       string directory = Path.GetDirectoryName( Assembly.GetExecutingAssembly().Location ) ! ;
       var resourcesPath = Path.Combine( directory.Substring( 0, directory.IndexOf( "bin", StringComparison.Ordinal ) ), "resources" ) ;
       string settingFilePath = Path.Combine( resourcesPath, layerSettingsFileName ) ;
+      var names  =  GetLayerNames( settingFilePath ) ;
+      var oldNames = names.Select( x => x.Copy() ).ToList() ;
+      var viewModel = new LayerNameSettingViewModel( names ) ;
+      var dlgLayerNameSettingDialog= new LayerNameSettingDialog( viewModel ) ;
+      dlgLayerNameSettingDialog.ShowDialog() ;
+      if (  dlgLayerNameSettingDialog.DialogResult == true  ) ReplaceLayerNames( oldNames , names, settingFilePath, GetEncoding(settingFilePath) ) ;
+      
       DWGExportOptions options = new() { LayerMapping = settingFilePath } ;
       List<ElementId> viewIds = new() { activeView.Id } ;
       document.Export( filePath, fileName, viewIds, options ) ;
+    }
+
+    private List<Layer> GetLayerNames( string filePath )
+    {
+      const string exceptString = "Ifc" ;
+      List<Layer> names = new List<Layer>() ;
+      using ( StreamReader reader = File.OpenText(filePath) ) {
+        string line ;
+        while ( ( line = reader.ReadLine() ) != null ) {
+          if ( line[ 0 ] != '#' ) {
+            var words = line.Split( '\t' ) ;
+            var name = words[ 2 ] ;
+            if ( name != "" && !name.Contains( exceptString ) && names.FirstOrDefault( c => c.Name == name ) == null ) {
+              names.Add( new Layer( name ) ) ;
+            }
+          }
+        }
+      
+        reader.Close() ;
+      }
+
+      return names ;
+    }
+    
+    private Encoding GetEncoding( string filename )
+    {
+      var bom = new byte[ 4 ] ;
+      using ( var file = new FileStream( filename, FileMode.Open, FileAccess.Read ) ) {
+        file.Read( bom, 0, 4 ) ;
+      }
+
+      if ( bom[ 0 ] == 0x2b && bom[ 1 ] == 0x2f && bom[ 2 ] == 0x76 ) return Encoding.UTF7 ;
+      if ( bom[ 0 ] == 0xef && bom[ 1 ] == 0xbb && bom[ 2 ] == 0xbf ) return Encoding.UTF8 ;
+      if ( bom[ 0 ] == 0xff && bom[ 1 ] == 0xfe && bom[ 2 ] == 0 && bom[ 3 ] == 0 ) return Encoding.UTF32 ; //UTF-32LE
+      if ( bom[ 0 ] == 0xff && bom[ 1 ] == 0xfe ) return Encoding.Unicode ; //UTF-16LE
+      if ( bom[ 0 ] == 0xfe && bom[ 1 ] == 0xff ) return Encoding.BigEndianUnicode ; //UTF-16BE
+      if ( bom[ 0 ] == 0 && bom[ 1 ] == 0 && bom[ 2 ] == 0xfe && bom[ 3 ] == 0xff ) return new UTF32Encoding( true, true ) ; //UTF-32BE
+
+      return Encoding.ASCII ;
+    }
+
+    private void ReplaceLayerNames( List<Layer> oldLayerNames, List<Layer> newLayerNames, string filePath, Encoding encoding)
+    {
+      try {
+        bool hasChange = false ;
+        string text = File.ReadAllText( filePath ) ;
+        for ( int i = 0 ; i < newLayerNames.Count() ; i++ ) {
+          if ( oldLayerNames[ i ].Name != newLayerNames[ i ].Name ) {
+            hasChange = true ;
+            text = text.Replace( oldLayerNames[ i ].Name, newLayerNames[ i ].Name ) ;
+          }
+        }
+        if(hasChange) File.WriteAllText( filePath, text, encoding ) ;
+      }
+      catch ( Exception e ) {
+        MessageBox.Show( e.Message, "Error" ) ;
+      }
     }
   }
 }
