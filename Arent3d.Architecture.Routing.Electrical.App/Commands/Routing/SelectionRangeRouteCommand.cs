13eaf240cb24e2e65a25bfb1662acafd6c008518
Mi Do Tra <midtt@rikkeisoft.com>
2022-06-16 17:38:32 +0700

    create pull box after selection range route

--- Arent3d.Architecture.Routing.Electrical.App/Commands/Routing/SelectionRangeRouteCommand.cs
+++ Arent3d.Architecture.Routing.Electrical.App/Commands/Routing/SelectionRangeRouteCommand.cs
@@ -1,7 +1,10 @@
 ﻿using System.Collections.Generic ;
+using System.Linq ;
 using Arent3d.Architecture.Routing.AppBase ;
 using Arent3d.Architecture.Routing.AppBase.Commands.Routing ;
+using Arent3d.Architecture.Routing.AppBase.Manager ;
 using Arent3d.Architecture.Routing.EndPoints ;
+using Arent3d.Revit.I18n ;
 using Arent3d.Revit.UI ;
 using Autodesk.Revit.Attributes ;
 using Autodesk.Revit.DB ;
@@ -53,6 +56,26 @@ namespace Arent3d.Architecture.Routing.Electrical.App.Commands.Routing
     protected override void AfterRouteGenerated( Document document, IReadOnlyCollection<Route> executeResultValue, SelectState selectState )
     {
       ElectricalCommandUtil.SetPropertyForCable( document, executeResultValue ) ;
+    }
+    
+    protected override void CreatePullBoxAfterRouteGenerated( Document document, RoutingExecutor executor, IReadOnlyCollection<Route> executeResultValue )
+    {
+      using var progress = ShowProgressBar( "Routing...", false ) ;
+      var routeNames = executeResultValue.Select( r => r.RouteName ).Distinct().ToHashSet() ;
+      while ( true ) {
+        var segments = PullBoxRouteManager.GetSegmentsWithPullBox( document, executeResultValue ) ;
+        if ( ! segments.Any() ) break ;
+        using Transaction transaction = new( document ) ;
+        transaction.Start( "TransactionName.Commands.Routing.Common.Routing".GetAppStringByKeyOrDefault( "Routing" ) ) ;
+        try {
+          var result = executor.Run( segments, progress ) ;
+          executeResultValue = result.Value.Where( r => routeNames.Contains( r.RouteName ) ).ToList() ;
+        }
+        catch {
+          break ;
+        }
+        transaction.Commit() ;
+      }
     }
   }
 }
