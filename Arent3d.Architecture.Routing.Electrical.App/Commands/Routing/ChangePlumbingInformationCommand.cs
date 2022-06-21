a5b3c0663ad37a0d5bdef954ed20a584f3415389
TuanAnhArent <107823090+TuanAnhArent@users.noreply.github.com>
2022-06-21 15:00:03 +0700

    Fix bug Change Plumbing Info: Connectors (without set code) case

--- Arent3d.Architecture.Routing.Electrical.App/Commands/Routing/ChangePlumbingInformationCommand.cs
+++ Arent3d.Architecture.Routing.Electrical.App/Commands/Routing/ChangePlumbingInformationCommand.cs
@@ -167,11 +167,11 @@ namespace Arent3d.Architecture.Routing.Electrical.App.Commands.Routing
           : classificationOfPlumbingNames.First() ;
         var wireCrossSectionalArea = oldChangePlumbingInformationModel?.WireCrossSectionalArea ?? 0 ;
         var ceedCodeInfo = ElectricalCommandUtil.GetCeedSetCodeOfElement( connector ) ;
-        var ceedCode = ceedCodeInfo.First() ;
+        var ceedCode = ceedCodeInfo.FirstOrDefault() ;
         var deviceSymbol = ceedCodeInfo.Count > 1 ? ceedCodeInfo.ElementAt( 1 ) : string.Empty ;
         var modelNumber = ceedCodeInfo.Count > 2 ? ceedCodeInfo.ElementAt( 2 ) : string.Empty ;
-        var ceedModel = ceedModels.FirstOrDefault( c => c.CeedSetCode == ceedCode && c.GeneralDisplayDeviceSymbol == deviceSymbol && c.ModelNumber == modelNumber ) ;
-        var registrationOfBoardDataModel = registrationOfBoardDataModels.FirstOrDefault( b => b.AutoControlPanel == ceedCode || b.SignalDestination == ceedCode ) ;
+        var ceedModel = ceedModels.FirstOrDefault( c => ceedCode != null && c.CeedSetCode == ceedCode && c.GeneralDisplayDeviceSymbol == deviceSymbol && c.ModelNumber == modelNumber ) ;
+        var registrationOfBoardDataModel = registrationOfBoardDataModels.FirstOrDefault( b => ceedCode != null && (b.AutoControlPanel == ceedCode || b.SignalDestination == ceedCode) ) ;
         var isInDoor = oldChangePlumbingInformationModel?.IsInDoor ?? true ;
         if ( ! string.IsNullOrEmpty( ceedCode ) ) {
           List<string> materialCodes = new() ;
