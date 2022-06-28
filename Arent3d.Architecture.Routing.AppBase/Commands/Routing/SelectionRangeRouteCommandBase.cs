529e148dbf27ecc807eadb0bb649a977c16eae6a
anhtv <anhtv@rikkeisoft.com>
2022-06-28 17:22:50 +0700

    change height direction x samller direction y if intersect

--- Arent3d.Architecture.Routing.AppBase/Commands/Routing/SelectionRangeRouteCommandBase.cs
+++ Arent3d.Architecture.Routing.AppBase/Commands/Routing/SelectionRangeRouteCommandBase.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic ;
+using System.Collections.ObjectModel ;
 using System.Linq ;
 using System.Windows ;
 using Arent3d.Architecture.Routing.AppBase.Commands.Initialization ;
@@ -13,6 +14,7 @@ using Arent3d.Revit.UI ;
 using Autodesk.Revit.DB ;
 using Autodesk.Revit.UI ;
 using MathLib ;
+using Arent3d.Architecture.Routing.AppBase.Extensions ;
 
 namespace Arent3d.Architecture.Routing.AppBase.Commands.Routing
 {
@@ -90,14 +92,19 @@ namespace Arent3d.Architecture.Routing.AppBase.Commands.Routing
       var result = new List<(string RouteName, RouteSegment Segment)>() ;
       // Powerコネクタの数が1の場合、（Power, Sensors）の間の配線を行う
       if ( powerConnectors.Count() == 1 ) return CreateRouteSegments( document, selectState, powerConnectors.First(), ref nextIndex ) ;
-
+      
       // Powerコネクタの数が2以上ある場合、（自動制御盤, 信号取り合い先）の間の配線を行う
       var heightDifference = 10.0.MillimetersToRevitUnits() ; // 各配線の高さの差
-      var listDictionaryFromToOfBoard = GetListDictionaryFromToOfBoard( powerConnectors, document ) ;
+      var listDictionaryFromToOfBoard = GetListDictionaryFromToOfBoard( powerConnectors, document ).ToList() ;
+      
+      ChangeElementPositionOfDictionaryFromToBoards( listDictionaryFromToOfBoard ) ;
+
+      // Create route segments for power board
       foreach ( var dictionaryFromToOfBoard in listDictionaryFromToOfBoard ) {
         var powerConnector = dictionaryFromToOfBoard.Key ;
         var powerToConnectors = dictionaryFromToOfBoard.Value ;
         var powerToDirection = SelectionRangeRouteManager.SortSensorConnectors( powerConnector.GetTopConnectorOfConnectorFamily().Origin, ref powerToConnectors ) ;
+        
         if ( SelectionRangeRouteManager.SensorArrayDirection.Invalid == powerToDirection ) {
           MessageBox.Show( ErrorMessageCannotDeterminePowerConnectorArrayDirection ) ;
           return result ;
@@ -112,6 +119,52 @@ namespace Arent3d.Architecture.Routing.AppBase.Commands.Routing
       return result ;
     }
 
+    private void ChangeElementPositionOfDictionaryFromToBoards( List<KeyValuePair<FamilyInstance, List<FamilyInstance>>> listDictionaryFromToOfBoard )
+    {
+      for ( int i = 0 ; i < listDictionaryFromToOfBoard.Count() ; i++ ) {
+        var powerConnector = listDictionaryFromToOfBoard[i].Key ;
+        var powerToConnectors = listDictionaryFromToOfBoard[i].Value ;
+        var powerToDirection = SelectionRangeRouteManager.SortSensorConnectors( powerConnector.GetTopConnectorOfConnectorFamily().Origin, ref powerToConnectors ) ;
+        var farthestPowerConnector = powerToConnectors.LastOrDefault() ;
+        for ( int j = i + 1 ; j < listDictionaryFromToOfBoard.Count() ; j++ ) {
+          var powerConnectorNext = listDictionaryFromToOfBoard[j].Key ;
+          var powerToConnectorsNext = listDictionaryFromToOfBoard[j].Value ;
+          var powerToDirectionNext = SelectionRangeRouteManager.SortSensorConnectors( powerConnectorNext.GetTopConnectorOfConnectorFamily().Origin, ref powerToConnectorsNext ) ;
+          var farthestPowerConnectorNext = powerToConnectorsNext.LastOrDefault() ;
+
+          var condition1 =
+            ( ( SelectionRangeRouteManager.SensorArrayDirection.XMinus == powerToDirectionNext || SelectionRangeRouteManager.SensorArrayDirection.XPlus == powerToDirectionNext ) &&
+              ( SelectionRangeRouteManager.SensorArrayDirection.YMinus == powerToDirection || SelectionRangeRouteManager.SensorArrayDirection.YPlus == powerToDirection ) ) ;
+        
+          var condition2 = IsIntersect( powerConnector, farthestPowerConnector!, powerConnectorNext, farthestPowerConnectorNext! ) ;
+          if ( condition1 && condition2 )
+          {
+            listDictionaryFromToOfBoard.Swap( i, j ) ;
+          }
+        }
+      }
+    }
+
+
+    private bool IsIntersect(FamilyInstance powerFrom1, FamilyInstance powerTo1, FamilyInstance powerFrom2, FamilyInstance powerTo2)
+    {
+      var positionFrom1 = (powerFrom1.Location as LocationPoint)?.Point ; // A
+      var positionTo1 = (powerTo1.Location as LocationPoint)?.Point;     // B
+      
+      var positionFrom2 = (powerFrom2.Location as LocationPoint)?.Point ; // C
+      var positionTo2 = (powerTo2.Location as LocationPoint)?.Point ; // D
+      
+      if ( ThreePointOrientation( positionFrom2!, positionTo2!, positionFrom1! ) != ThreePointOrientation( positionFrom2!, positionTo2!, positionTo1! ) &&
+           ThreePointOrientation( positionFrom1!, positionTo1!, positionFrom2! ) != ThreePointOrientation( positionFrom1!, positionTo1!, positionTo2! ) ) return true ;
+      
+      return false ;
+    }
+    
+    private string ThreePointOrientation(XYZ a, XYZ b, XYZ c) {
+      double check = (b.Y - a.Y) * (c.X - b.X) - (c.Y - b.Y) * (b.X - a.X);
+      return check > 0 ? "clockwise" : "counterclockwise";
+    }
+
     private Dictionary<FamilyInstance, List<FamilyInstance>> GetListDictionaryFromToOfBoard( IReadOnlyCollection<FamilyInstance> powerConnectors, Document document )
     {
       var registrationOfBoardDataModels = document.GetRegistrationOfBoardDataStorable().RegistrationOfBoardData ;
@@ -122,7 +175,6 @@ namespace Arent3d.Architecture.Routing.AppBase.Commands.Routing
         element.TryGetProperty( ElectricalRoutingElementParameter.CeedCode, out string? ceedCodeOfFromConnector ) ;
         if ( ! string.IsNullOrEmpty( ceedCodeOfFromConnector ) && listDictionaryBoard.TryGetValue( ceedCodeOfFromConnector!, out var dictionaryBoard ) ) {
           var toConnectors = powerConnectors.Where( x => x.TryGetProperty( ElectricalRoutingElementParameter.CeedCode, out string? ceedCodeOfToConnector ) && dictionaryBoard.Any( b => b.SignalDestination == ceedCodeOfToConnector ) ).ToList() ;
-
           if ( toConnectors.Any() )
             result.Add( element, toConnectors ) ;
         }
---
+++ Arent3d.Architecture.Routing.AppBase/Extensions/Extensions.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic ;
+
+namespace Arent3d.Architecture.Routing.AppBase.Extensions
+{
+  public static class Extensions
+  {
+    public static void Swap<T>(this List<T> list, int i, int j)
+    {
+      ( list[i], list[j] ) = ( list[j], list[i] ) ;
+    }
+  }
+}
