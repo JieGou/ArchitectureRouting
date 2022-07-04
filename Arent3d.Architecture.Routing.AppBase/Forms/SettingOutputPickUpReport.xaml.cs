5953890576a1953d78a47bb2410217fb36ea6658
anhtv <anhtv@rikkeisoft.com>
2022-07-04 17:21:22 +0700

    add setting output pick up report dialog

---
+++ Arent3d.Architecture.Routing.AppBase/Forms/SettingOutputPickUpReport.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows ;
+using System.Windows.Controls ;
+using Arent3d.Architecture.Routing.AppBase.ViewModel ;
+using Autodesk.Revit.DB ;
+
+namespace Arent3d.Architecture.Routing.AppBase.Forms
+{
+  public partial class SettingOutputPickUpReport : Window
+  {
+    private PickUpReportViewModel ViewModel => (PickUpReportViewModel)DataContext ;
+    
+    public SettingOutputPickUpReport(PickUpReportViewModel viewModel )
+    {
+      InitializeComponent() ;
+      DataContext = viewModel ;
+    }
+  }
+  
+  public class DesignSettingPickUpReportViewModel : PickUpReportViewModel
+  {
+    public DesignSettingPickUpReportViewModel( Document document ) : base( default! )
+    {
+    }
+  }
+}
+
