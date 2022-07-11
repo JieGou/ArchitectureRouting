623970fe6a072dc645305ed1d9468f491152152b
TuanAnhArent <107823090+TuanAnhArent@users.noreply.github.com>
2022-07-11 15:13:28 +0700

    rename function SwitchPrintMode

--- Arent3d.Architecture.Routing.Electrical.App/Commands/Initialization/SwitchPrintModeCommand.cs
+++ Arent3d.Architecture.Routing.Electrical.App/Commands/Initialization/SwitchPrintModeCommand.cs
@@ -5,7 +5,7 @@ using Autodesk.Revit.Attributes ;
 namespace Arent3d.Architecture.Routing.Electrical.App.Commands.Initialization
 {
   [Transaction( TransactionMode.Manual )]
-  [DisplayNameKey( "Electrical.App.Commands.Initialization.SwitchPrintModeCommand", DefaultString = "Switch Print Mode" )]
+  [DisplayNameKey( "Electrical.App.Commands.Initialization.SwitchPrintModeCommand", DefaultString = "Switch to\nPrint Mode" )]
   [Image( "resources/Initialize-32.bmp", ImageType = ImageType.Large )]
   public class SwitchPrintModeCommand : SwitchPrintModeCommandBase
   {
