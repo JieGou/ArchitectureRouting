34d164d09c48f2c14c59203ec41b9129ca266bbf
nhutruong-arent <nhu.truong@arent3d.com>
2022-07-13 21:10:37 +0700

    Delete demo storage

--- Arent3d.Architecture.Routing/Extensions/DocumentExtensions.cs
+++ Arent3d.Architecture.Routing/Extensions/DocumentExtensions.cs
@@ -345,16 +345,6 @@ namespace Arent3d.Architecture.Routing.Extensions
         return new ChangePlumbingInformationStorable( document ) ;
       }
     }
-    
-    public static DemoStorable GetDemoStorable( this Document document )
-    {
-      try {
-        return DemoStorableCache.Get( DocumentKey.Get( document ) ).FindOrCreate( DemoStorable.StorableName ) ;
-      }
-      catch ( InvalidOperationException ) {
-        return new DemoStorable( document ) ;
-      }
-    }
-    
+
   }
 }
