05a7de54ba61e3621dbd59b1817f32d685cb21a0
nhutruong-arent <nhu.truong@arent3d.com>
2022-07-14 11:31:21 +0700

    Change UserName to LoginUserId

--- Arent3d.Architecture.Routing/ExtensibleStorages/Extensions/DataStorageExtension.cs
+++ Arent3d.Architecture.Routing/ExtensibleStorages/Extensions/DataStorageExtension.cs
@@ -11,27 +11,27 @@ namespace Arent3d.Architecture.Routing.ExtensibleStorages.Extensions
     {
         public static DataStorage FindOrCreateDataStorageForUser( this Document document )
         {
-            if ( string.IsNullOrEmpty( document.Application.Username ) )
+            if ( string.IsNullOrEmpty( document.Application.LoginUserId ) )
                 throw new InvalidDataException( "Please login to Revit." ) ;
-        
-            var dataStorage = document.GetAllInstances<DataStorage>().SingleOrDefault( x => x.Name == $"{AppInfo.VendorId}_{document.Application.Username}" ) ;
+
+            var dataStorage = document.GetAllInstances<DataStorage>().SingleOrDefault( x => x.Name.StartsWith( AppInfo.VendorId ) && x.Name.EndsWith( document.Application.LoginUserId ) ) ;
             if ( null != dataStorage )
                 return dataStorage ;
 
             dataStorage = DataStorage.Create( document ) ;
-            dataStorage.Name = $"{AppInfo.VendorId}_{document.Application.Username}" ;
+            dataStorage.Name = $"{AppInfo.VendorId}-{document.Application.Username}-{document.Application.LoginUserId}" ;
             return dataStorage ;
         }
 
         public static IEnumerable<DataStorage> GetDataStorageUsers( this Document document )
         {
-            return document.GetAllInstances<DataStorage>(x => x.Name.StartsWith(AppInfo.VendorId)) ;
+            return document.GetAllInstances<DataStorage>( x => x.Name.StartsWith( AppInfo.VendorId ) ) ;
         }
 
-        public static IEnumerable<TDataModel> GetAllData<TDataModel>(this Document document) where TDataModel : class, IDataModel
+        public static IEnumerable<TDataModel> GetAllData<TDataModel>( this Document document ) where TDataModel : class, IDataModel
         {
             var dataStorages = GetDataStorageUsers( document ).ToList() ;
-            return !dataStorages.Any() ? Enumerable.Empty<TDataModel>() : dataStorages.Select( x => x.GetData<TDataModel>() ).OfType<TDataModel>() ;
+            return ! dataStorages.Any() ? Enumerable.Empty<TDataModel>() : dataStorages.Select( x => x.GetData<TDataModel>() ).OfType<TDataModel>() ;
         }
     }
 }
