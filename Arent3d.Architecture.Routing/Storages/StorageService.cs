dc17313581f8d6391cb0e496fba877768fa3b239
nhutruong-arent <nhu.truong@arent3d.com>
2022-07-18 20:04:26 +0700

    Add storage service

---
+++ Arent3d.Architecture.Routing/Storages/StorageService.cs
@@ -0,0 +1,32 @@
+﻿using System ;
+using System.Collections.Generic ;
+using Arent3d.Architecture.Routing.Storages.Extensions ;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.ExtensibleStorage ;
+
+namespace Arent3d.Architecture.Routing.Storages
+{
+    public class StorageService<TDataModel> where TDataModel : class, IDataModel
+    {
+        public DataStorage Owner { get ; }
+        public TDataModel Data { get ;  }
+
+        private IEnumerable<(Element Owner, TDataModel Data)>? _dataStorages ;
+        private IEnumerable<(Element Owner, TDataModel Data)> DataStorages
+        {
+            get { return _dataStorages ??= Owner.Document.GetAllDataStorage<TDataModel>() ; }
+        }
+
+        public StorageService(Document document, bool isForUser)
+        {
+            Owner = document.FindOrCreateDataStorage<TDataModel>( isForUser ) ;
+            Data = Owner.GetData<TDataModel>() ?? Activator.CreateInstance<TDataModel>() ;
+        }
+
+        public void SaveChange()
+        {
+            Owner.SetData(Data);
+        }
+        
+    }
+}
