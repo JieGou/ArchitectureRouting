81b5c387d7070c1983da206438e371cb0bc5661d
Mi Do Tra <midtt@rikkeisoft.com>
2022-07-26 17:01:44 +0700

    reroute all related branch route and change representative route name of sub route

--- Arent3d.Architecture.Routing.Electrical.App/Commands/Routing/PickAndReRouteCommand.cs
+++ Arent3d.Architecture.Routing.Electrical.App/Commands/Routing/PickAndReRouteCommand.cs
@@ -25,7 +25,12 @@ namespace Arent3d.Architecture.Routing.Electrical.App.Commands.Routing
     {
       ElectricalCommandUtil.SetPropertyForCable( document, executeResultValue ) ;
       var reRouteNames = executeResultValue.Select( r => r.RouteName ).Distinct().ToHashSet() ;
-      var (_, oldConduitIds) = reRouteState ;
+      var (_, oldConduitIds, routeNameDictionary) = reRouteState ;
+
+      if ( routeNameDictionary.Any() ) {
+        RouteGenerator.ChangeRepresentativeRouteName( document, routeNameDictionary ) ;
+      }
+
       var ( wireTypeName, isLeakRoute ) = ChangeWireTypeCommand.RemoveDetailLines( document, oldConduitIds ) ;
       if ( string.IsNullOrEmpty( wireTypeName ) ) return ;
       ChangeWireTypeCommand.ChangeWireType( document, reRouteNames, wireTypeName, isLeakRoute ) ;
