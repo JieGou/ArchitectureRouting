67a469d754c8aa101b666978bbb6d6d57feec022
Mi Do Tra <midtt@rikkeisoft.com>
2022-07-27 15:26:20 +0700

    fix bug sort conduit fitting

--- Arent3d.Architecture.Routing.AppBase/Manager/PullBoxRouteManager.cs
+++ Arent3d.Architecture.Routing.AppBase/Manager/PullBoxRouteManager.cs
@@ -1028,6 +1028,7 @@ namespace Arent3d.Architecture.Routing.AppBase.Manager
     {
       var sortConduits = new List<Element>() ;
       var toEndPoint = route.FirstToConnector()?.RoutingStartPosition ;
+      if ( toEndPoint == null ) return ;
       while ( sortConduits.Count != conduits.Count ) {
         var minDistance = double.MaxValue ;
         var nearestConduit = conduits.First() ;
