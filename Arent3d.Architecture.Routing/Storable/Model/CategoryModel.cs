90c3d41f2ba72fd601d0264642b5a0b73f603dc3
Mi Do Tra <midtt@rikkeisoft.com>
2022-08-04 15:46:21 +0700

    create category model to save in storable

---
+++ Arent3d.Architecture.Routing/Storable/Model/CategoryModel.cs
@@ -0,0 +1,18 @@
+﻿namespace Arent3d.Architecture.Routing.Storable.Model
+{
+  public class CategoryModel
+  {
+    public string Name { get ; }
+    public string ParentName { get ; }
+    public bool IsExpanded { get ; }
+    public bool IsSelected { get ; }
+
+    public CategoryModel( string? name, string? parentName, bool? isExpanded, bool? isSelected )
+    {
+      Name = name ?? string.Empty ;
+      ParentName = parentName ?? string.Empty ;
+      IsExpanded = isExpanded ?? false ;
+      IsSelected = isSelected ?? false ;
+    }
+  }
+}
---
+++ Arent3d.Architecture.Routing/Storable/StorableConverter/CategoryModelStorableConvert.cs
@@ -0,0 +1,43 @@
+﻿using Arent3d.Architecture.Routing.Storable.Model ;
+using Arent3d.Revit ;
+using Arent3d.Utility.Serialization ;
+using Autodesk.Revit.DB ;
+
+namespace Arent3d.Architecture.Routing.Storable.StorableConverter
+{
+  [StorableConverterOf( typeof( CategoryModel ) )]
+  public class CategoryModelStorableConvert : StorableConverterBase<CategoryModel>
+  {
+    private enum SerializeField
+    {
+      Name,
+      ParentName,
+      IsExpanded,
+      IsSelected
+    }
+    
+    protected override CategoryModel Deserialize( Element storedElement, IDeserializerObject deserializerObject )
+    {
+      var deserializer = deserializerObject.Of<SerializeField>() ;
+      
+      var name = deserializer.GetString( SerializeField.Name ) ;
+      var parentName = deserializer.GetString( SerializeField.ParentName ) ;
+      var isExpanded = deserializer.GetBool( SerializeField.IsExpanded ) ;
+      var isSelected = deserializer.GetBool( SerializeField.IsSelected ) ;
+
+      return new CategoryModel( name, parentName, isExpanded, isSelected ) ;
+    }
+
+    protected override ISerializerObject Serialize( Element storedElement, CategoryModel customTypeValue )
+    {
+      var serializerObject = new SerializerObject<SerializeField>() ;
+      
+      serializerObject.AddNonNull( SerializeField.Name, customTypeValue.Name ) ;
+      serializerObject.AddNonNull( SerializeField.ParentName, customTypeValue.ParentName ) ;
+      serializerObject.Add( SerializeField.IsExpanded, customTypeValue.IsExpanded ) ;
+      serializerObject.Add( SerializeField.IsSelected, customTypeValue.IsSelected ) ;
+
+      return serializerObject ;
+    }
+  }
+}
