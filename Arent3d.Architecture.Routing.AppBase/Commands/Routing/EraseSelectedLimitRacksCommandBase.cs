025a43eeef93dddcf65e3da8eb8f9bcbe2804343
NguyenPhuongLam <106937783+NguyenPhuongLam@users.noreply.github.com>
2022-08-09 10:10:22 +0700

    Update logic when delete limit rack in 2d

--- Arent3d.Architecture.Routing.AppBase/Commands/Routing/EraseSelectedLimitRacksCommandBase.cs
+++ Arent3d.Architecture.Routing.AppBase/Commands/Routing/EraseSelectedLimitRacksCommandBase.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic ;
 using System.Linq ;
+using System.Windows ;
 using Arent3d.Architecture.Routing.AppBase.Selection ;
 using Arent3d.Architecture.Routing.Storable ;
 using Arent3d.Architecture.Routing.Storable.Model ;
@@ -19,26 +20,41 @@ namespace Arent3d.Architecture.Routing.AppBase.Commands.Routing
         .PickElementsByRectangle( LimitRackReferenceSelectionFilter.Instance, "Please select any rack or rack detail curve by mouse drag." ).ToList() ;
       if ( ! selectedLimitRackRefElements.Any() ) return ( new List<string>(), new List<LimitRackModel>() ) ;
 
-      var selectedLimitRackModelsByAnyRack = limitRackStorable.LimitRackModels
+      var selectedLimitRackModelsByAnyRacks = limitRackStorable.LimitRackModels
         .Where( lmb => lmb.RackIds
           .Any( rackId => selectedLimitRackRefElements
             .Any( r => r.UniqueId == rackId ) ) )
         .EnumerateAll() ;
-      if ( selectedLimitRackModelsByAnyRack.Any() ) {
-        var allRacks = selectedLimitRackModelsByAnyRack.SelectMany( lm => lm.RackIds ).Distinct().EnumerateAll() ;
-        return ( allRacks, selectedLimitRackModelsByAnyRack ) ;
+      if ( selectedLimitRackModelsByAnyRacks.Any() ) {
+        var allRacks = selectedLimitRackModelsByAnyRacks.SelectMany( lm => lm.RackIds ).Distinct().EnumerateAll() ;
+        return ( allRacks, selectedLimitRackModelsByAnyRacks ) ;
       }
 
-      var selectedLimitRackModelsByAnyDetailCurve = limitRackStorable.LimitRackModels
+      var selectedLimitRackModelsByAnyDetailCurves = limitRackStorable.LimitRackModels
         .Where( lmb => lmb.RackDetailLineIds
           .Any( dtlId => selectedLimitRackRefElements
             .Any( r => r.UniqueId == dtlId ) ) )
-        .EnumerateAll() ;
-      if ( ! selectedLimitRackModelsByAnyDetailCurve.Any() ) return ( new List<string>(), new List<LimitRackModel>() ) ;
+        .ToList() ;
+
+      var removeLimitRackModels = new List<LimitRackModel>() ;
+      foreach ( var selectedLimitRackModel in selectedLimitRackModelsByAnyDetailCurves ) {
+        var selectedDetailIds = selectedLimitRackRefElements.Where( lrm =>
+          selectedLimitRackModel.RackDetailLineIds.Any( id => id.Equals( lrm.UniqueId ) ) ) ;
+        if ( selectedDetailIds.Count() != selectedLimitRackModel.RackDetailLineIds.Count() ) {
+          removeLimitRackModels.Add( selectedLimitRackModel );
+        }
+      }
+
+      if ( removeLimitRackModels.Any() ) {
+        MessageBox.Show( "Some limit rack can not be delete. Please select all detail line for limit rack you want to delete." ) ;
+        removeLimitRackModels.ForEach( rlmd=> selectedLimitRackModelsByAnyDetailCurves. Remove( rlmd ));
+      }
+      
+      if ( ! selectedLimitRackModelsByAnyDetailCurves.Any() ) return ( new List<string>(), new List<LimitRackModel>() ) ;
       {
-        var allRacks = selectedLimitRackModelsByAnyDetailCurve.SelectMany( lm => lm.RackIds ).Distinct()
+        var allRacks = selectedLimitRackModelsByAnyDetailCurves.SelectMany( lm => lm.RackIds ).Distinct()
           .EnumerateAll() ;
-        return ( allRacks, selectedLimitRackModelsByAnyDetailCurve ) ;
+        return ( allRacks, selectedLimitRackModelsByAnyDetailCurves ) ;
       }
     }
 
