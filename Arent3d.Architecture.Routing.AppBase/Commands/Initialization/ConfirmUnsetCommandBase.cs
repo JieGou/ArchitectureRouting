25c36022b6d90a51c2be83ea15d1cd970f850a3d
TuanAnhArent <107823090+TuanAnhArent@users.noreply.github.com>
2022-08-30 17:39:46 +0700

    refactor code

--- Arent3d.Architecture.Routing.AppBase/Commands/Initialization/ConfirmUnsetCommandBase.cs
+++ Arent3d.Architecture.Routing.AppBase/Commands/Initialization/ConfirmUnsetCommandBase.cs
@@ -18,18 +18,18 @@ namespace Arent3d.Architecture.Routing.AppBase.Commands.Initialization
       try {
         return document.Transaction( "TransactionName.Commands.Routing.ConfirmUnset".GetAppStringByKeyOrDefault( "Confirm Unset" ), _ =>
         {
-          var elementNotConstruction = document.GetAllElements<Element>().OfCategory( BuiltInCategorySets.ConstructionItems ).Where( c => c.TryGetProperty( ElectricalRoutingElementParameter.ConstructionItem, out string? constructionItem ) && ( string.IsNullOrEmpty( constructionItem ) || constructionItem == DefaultConstructionItems )).ToList() ;
+          var elementsNotHavingConstructionItem = document.GetAllElements<Element>().OfCategory( BuiltInCategorySets.ConstructionItems ).Where( c => c.TryGetProperty( ElectricalRoutingElementParameter.ConstructionItem, out string? constructionItem ) && ( string.IsNullOrEmpty( constructionItem ) || constructionItem == DefaultConstructionItems )).ToList() ;
           var color = new Color( 255, 0, 0 ) ;
-          if ( elementNotConstruction.Any( t =>
+          if ( elementsNotHavingConstructionItem.Any( t =>
               {
                 var colorOfElement = t.Document.ActiveView.GetElementOverrides( t.Id ).ProjectionLineColor ;
                 if ( ! colorOfElement.IsValid ) return false ;
                 
-                return colorOfElement.Red == 255 && colorOfElement.Blue == 0 && colorOfElement.Green == 0 ;
+                return colorOfElement.Red == color.Red && colorOfElement.Blue == color.Blue && colorOfElement.Green == color.Green ;
               } ) )
-            ResetElementColor( elementNotConstruction ) ;
+            ResetElementColor( elementsNotHavingConstructionItem ) ;
           else
-            ChangeElementColor( elementNotConstruction, color ) ;
+            ChangeElementColor( elementsNotHavingConstructionItem, color ) ;
 
           return Result.Succeeded ;
         } ) ;
