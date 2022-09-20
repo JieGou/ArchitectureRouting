d5733ac64bd9b632e112b45e15aea7b1e8bbbf86
arent3d-chaunguyen <chau.nguyen@arent3d.com>
2022-09-20 21:40:38 +0700

    modify rack size dialog

--- Arent3d.Architecture.Routing.Electrical.App/Forms/RackSizeDialog.xaml.cs
+++ Arent3d.Architecture.Routing.Electrical.App/Forms/RackSizeDialog.xaml.cs
@@ -1,16 +1,24 @@
+using System.Linq ;
 using System.Windows ;
+using System.Windows.Controls ;
 using System.Windows.Interop ;
 
 namespace Arent3d.Architecture.Routing.Electrical.App.Forms
 {
   public partial class RackSizeDialog : Window
   {
-    private readonly double[] _rackSizes = { 200, 300, 400, 500, 600} ;
+    public string Material { get ; set ; } = "" ;
+    public bool IsAutoSizing { get ; set ; } = false ;
     public RackSizeDialog()
     {
       InitializeComponent() ;
-      cmbSizes.ItemsSource = _rackSizes ;
-      cmbSizes.SelectedIndex = 0 ;
+      
+      cmbSizes.ItemsSource = Enumerable.Range(1, 10).Select(x => x * 100.0).ToArray() ;
+      cmbSizes.SelectedIndex = 1 ;
+      
+      cmbNumberOfRack.ItemsSource = Enumerable.Range(1, 30).ToArray() ;
+      cmbNumberOfRack.SelectedIndex = 0 ;
+
       var helper = new WindowInteropHelper( this ) { Owner = Autodesk.Windows.ComponentManager.ApplicationWindow } ;
     }
 
@@ -25,6 +33,18 @@ namespace Arent3d.Architecture.Routing.Electrical.App.Forms
         this.DialogResult = true ;
       else
         MessageBox.Show("有効なサイズを入力してください。", "エラー", MessageBoxButton.OK, MessageBoxImage.Error, MessageBoxResult.Yes);
+    }
+
+    private void OnChangeMaterial( object sender, RoutedEventArgs e )
+    {
+      Material = (string)((sender as RadioButton)?.Content?? "");
+    }
+
+    private void OnClickAutoCalculate( object sender, RoutedEventArgs e )
+    {
+      cmbSizes.Text = "" ;
+      cmbNumberOfRack.SelectedIndex = -1 ;
+      IsAutoSizing = true ;
     }
   }
 }
