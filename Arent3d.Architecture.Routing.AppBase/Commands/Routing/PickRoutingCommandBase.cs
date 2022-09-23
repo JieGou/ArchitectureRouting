ffa42b039d54ef2b6fe084e5b49ced73fca5e1a6
Mi Do Tra <midtt@rikkeisoft.com>
2022-09-23 16:49:47 +0700

    show error "The shaft does not go through the levels of connectors" when route

--- Arent3d.Architecture.Routing.AppBase/Commands/Routing/PickRoutingCommandBase.cs
+++ Arent3d.Architecture.Routing.AppBase/Commands/Routing/PickRoutingCommandBase.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic ;
 using System.Linq ;
 using System.Text.RegularExpressions ;
+using System.Windows ;
 using Arent3d.Architecture.Routing.AppBase.Commands.Initialization ;
 using Arent3d.Architecture.Routing.AppBase.Forms ;
 using Arent3d.Architecture.Routing.AppBase.Selection ;
@@ -52,6 +53,14 @@ namespace Arent3d.Architecture.Routing.AppBase.Commands.Routing
 
         var property = ShowPropertyDialog( uiDocument.Document, fromPickResult, toPickResult ) ;
         if ( true != property?.DialogResult ) return OperationResult<PickState>.Cancelled ;
+
+        if ( property.GetShaft() != null ) {
+          var isShaftThroughFromAndToLevel = CheckShaft( document, property.GetShaft()!.UniqueId, fromPickResult.GetLevelId(), toPickResult.GetLevelId() ) ;
+          if ( ! isShaftThroughFromAndToLevel ) {
+            MessageBox.Show( "The shaft does not go through the levels of connectors", "Arent" ) ;
+            return OperationResult<PickState>.Cancelled ;
+          }
+        }
 
         if ( GetMEPSystemClassificationInfo( fromPickResult, toPickResult, property.GetSystemType() ) is not { } classificationInfo ) return OperationResult<PickState>.Cancelled ;
 
@@ -214,6 +223,17 @@ namespace Arent3d.Architecture.Routing.AppBase.Commands.Routing
       sv.ShowDialog() ;
 
       return sv ;
+    }
+
+    private static bool CheckShaft( Document document, string shaftElementUniqueId, ElementId fromLevelId, ElementId toLevelId )
+    {
+      var shaft = document.GetElementById<Opening>( shaftElementUniqueId ) ;
+      if ( shaft == null ) return false ;
+      var baseLevel = (Level) document.GetElement( shaft.get_Parameter( BuiltInParameter.WALL_BASE_CONSTRAINT ).AsElementId() ) ;
+      var topLevel = (Level) document.GetElement( shaft.get_Parameter( BuiltInParameter.WALL_HEIGHT_TYPE ).AsElementId() ) ;
+      var fromLevel = (Level) document.GetElement( fromLevelId ) ;
+      var toLevel = (Level) document.GetElement( toLevelId ) ;
+      return fromLevel.Elevation >= baseLevel.Elevation && fromLevel.Elevation <= topLevel.Elevation && toLevel.Elevation >= baseLevel.Elevation && toLevel.Elevation <= topLevel.Elevation ;
     }
 
     protected override IReadOnlyCollection<(string RouteName, RouteSegment Segment)> GetRouteSegments( Document document, PickState pickState )
